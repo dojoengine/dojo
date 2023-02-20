edb569ed298254d64bc218360b2d6f3db2d404d1
Tarrence van As <tarrence@vanas.family>
2023-02-20 15:36:42 -0500

    feat: deployment config

--- crates/dojo-project/src/test.rs
+++ crates/dojo-project/src/test.rs
@@ -13,6 +13,14 @@ fn test_serde() {
             address: Some(FieldElement::from_str("0xdead").unwrap()),
             initializer_class_hash: Some(FieldElement::from_str("0xbeef").unwrap()),
         },
+        deployments: Some(crate::Deployments {
+            testnet: Some(crate::Deployment {
+                rpc: Some("https://starknet-goerli.rpc.gg/rpc/v0.2".into()),
+            }),
+            mainnet: Some(crate::Deployment {
+                rpc: Some("https://starknet.rpc.gg/rpc/v0.2".into()),
+            }),
+        }),
     };
     let serialized = toml::to_string(&config).unwrap();
     // NOTE: FieldElement encodes back to bigint string
@@ -25,6 +33,11 @@ fn test_serde() {
             [world]
             address = "57005"
             initializer_class_hash = "48879"
+            [deployments.testnet]
+            rpc = "https://starknet-goerli.rpc.gg/rpc/v0.2"
+
+            [deployments.mainnet]
+            rpc = "https://starknet.rpc.gg/rpc/v0.2"
         "# }
     );
     assert_eq!(config, toml::from_str(&serialized).unwrap());
