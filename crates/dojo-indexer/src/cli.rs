ac7324bf0ad0f71397d90182f6a6c02f53c9b7c9
Francesco Ceccon <francesco@ceccon.me>
2023-03-12 15:02:20 +0000

    feat(indexer): switch to the apibara sdk

--- crates/dojo-indexer/src/cli.rs
+++ crates/dojo-indexer/src/cli.rs
@@ -1,5 +1,7 @@
+use std::str::FromStr;
 use std::vec;
 
+use apibara_sdk::Uri;
 use clap::Parser;
 use futures::join;
 use num::{BigUint, Num};
@@ -14,7 +16,7 @@ use crate::processors::component_register::ComponentRegistrationProcessor;
 use crate::processors::component_state_update::ComponentStateUpdateProcessor;
 use crate::processors::system_register::SystemRegistrationProcessor;
 use crate::server::start_server;
-// use crate::server::start_server;
+use crate::stream::StarknetClientBuilder;
 
 mod processors;
 
@@ -22,10 +24,7 @@ mod graphql;
 mod hash;
 mod indexer;
 mod server;
-
 mod stream;
-// mod schema;
-// mod model;
 
 /// Command line args parser.
 /// Exits with 0/1 if the input is formatted correctly/incorrectly.
@@ -49,15 +48,14 @@ async fn main() -> anyhow::Result<()> {
     let world = BigUint::from_str_radix(&args.world[2..], 16).unwrap_or_else(|error| {
         panic!("Failed parsing world address: {error:?}");
     });
-    let node = &args.node;
 
     let database_url = &args.database_url;
     #[cfg(feature = "sqlite")]
     let pool = SqlitePoolOptions::new().max_connections(5).connect(database_url).await?;
     // #[cfg(feature = "postgres")]
     // let pool = PgPoolOptions::new().max_connections(5).connect(database_url).await?;
-
-    let stream = stream::ApibaraClient::new(node).await;
+    let node = Uri::from_str(&args.node)?;
+    let stream = StarknetClientBuilder::default().connect(node).await;
 
     let provider = JsonRpcClient::new(HttpTransport::new(Url::parse(&args.rpc).unwrap()));
 
@@ -72,10 +70,11 @@ async fn main() -> anyhow::Result<()> {
     };
 
     match stream {
-        std::result::Result::Ok(s) => {
+        std::result::Result::Ok((data_stream, stream_client)) => {
             println!("Connected");
             let graphql = start_server(&pool);
-            let indexer = start_indexer(s, &pool, &provider, &processors, world);
+            let indexer =
+                start_indexer(data_stream, stream_client, &pool, &provider, &processors, world);
             let _res = join!(graphql, indexer);
         }
         std::result::Result::Err(e) => panic!("Error: {:?}", e),
