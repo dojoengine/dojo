a4caff2783f3732c9a52621cda412c5fff7aa97c
Ammar Arif <evergreenkary@gmail.com>
2023-04-05 07:17:53 +0800

    Refactor: build World for migration from local and remote manifest (#177)

--- crates/dojo-lang/src/manifest_test.rs
+++ crates/dojo-lang/src/manifest_test.rs
@@ -1,6 +1,10 @@
+use std::collections::HashMap;
+
 use cairo_lang_filesystem::db::FilesGroup;
 use cairo_lang_semantic::test_utils::setup_test_crate;
 use pretty_assertions::assert_eq;
+use smol_str::SmolStr;
+use starknet::core::types::FieldElement;
 
 use crate::manifest::Manifest;
 use crate::testing::build_test_db;
@@ -24,7 +28,16 @@ fn test_manifest_generation() {
         ",
     );
 
-    let manifest = Manifest::new(db, &db.crates());
+    let mut compiled_contracts: HashMap<SmolStr, FieldElement> = HashMap::new();
+    compiled_contracts.insert("World".into(), FieldElement::from_hex_be("0x123").unwrap());
+    compiled_contracts.insert("Store".into(), FieldElement::from_hex_be("0x123").unwrap());
+    compiled_contracts.insert("Indexer".into(), FieldElement::from_hex_be("0x123").unwrap());
+    compiled_contracts.insert("Executor".into(), FieldElement::from_hex_be("0x123").unwrap());
+    compiled_contracts
+        .insert("PositionComponent".into(), FieldElement::from_hex_be("0x123").unwrap());
+    compiled_contracts.insert("MoveSystem".into(), FieldElement::from_hex_be("0x123").unwrap());
+
+    let manifest = Manifest::new(db, &db.crates(), compiled_contracts);
     assert_eq!(manifest.components.len(), 1);
     assert_eq!(manifest.systems.len(), 1);
 }
