1b7b685da76984a6dee678d75ee195140d35b18a
Tarrence van As <tarrence@vanas.family>
2023-04-04 09:17:40 -0400

    feat: dojo-physics

--- scripts/cairo_test.sh
+++ scripts/cairo_test.sh
@@ -4,4 +4,5 @@ set -euxo pipefail
 cargo +nightly-2022-11-03 run --bin dojo-test -- -p crates/dojo-core
 # Uncomment once erc crate passes
 # cargo +nightly-2022-11-03 run --bin dojo-test -- -p crates/dojo-erc
+cargo +nightly-2022-11-03 run --bin dojo-test -- -p crates/dojo-physics
 cargo +nightly-2022-11-03 run --bin dojo-test -- -p examples
