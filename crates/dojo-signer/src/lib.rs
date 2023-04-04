9a69207b28b9605870e0afb69020268946bc713c
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2023-04-04 13:14:20 +0000

    sig init and scaffold

---
+++ crates/dojo-signer/src/lib.rs
@@ -0,0 +1,52 @@
+use starknet::signers::{LocalWallet, VerifyingKey, SigningKey, Signer};
+use starknet::core::{crypto::{Signature}, types::FieldElement};
+use dotenv::dotenv;
+use lazy_static::lazy_static;
+use std::sync::Mutex;
+use std::env;
+use async_trait::async_trait;
+
+lazy_static! {
+    static ref STARK_PRIVATE_KEY: Mutex<Option<SigningKey>> = Mutex::new(None);
+}
+
+#[derive(Debug, Clone)]
+pub struct EnvLocalWallet {
+    inner_wallet: LocalWallet,
+}
+
+impl EnvLocalWallet {
+    pub fn from_env() -> Result<Self, Box<dyn std::error::Error>> {
+        dotenv().ok();
+
+        let private_key = {
+            let mut private_key_guard = STARK_PRIVATE_KEY.lock().unwrap();
+            match private_key_guard.clone() {
+                Some(key) => key,
+                None => {
+                    let private_key_str = env::var("STARK_PRIVATE_KEY")?;
+                    let key = SigningKey::from_str(&private_key_str)?;
+                    *private_key_guard = Some(key.clone());
+                    key
+                }
+            }
+        };
+
+        let local_wallet = LocalWallet::from_signing_key(private_key);
+        Ok(Self { inner_wallet: local_wallet })
+    }
+}
+
+#[async_trait]
+impl Signer for EnvLocalWallet {
+    type GetPublicKeyError = <LocalWallet as Signer>::GetPublicKeyError;
+    type SignError = <LocalWallet as Signer>::SignError;
+
+    async fn get_public_key(&self) -> Result<VerifyingKey, Self::GetPublicKeyError> {
+        self.inner_wallet.get_public_key().await
+    }
+
+    async fn sign_hash(&self, hash: &FieldElement) -> Result<Signature, Self::SignError> {
+        self.inner_wallet.sign_hash(hash).await
+    }
+}
