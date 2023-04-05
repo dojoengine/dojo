fb1a97a42e298f7e57b20dcdf859ca36e0652cbc
Tarrence van As <tarrence@vanas.family>
2023-04-05 14:26:12 -0400

    fix: build with dojo_core + add test

---
+++ crates/dojo-lang/src/compiler_test.rs
@@ -0,0 +1,29 @@
+use std::env;
+
+use camino::Utf8PathBuf;
+use scarb::compiler::CompilerRepository;
+use scarb::core::Config;
+use scarb::ops;
+use scarb::ui::Verbosity;
+
+use super::DojoCompiler;
+
+#[test]
+fn test_compiler() {
+    let mut compilers = CompilerRepository::empty();
+    compilers.add(Box::new(DojoCompiler)).unwrap();
+
+    let path = Utf8PathBuf::from_path_buf("src/cairo_level_tests/Scarb.toml".into()).unwrap();
+
+    println!("Path: {:?}", path.canonicalize_utf8());
+    let config = Config::builder(path.canonicalize_utf8().unwrap())
+        .ui_verbosity(Verbosity::Verbose)
+        .log_filter_directive(env::var_os("SCARB_LOG"))
+        .compilers(compilers)
+        .build()
+        .unwrap();
+
+    let ws = ops::read_workspace(config.manifest_path(), &config).unwrap();
+
+    ops::compile(&ws).unwrap_or_else(|op| panic!("Error compiling: {:?}", op))
+}
