30081af4eda04dfdf299b07e5d15e1c6d6077c5b
Tarrence van As <tarrence@vanas.family>
2023-04-06 13:51:26 -0400

    refactor: simplify signer

--- crates/dojo-signer/src/lib.rs
+++ crates/dojo-signer/src/lib.rs
@@ -1,44 +1,23 @@
-use starknet::signers::{LocalWallet, VerifyingKey, SigningKey, Signer};
-use starknet::core::{crypto::{Signature}, types::FieldElement};
-use dotenv::dotenv;
-use lazy_static::lazy_static;
-use std::sync::Mutex;
 use std::env;
-use async_trait::async_trait;
 
-lazy_static! {
-    static ref STARK_PRIVATE_KEY: Mutex<Option<SigningKey>> = Mutex::new(None);
-}
+use async_trait::async_trait;
+use starknet::core::crypto::Signature;
+use starknet::core::types::FieldElement;
+use starknet::signers::{LocalWallet, Signer, SigningKey, VerifyingKey};
 
 #[derive(Debug, Clone)]
 pub struct EnvLocalWallet {
-    inner_wallet: LocalWallet,
+    inner: LocalWallet,
 }
 
 impl EnvLocalWallet {
-    pub fn from_env() -> Result<Self, Box<dyn std::error::Error>> {
-        dotenv().ok();
+    pub fn from_env() -> anyhow::Result<Self> {
+        let private_key_str = env::var("STARK_PRIVATE_KEY")?;
+        let private_key = FieldElement::from_hex_be(&private_key_str)?;
 
-        let private_key = {
-            let mut private_key_guard = STARK_PRIVATE_KEY.lock().unwrap();
-            match private_key_guard.clone() {
-                Some(key) => key,
-                None => {
-                    let private_key_str = env::var("STARK_PRIVATE_KEY")?;
-                    let private_key = FieldElement::from_hex_be(
-                        &private_key_str,
-                    )
-                    .unwrap();
-            
-                    let key = SigningKey::from_secret_scalar(private_key);
-                    *private_key_guard = Some(key.clone());
-                    key
-                }
-            }
-        };
-
-        let local_wallet = LocalWallet::from_signing_key(private_key);
-        Ok(Self { inner_wallet: local_wallet })
+        Ok(Self {
+            inner: LocalWallet::from_signing_key(SigningKey::from_secret_scalar(private_key)),
+        })
     }
 }
 
@@ -48,10 +27,10 @@ impl Signer for EnvLocalWallet {
     type SignError = <LocalWallet as Signer>::SignError;
 
     async fn get_public_key(&self) -> Result<VerifyingKey, Self::GetPublicKeyError> {
-        self.inner_wallet.get_public_key().await
+        self.inner.get_public_key().await
     }
 
     async fn sign_hash(&self, hash: &FieldElement) -> Result<Signature, Self::SignError> {
-        self.inner_wallet.sign_hash(hash).await
+        self.inner.sign_hash(hash).await
     }
 }
