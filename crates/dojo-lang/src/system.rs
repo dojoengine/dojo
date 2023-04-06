c2e2b8fe9f389742038dd42ef91dac122db22ca2
Yun <broody@gmail.com>
2023-04-06 09:10:44 -0700

    feat: handle if/block/match nodes in system (#183)
    * feat: handle if/block/match expressions in system
    
    * fix: block parsing issue

--- crates/dojo-lang/src/system.rs
+++ crates/dojo-lang/src/system.rs
@@ -136,17 +136,15 @@ impl System {
             ast::Statement::Let(statement_let) => {
                 if let ast::Expr::FunctionCall(expr_fn) = statement_let.rhs(db) {
                     if let Some(rewrite_nodes) =
-                        self.handle_expr(db, Some(statement_let.pattern(db)), expr_fn)
+                        self.handle_fn_call(db, Some(statement_let.pattern(db)), expr_fn)
                     {
                         return rewrite_nodes;
                     }
                 }
             }
             ast::Statement::Expr(expr) => {
-                if let ast::Expr::FunctionCall(expr_fn) = expr.expr(db) {
-                    if let Some(rewrite_nodes) = self.handle_expr(db, None, expr_fn) {
-                        return rewrite_nodes;
-                    }
+                if let Some(rewrite_nodes) = self.handle_expr(db, expr.expr(db)) {
+                    return rewrite_nodes;
                 }
             }
             _ => {}
@@ -155,7 +153,119 @@ impl System {
         vec![RewriteNode::Copied(statement_ast.as_syntax_node())]
     }
 
-    fn handle_expr(
+    fn handle_expr(&mut self, db: &dyn SyntaxGroup, expr: ast::Expr) -> Option<Vec<RewriteNode>> {
+        match expr {
+            ast::Expr::FunctionCall(expr_fn) => self.handle_fn_call(db, None, expr_fn),
+            ast::Expr::If(expr_if) => Some(self.handle_if(db, expr_if, false)),
+            ast::Expr::Block(expr_block) => Some(self.handle_block(db, expr_block)),
+            ast::Expr::Match(expr_match) => Some(self.handle_match(db, expr_match)),
+            // TODO: loop expression when supported
+            _ => None,
+        }
+    }
+
+    fn handle_if(
+        &mut self,
+        db: &dyn SyntaxGroup,
+        expr_if: ast::ExprIf,
+        is_else_if: bool,
+    ) -> Vec<RewriteNode> {
+        // recurse thru if blocks
+        let if_block: Vec<RewriteNode> = self.handle_block(db, expr_if.if_block(db));
+        let else_prefix = if is_else_if { "else " } else { "" };
+        let code = format!("{}if $condition$ $block$", else_prefix);
+        let if_rewrite = RewriteNode::interpolate_patched(
+            &code,
+            HashMap::from([
+                (
+                    "condition".to_string(),
+                    RewriteNode::Copied(expr_if.condition(db).as_syntax_node()),
+                ),
+                ("block".to_string(), RewriteNode::new_modified(if_block)),
+            ]),
+        );
+
+        // recurse thru else/if blocks
+        if let ast::OptionElseClause::ElseClause(else_clause) = expr_if.else_clause(db) {
+            match else_clause.else_block_or_if(db) {
+                ast::BlockOrIf::Block(expr_else_block) => {
+                    let else_block = self.handle_block(db, expr_else_block);
+                    let else_rewrite = RewriteNode::interpolate_patched(
+                        "else $block$",
+                        HashMap::from([(
+                            "block".to_string(),
+                            RewriteNode::new_modified(else_block),
+                        )]),
+                    );
+                    return vec![if_rewrite, else_rewrite];
+                }
+                ast::BlockOrIf::If(expr_else_if) => {
+                    let else_if_nodes: Vec<RewriteNode> = self.handle_if(db, expr_else_if, true);
+                    return vec![if_rewrite].into_iter().chain(else_if_nodes.into_iter()).collect();
+                }
+            };
+        }
+
+        vec![if_rewrite]
+    }
+
+    fn handle_block(
+        &mut self,
+        db: &dyn SyntaxGroup,
+        expr_block: ast::ExprBlock,
+    ) -> Vec<RewriteNode> {
+        let block_nodes: Vec<RewriteNode> = expr_block
+            .statements(db)
+            .elements(db)
+            .iter()
+            .flat_map(|statement| self.handle_statement(db, statement.clone()))
+            .collect();
+
+        let block_rewrite = RewriteNode::interpolate_patched(
+            "{ $nodes$ }",
+            HashMap::from([("nodes".to_string(), RewriteNode::new_modified(block_nodes))]),
+        );
+        vec![block_rewrite]
+    }
+
+    fn handle_match(
+        &mut self,
+        db: &dyn SyntaxGroup,
+        expr_match: ast::ExprMatch,
+    ) -> Vec<RewriteNode> {
+        let match_nodes: Vec<RewriteNode> = expr_match
+            .arms(db)
+            .elements(db)
+            .iter()
+            .flat_map(|arm| {
+                if let ast::Expr::Block(arm_block) = arm.expression(db) {
+                    let arm_pat = arm.pattern(db);
+                    let arm_block = self.handle_block(db, arm_block);
+                    let arm_rewrite = RewriteNode::interpolate_patched(
+                        "$pattern$ => $block$,",
+                        HashMap::from([
+                            ("pattern".to_string(), RewriteNode::Copied(arm_pat.as_syntax_node())),
+                            ("block".to_string(), RewriteNode::new_modified(arm_block)),
+                        ]),
+                    );
+                    return vec![arm_rewrite];
+                }
+
+                vec![RewriteNode::Copied(arm.as_syntax_node())]
+            })
+            .collect();
+
+        let match_rewrite = RewriteNode::interpolate_patched(
+            "match $expr$ { $arms$ }",
+            HashMap::from([
+                ("expr".to_string(), RewriteNode::Copied(expr_match.expr(db).as_syntax_node())),
+                ("arms".to_string(), RewriteNode::new_modified(match_nodes)),
+            ]),
+        );
+        vec![match_rewrite]
+    }
+
+    fn handle_fn_call(
         &mut self,
         db: &dyn SyntaxGroup,
         var_name: Option<ast::Pattern>,
