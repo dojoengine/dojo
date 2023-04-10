64f4e33e0157d5aafce81b162cc9aea05635f3f1
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2023-04-10 13:35:30 +0000

    hook work, readme

--- packages/react/src/hooks/get.ts
+++ packages/react/src/hooks/get.ts
@@ -3,6 +3,9 @@ import { useDojoContext } from '../provider';
 import { Query } from 'dojo-core/dist/types';
 import { Store } from 'dojo-core';
 
+// todo: this should really be components and not entities
+
+// key should be from world setup, and should be an optional trigger to rerender
 export function useDojoEntity<T>({
   key,
   parser,
@@ -10,39 +13,119 @@ export function useDojoEntity<T>({
   key: any;
   parser: (data: any) => T | undefined;
 }) {
+  // -- Context -- //
+  const { rpcProvider } = useDojoContext();
 
   // -- Store -- //
   const store = Store.EntityStore;
 
-  // -- Context -- //
-  const { rpcProvider } = useDojoContext();
-
   // -- Callbacks -- //
-  const getEntity = useCallback(
+  const getEntityCallback = useCallback(
     async (
       component: bigint,
       query: Query,
       offset: number,
       length: number
     ) => {
-      if (rpcProvider) {
-        const fetchedEntity = await rpcProvider.entity(
-          component,
-          query,
-          offset,
-          length
-        );
-        store.setState({ entity: fetchedEntity });
-      } else {
-        store.setState({ entity: [] });
-      }
+      await getEntity(
+        store,
+        rpcProvider,
+        component,
+        query,
+        offset,
+        length
+      );
     },
-    [rpcProvider, key]
+    [rpcProvider, key, parser]
   );
 
+  const setEntityCallback = useCallback(
+    async (
+      optimistic: boolean,
+      value: bigint[],
+      component: bigint,
+      query: Query,
+      offset: number,
+      length: number
+    ) => {
+      await setEntity(
+        store,
+        rpcProvider,
+        optimistic,
+        value,
+        component,
+        query,
+        offset,
+        length
+      );
+    },
+    [rpcProvider, key, parser]
+  );
 
+  return {
+    entity: parser(store.getState()),
+    getEntity: getEntityCallback,
+    setEntity: setEntityCallback,
+  };
+}
 
-  console.log("useDojoEntity", store.getState());
+// we should pass in providers here
 
-  return { entity: parser(store.getState()), getEntity, setEntity: store.setState };
+// todo - this should know the component and fill in the gaps
+export async function setEntity<T>(
+  store: any, // TODO: Types
+  rpcProvider: any,
+  optimistic: boolean,
+  value: bigint[],
+  component: bigint,
+  query: Query,
+  offset: number,
+  length: number
+) {
+  // TODO: This is very incomplete
+  // Set the entity immediately in the store
+  // Idea here was to optimistically update the state in zustand to reflect the users input
+  // restrictions in the client logic can control auth here.
+  // From a User POV, most of the time they just want to see the reflection in the client of what they have done, this gives a client
+  // representation of it.
+  // this could be replaced in the future with the state diff
+  if (optimistic) store.setState({ entity: value });
+
+  // execute here
+  if (rpcProvider) {
+    await rpcProvider.updateEntity(component, query, offset, length);
+    // Trigger getEntity to fetch the updated entity or 
+
+    // Fetch entity 
+    await getEntity(
+      store,
+      rpcProvider,
+      component,
+      query,
+      offset,
+      length
+    );
+  }
+}
+
+// we should pass in providers here
+export async function getEntity<T>(
+  store: any, // todo: fix types
+  rpcProvider: any,
+  component: bigint,
+  query: Query,
+  offset: number,
+  length: number
+) {
+  if (rpcProvider) {
+    const fetchedEntity = await rpcProvider.entity(
+      component,
+      query,
+      offset,
+      length
+    );
+    store.setState({ entity: fetchedEntity });
+  } else {
+    store.setState({ entity: [] });
+  }
 }
--- packages/react/src/provider/DojoConnect.tsx
+++ packages/react/src/provider/DojoConnect.tsx
@@ -4,10 +4,16 @@ import { Providers } from 'dojo-core';
 export interface DojoContext {
   worldAddress?: string;
   rpcProvider?: Providers.RPCProvider;
+  components: { [id: string]: any };
+  registerComponent: (components: { id: string; component: any }[]) => void;
+  unregisterComponent: (id: string) => void;
 }
 
 const DOJO_INITIAL_STATE: DojoContext = {
-  worldAddress: undefined // we can add a default world address here
+  worldAddress: undefined,
+  components: {},
+  registerComponent: () => { },
+  unregisterComponent: () => { },
 };
 
 const RpcContext = createContext<DojoContext>(DOJO_INITIAL_STATE);
@@ -25,13 +31,35 @@ export const DojoConfig: React.FC<DojoConfigProps> = ({
 }: any) => {
   const [rpcProvider, setRpcProvider] = useState<Providers.RPCProvider>();
 
+
   useEffect(() => {
     const newRpcProvider = new Providers.RPCProvider(worldAddress, rpcUrl);
     setRpcProvider(newRpcProvider);
   }, [worldAddress, rpcUrl]);
 
+  // TODO: Do we also move this to zustand?
+  const [components, setComponents] = useState<{ [id: string]: any }>({});
+
+  // --- Component Registry --- //
+  const registerComponent = (newComponents: { id: string; component: any }[]) => {
+    setComponents((prevComponents) => {
+      const updatedComponents = { ...prevComponents };
+      newComponents.forEach(({ id, component }) => {
+        updatedComponents[id] = component;
+      });
+      return updatedComponents;
+    });
+  };
+
+  const unregisterComponent = (id: string) => {
+    setComponents((prevComponents) => {
+      const { [id]: _, ...rest } = prevComponents;
+      return rest;
+    });
+  };
+
   return (
-    <RpcContext.Provider value={{ rpcProvider }}>
+    <RpcContext.Provider value={{ worldAddress, rpcProvider, components, registerComponent, unregisterComponent }}>
       {children}
     </RpcContext.Provider>
   );
