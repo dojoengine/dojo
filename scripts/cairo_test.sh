7c3a8c5b2c72bd0eb18933fd95195a2511734bfc
Milan Cermak <milan.cermak@gmail.com>
2023-04-15 21:33:53 +0200

    dev: re-enabling dojo-erc tests

--- scripts/cairo_test.sh
+++ scripts/cairo_test.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -euxo pipefail
 
-cargo +nightly-2022-11-03 run --bin dojo-test -- crates/dojo-core
-# Uncomment once erc crate passes
+cargo +nightly-2022-11-03 run --bin dojo-test -- -p crates/dojo-core
 cargo +nightly-2022-11-03 run --bin dojo-test -- -p crates/dojo-erc
 cargo +nightly-2022-11-03 run --bin dojo-test -- -p crates/dojo-physics
 cargo +nightly-2022-11-03 run --bin dojo-test -- -p examples
