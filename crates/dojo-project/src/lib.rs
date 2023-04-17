6cce8ccf27cb80bd3d37ebcf4d1026076516439d
Kari <evergreenkary@gmail.com>
2023-04-17 18:36:27 +0800

    environment config in manifest file

--- crates/dojo-project/src/lib.rs
+++ crates/dojo-project/src/lib.rs
@@ -1,15 +1,24 @@
 pub mod migration;
 
+use std::str::FromStr;
+
+use anyhow::anyhow;
 use scarb::core::Workspace;
 use serde::{Deserialize, Serialize};
-use starknet::core::types::FieldElement;
+use starknet::{
+    core::{chain_id, types::FieldElement},
+    providers::SequencerGatewayProvider,
+};
 use toml::Value;
+use url::Url;
 
 #[allow(clippy::enum_variant_names)]
 #[derive(thiserror::Error, Debug)]
 pub enum DeserializationError {
     #[error("parsing field element")]
     ParsingFieldElement,
+    #[error("parsing url")]
+    ParsingUrl,
 }
 
 #[derive(Clone, Copy, Debug, Default, PartialEq, Eq, Serialize, Deserialize)]
@@ -51,5 +60,109 @@ impl WorldConfig {
         }
 
         Ok(world_config)
+    }
+}
+
+#[derive(Debug, Deserialize, Clone, Default)]
+pub struct EnvironmentConfig {
+    pub rpc: Option<Url>,
+    pub network: Option<String>,
+    pub chain_id: Option<FieldElement>,
+    pub private_key: Option<FieldElement>,
+    pub account_address: Option<FieldElement>,
+}
+
+impl EnvironmentConfig {
+    pub fn from_workspace<T: AsRef<str>>(
+        env: T,
+        ws: &Workspace<'_>,
+    ) -> Result<Self, DeserializationError> {
+        let mut config = EnvironmentConfig::default();
+
+        if let Some(dojo_metadata) = dojo_metadata_from_workspace(ws) {
+            if let Some(env_metadata) = dojo_metadata.get("env") {
+                if let Some(env) = env_metadata.get(env.as_ref()) {
+                    if let Some(rpc) = env.get("rpc_url") {
+                        if let Some(rpc) = rpc.as_str() {
+                            let url =
+                                Url::parse(rpc).map_err(|_| DeserializationError::ParsingUrl)?;
+                            config.rpc = Some(url);
+                        };
+                    }
+
+                    if let Some(chain_id) = env.get("chain_id") {
+                        if let Some(chain_id) = chain_id.as_str() {
+                            let chain_id = FieldElement::from_str(chain_id)
+                                .map_err(|_| DeserializationError::ParsingFieldElement)?;
+                            config.chain_id = Some(chain_id);
+                        };
+                    }
+
+                    if let Some(private_key) = env.get("private_key") {
+                        if let Some(private_key) = private_key.as_str() {
+                            let pk = FieldElement::from_hex_be(private_key)
+                                .map_err(|_| DeserializationError::ParsingFieldElement)?;
+                            config.private_key = Some(pk);
+                        }
+                    }
+
+                    if let Some(account_address) = env.get("account_address") {
+                        if let Some(account_address) = account_address.as_str() {
+                            let address = FieldElement::from_hex_be(account_address)
+                                .map_err(|_| DeserializationError::ParsingFieldElement)?;
+                            config.account_address = Some(address);
+                        }
+                    }
+
+                    if let Some(network) = env.get("network") {
+                        if let Some(network) = network.as_str() {
+                            config.network = Some(network.into());
+
+                            if config.chain_id.is_none() {
+                                config.chain_id = get_chain_id_from_network(network);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        Ok(config)
+    }
+
+    pub fn get_provider(&self) -> anyhow::Result<SequencerGatewayProvider> {
+        if self.rpc.is_none() && self.network.is_none() {
+            return Err(anyhow!("Missing `rpc_url` or `network` in the environment config"));
+        }
+
+        let provider = if let Some(url) = &self.rpc {
+            let mut base = url.clone();
+            base.path_segments_mut().unwrap().pop_if_empty();
+
+            let mut gateway = base.clone();
+            gateway.path_segments_mut().unwrap().push("gateway");
+            let mut feeder_gateway = base.clone();
+            feeder_gateway.path_segments_mut().unwrap().push("feeder_gateway");
+
+            SequencerGatewayProvider::new(gateway, feeder_gateway)
+        } else {
+            match self.network.as_ref().unwrap().as_str() {
+                "mainnet" => SequencerGatewayProvider::starknet_alpha_mainnet(),
+                "goerli" => SequencerGatewayProvider::starknet_alpha_goerli(),
+                "goerli2" => SequencerGatewayProvider::starknet_alpha_goerli_2(),
+                n => return Err(anyhow!("Unsupported network: {n}")),
+            }
+        };
+
+        Ok(provider)
+    }
+}
+
+fn get_chain_id_from_network(network: &str) -> Option<FieldElement> {
+    match network {
+        "mainnet" => Some(chain_id::MAINNET),
+        "goerli" => Some(chain_id::TESTNET),
+        "goerli2" => Some(chain_id::TESTNET2),
+        _ => None,
     }
 }
