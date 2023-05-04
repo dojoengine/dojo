5adc92e063d63c727b131e7ca89c5a987b5f2117
tbelqdx <thomas.belloc@gmail.com>
2023-05-04 08:16:46 +0000

    merge main

--- scripts/cairo_test.sh
+++ scripts/cairo_test.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 cargo +nightly-2022-11-03 run --bin dojo-test -- crates/dojo-core
 cargo +nightly-2022-11-03 run --bin dojo-test -- crates/dojo-erc
-cargo +nightly-2022-11-03 run --bin dojo-test -- crates/dojo-physics
+#cargo +nightly-2022-11-03 run --bin dojo-test -- crates/dojo-physics
 cargo +nightly-2022-11-03 run --bin dojo-test -- crates/dojo-core/tests
 cargo +nightly-2022-11-03 run --bin dojo-test -- crates/dojo-defi
 cargo +nightly-2022-11-03 run --bin dojo-test -- examples
