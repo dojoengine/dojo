ce2dbd1cd1ba80cbb8496ed9c7bfcea0d621fc6f
JunichiSugiura <jun.sugiura.jp@gmail.com>
2023-05-13 16:54:17 +0200

    chore(bevy): execlude indexer module from wasm target

--- crates/bevy-dojo/src/lib.rs
+++ crates/bevy-dojo/src/lib.rs
@@ -1,9 +1,13 @@
-mod indexer;
 mod light_client;
 
-pub use indexer::*;
-pub use light_client::*;
+#[cfg(not(target_arch = "wasm32"))]
+mod indexer;
 
+#[cfg(not(target_arch = "wasm32"))]
 pub mod apibara {
     pub use {apibara_core as core, apibara_sdk as sdk};
+
+    pub use crate::indexer::*;
 }
+
+pub use light_client::*;
