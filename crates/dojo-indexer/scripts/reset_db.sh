f1cfec6948b0ca0e73b8b81cb18aa8abc4ecdf83
broody <broody@gmail.com>
2023-05-15 08:45:02 -0700

    Graphql entity keys to vec

--- crates/dojo-indexer/scripts/reset_db.sh
+++ crates/dojo-indexer/scripts/reset_db.sh
@@ -2,10 +2,7 @@
 set -o pipefail
 set -e
 
-# if error 'unable to open database file', 
-# use absolute path to database file
-# https://github.com/launchbadge/sqlx/issues/1260
 rm -f indexer.db
-cargo sqlx database create --database-url sqlite:indexer.db
-cargo sqlx migrate run --database-url sqlite:indexer.db
-cargo sqlx prepare --database-url sqlite:indexer.db
+cargo sqlx database create --database-url sqlite://$PWD/indexer.db
+cargo sqlx migrate run --database-url sqlite://$PWD/indexer.db
+cargo sqlx prepare --database-url sqlite://$PWD/indexer.db
