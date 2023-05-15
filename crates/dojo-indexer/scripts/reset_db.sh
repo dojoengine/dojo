fde8972422de497c04417e215498cb8e21faa986
broody <broody@gmail.com>
2023-05-15 16:05:18 -0700

    Seed indexer db with mock data

--- crates/dojo-indexer/scripts/reset_db.sh
+++ crates/dojo-indexer/scripts/reset_db.sh
@@ -2,7 +2,8 @@
 set -o pipefail
 set -e
 
-rm -f indexer.db
-cargo sqlx database create --database-url sqlite://$PWD/indexer.db
+pushd $(dirname "$0")/..
+
+yes | cargo sqlx database reset --database-url sqlite://$PWD/indexer.db
 cargo sqlx migrate run --database-url sqlite://$PWD/indexer.db
 cargo sqlx prepare --database-url sqlite://$PWD/indexer.db
