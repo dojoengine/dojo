e0a8cf00a650739d1e4019edfd7539b6fd80445a
broody <broody@gmail.com>
2023-05-16 10:27:57 -0700

    Simplify graphql queries

--- crates/dojo-indexer/src/graphql/mod.rs
+++ crates/dojo-indexer/src/graphql/mod.rs
@@ -48,30 +48,19 @@ impl Query {
         entity::entity(context, id).await
     }
 
-    async fn entities(context: &Context) -> FieldResult<Vec<Entity>> {
-        entity::entities(context).await
-    }
-
-    async fn entities_by_partition_id(
+    async fn entities(
         context: &Context,
         partition_id: String,
+        keys: Option<Vec<String>>,
     ) -> FieldResult<Vec<Entity>> {
-        entity::entities_by_partition_id(context, partition_id).await
-    }
-
-    async fn entity_by_partition_id_keys(
-        context: &Context,
-        partition_id: String,
-        keys: Vec<String>,
-    ) -> FieldResult<entity::Entity> {
-        entity::entity_by_partition_id_keys(context, partition_id, keys).await
+        entity::entities(context, partition_id, keys).await
     }
 
     async fn event(context: &Context, id: String) -> FieldResult<Event> {
         event::event(context, id).await
     }
 
-    async fn events_by_keys(context: &Context, keys: Vec<String>) -> FieldResult<Vec<Event>> {
-        event::events_by_keys(context, keys).await
+    async fn events(context: &Context, keys: Vec<String>) -> FieldResult<Vec<Event>> {
+        event::events(context, keys).await
     }
 }
