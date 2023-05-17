4e4523fd01001d477bc8f91e8807bd324d73c5a9
Thomas Belloc <thomas.belloc@gmail.com>
2023-05-17 15:18:35 +0200

    Fix ERC20 build (#209)
    * wip: fix build errors
    
    * Fix some errors
    
    * dev: improvements and fixes
    
    * dev: WIP commit
    
    * dev: improvements
    
    * dev: re-enabling dojo-erc tests
    
    * Extend component to describe schema (#231)
    
    * wip: schema description
    
    * end implementation schema view
    
    * change default len in schema+ use arrayTrait
    
    * fix schema of component
    
    * update system test data
    
    * bind specific dojo-dev image
    
    * dev: truncating component error messages to 31
    
    * Update devcontainer ci to propose devcontainer hash updates
    
    * dev: using fully qualified array path
    
    * fix: formatting
    
    * Use fully qualified path for component schema generation
    
    * Install protostar to devcontainer
    
    * dev: improvements
    
    * fix: missing import
    
    * wip: fix build errors
    
    * Fix some errors
    
    * dev: improvements and fixes
    
    * dev: WIP commit
    
    * dev: improvements
    
    * dev: re-enabling dojo-erc tests
    
    * Extend component to describe schema (#231)
    
    * wip: schema description
    
    * end implementation schema view
    
    * change default len in schema+ use arrayTrait
    
    * fix schema of component
    
    * update system test data
    
    * bind specific dojo-dev image
    
    * dev: truncating component error messages to 31
    
    * Update devcontainer ci to propose devcontainer hash updates
    
    * dev: using fully qualified array path
    
    * fix: formatting
    
    * Use fully qualified path for component schema generation
    
    * Install protostar to devcontainer
    
    * Extend component to describe schema (#231)
    
    * wip: schema description
    
    * end implementation schema view
    
    * change default len in schema+ use arrayTrait
    
    * fix schema of component
    
    * update system test data
    
    * Use fully qualified path for component schema generation
    
    * dev: improvements
    
    * fix: missing import
    
    * wip: fix build errors
    
    * dev: improvements and fixes
    
    * dev: WIP commit
    
    * dev: improvements
    
    * Extend component to describe schema (#231)
    
    * wip: schema description
    
    * end implementation schema view
    
    * change default len in schema+ use arrayTrait
    
    * fix schema of component
    
    * update system test data
    
    * Use fully qualified path for component schema generation
    
    * Extend component to describe schema (#231)
    
    * wip: schema description
    
    * end implementation schema view
    
    * change default len in schema+ use arrayTrait
    
    * fix schema of component
    
    * update system test data
    
    * Use fully qualified path for component schema generation
    
    * dev: improvements
    
    * fix: missing import
    
    * fix typo rebase query.cairo
    
    * remove duplicate
    
    * remove duplicate in erc
    
    * wip fix rebase
    
    * fix system erc
    
    * merge main
    
    * Update for new apis
    
    * fix: erc20 components derives
    
    ---------
    
    Co-authored-by: Tarrence van As <tarrence@vanas.family>
    Co-authored-by: Milan Cermak <milan.cermak@gmail.com>
    Co-authored-by: Milan Cermak <milancermak@users.noreply.github.com>

--- scripts/cairo_test.sh
+++ scripts/cairo_test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -euxo pipefail
 
+cargo +nightly-2022-11-03 run --bin dojo-test -- crates/dojo-core
+cargo +nightly-2022-11-03 run --bin dojo-test -- crates/dojo-erc
+#cargo +nightly-2022-11-03 run --bin dojo-test -- crates/dojo-physics
 cargo +nightly-2022-11-03 run --bin dojo-test -- crates/dojo-core/tests
 cargo +nightly-2022-11-03 run --bin dojo-test -- crates/dojo-defi
-# Uncomment once erc crate passes
-# cargo +nightly-2022-11-03 run --bin dojo-test -- crates/dojo-erc
-# cargo +nightly-2022-11-03 run --bin dojo-test -- crates/dojo-physics
 cargo +nightly-2022-11-03 run --bin dojo-test -- examples
