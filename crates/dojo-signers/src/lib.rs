6fc9e96019b6c15f5e3e374bd7e8cc28b35d801c
Pia <76558220+rkdud007@users.noreply.github.com>
2023-05-22 15:48:03 +0300

    implement fromkeystore

--- crates/dojo-signers/src/lib.rs
+++ crates/dojo-signers/src/lib.rs
@@ -9,11 +9,32 @@ pub trait FromEnv {
         Self: Sized;
 }
 
+pub trait FromKeystore {
+    fn from_keystore() -> anyhow::Result<Self>
+    where
+        Self: Sized;
+}
+
 impl FromEnv for LocalWallet {
     fn from_env() -> anyhow::Result<Self> {
         let private_key_str = env::var("STARK_PRIVATE_KEY")?;
         let private_key = FieldElement::from_hex_be(&private_key_str)?;
 
         Ok(LocalWallet::from_signing_key(SigningKey::from_secret_scalar(private_key)))
+    }
+}
+
+impl FromKeystore for LocalWallet {
+    fn from_keystore<P>(keystore_path: P, password: &str) -> anyhow::Result<Self>
+    where
+        P: AsRef<std::path::Path>,
+    {
+        let private_key_str = SigningKey::from_keystore(keystore_path, password)?;
+        let private_key = FieldElement::from_hex_be(&private_key_str)?;
+
+        Ok(LocalWalletKeystore::from_signing_key(SigningKey::from_keystore(
+            &keystore_path,
+            &password,
+        )?))
     }
 }
