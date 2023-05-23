1be761ebfd588125db556a85a41239e0c97c5588
Tarrence van As <tarrence@vanas.family>
2023-05-23 10:35:42 -0400

    Implement indexer engine
    commit-id:5a7e36da

--- crates/torii/src/indexer.rs
+++ crates/torii/src/indexer.rs
@@ -1,179 +1,126 @@
-use std::cmp::Ordering;
 use std::error::Error;
+use std::sync::Arc;
+use std::time::Duration;
 
 use num::BigUint;
 use sqlx::{Pool, Sqlite};
-use starknet::core::types::FieldElement;
-use starknet::core::utils::starknet_keccak;
+use starknet::core::types::{
+    BlockId, BlockWithTxs, Event, InvokeTransaction, MaybePendingBlockWithTxs,
+    MaybePendingTransactionReceipt, Transaction, TransactionReceipt,
+};
 use starknet::providers::jsonrpc::{JsonRpcClient, JsonRpcTransport};
+use starknet::providers::Provider;
+use tokio::time::sleep;
 use tokio_util::sync::CancellationToken;
-use tracing::{debug, info, warn};
+use tracing::info;
 
-use crate::processors::component_register::ComponentRegistrationProcessor;
-use crate::processors::component_state_update::ComponentStateUpdateProcessor;
-use crate::processors::system_register::SystemRegistrationProcessor;
+// use crate::processors::component_register::ComponentRegistrationProcessor;
+// use crate::processors::component_state_update::ComponentStateUpdateProcessor;
+// use crate::processors::system_register::SystemRegistrationProcessor;
 use crate::processors::{BlockProcessor, EventProcessor, TransactionProcessor};
-pub struct Processors<T> {
-    pub event_processors: Vec<Box<dyn EventProcessor<T>>>,
-    pub block_processors: Vec<Box<dyn BlockProcessor<T>>>,
-    pub transaction_processors: Vec<Box<dyn TransactionProcessor<T>>>,
-}
-
-fn filter_by_processors<T: JsonRpcTransport>(filter: &mut Filter, processors: &Processors<T>) {
-    for processor in &processors.event_processors {
-        let bytes: [u8; 32] = starknet_keccak(processor.event_key().as_bytes()).to_bytes_be();
-
-        filter.events.push(EventFilter {
-            keys: vec![ApibaraFieldElement::from_bytes(&bytes)],
-            ..Default::default()
-        })
-    }
-}
 
 pub async fn start_indexer<T: JsonRpcTransport + Sync + Send>(
-    ct: CancellationToken,
-    world: BigUint,
-    node_uri: Uri,
+    _ct: CancellationToken,
+    _world: BigUint,
     pool: &Pool<Sqlite>,
     provider: &JsonRpcClient<T>,
 ) -> Result<(), Box<dyn Error>> {
     info!("starting indexer");
 
-    let processors = Processors {
-        event_processors: vec![
-            Box::<ComponentStateUpdateProcessor>::default(),
-            Box::<ComponentRegistrationProcessor>::default(),
-            Box::<SystemRegistrationProcessor>::default(),
-        ],
-        block_processors: vec![],
-        transaction_processors: vec![],
-    };
+    let block_processors: Vec<Arc<dyn BlockProcessor<T>>> = vec![];
+    let transaction_processors: Vec<Arc<dyn TransactionProcessor<T>>> = vec![];
+    let event_processors: Vec<Arc<dyn EventProcessor<T>>> = vec![];
 
-    let mut filter = Filter {
-        header: Some(HeaderFilter { weak: true }),
-        transactions: vec![],
-        events: vec![],
-        messages: vec![],
-        state_update: Some(StateUpdateFilter {
-            deployed_contracts: vec![DeployedContractFilter {
-                // we just want to know when our world contract is deployed
-                contract_address: Some(ApibaraFieldElement::from_bytes(
-                    &world.to_bytes_be().try_into().unwrap(),
-                )),
-                ..Default::default()
-            }],
-            ..Default::default()
-        }),
-    };
+    let mut current_block_number: u64 = 0;
 
-    let (mut data_stream, stream_client) =
-        StarknetClientBuilder::default().connect(node_uri).await?;
+    loop {
+        let block_with_txs =
+            match provider.get_block_with_txs(BlockId::Number(current_block_number)).await {
+                Ok(block_with_txs) => block_with_txs,
+                Err(e) => {
+                    eprintln!("Error while fetching block: {}", e);
+                    sleep(Duration::from_secs(60)).await; // If there's an error, wait longer before the next attempt.
+                    continue;
+                }
+            };
 
-    // filter requested data by the events we process
-    filter_by_processors(&mut filter, &processors);
+        let block_with_txs = match block_with_txs {
+            MaybePendingBlockWithTxs::Block(block_with_txs) => block_with_txs,
+            _ => continue,
+        };
 
-    // TODO: should set starting block.
-    let starting_configuration = Configuration::<Filter>::default()
-        .with_finality(DataFinality::DataStatusAccepted)
-        .with_filter(|filter| filter.with_header(HeaderFilter { weak: false }));
-    stream_client.send(starting_configuration).await?;
+        process_block(pool, provider, &block_processors, &block_with_txs).await?;
 
-    // Reduce event processors into a Map that maps event_key to the processor
-    let event_processors = processors
-        .event_processors
-        .into_iter()
-        .map(|processor| (starknet_keccak(processor.event_key().as_bytes()), processor))
-        .collect::<std::collections::HashMap<_, _>>();
+        for transaction in block_with_txs.transactions {
+            let invoke_transaction = match &transaction {
+                Transaction::Invoke(invoke_transaction) => invoke_transaction,
+                _ => continue,
+            };
 
-    // dont process anything until our world is deployed
-    let mut world_deployed = false;
-    while let Some(message) = data_stream.try_next().await? {
-        if ct.is_cancelled() {
-            return Ok(());
-        }
+            let invoke_transaction = match invoke_transaction {
+                InvokeTransaction::V1(invoke_transaction) => invoke_transaction,
+                _ => continue,
+            };
 
-        debug!("Received message");
-        match message {
-            DataMessage::Invalidate { cursor } => {
-                panic!("chain reorganization: {cursor:?}");
-            }
-            DataMessage::Data { batch, .. } => {
-                for block in batch {
-                    match &block.header {
-                        Some(header) => {
-                            info!("Received block {}", header.block_number);
+            let receipt =
+                match provider.get_transaction_receipt(invoke_transaction.transaction_hash).await {
+                    Ok(receipt) => receipt,
+                    _ => continue,
+                };
 
-                            for processor in &processors.block_processors {
-                                processor
-                                    .process(pool, provider, block.clone())
-                                    .await
-                                    .unwrap_or_else(|op| {
-                                        panic!("Failed processing block: {op:?}");
-                                    })
-                            }
-                        }
-                        None => {
-                            warn!("Received block without header");
-                        }
-                    }
+            let receipt = match receipt {
+                MaybePendingTransactionReceipt::Receipt(receipt) => receipt,
+                _ => continue,
+            };
 
-                    // wait for our world contract to be deployed
-                    if !world_deployed {
-                        let state = block.state_update.as_ref();
-                        if state.is_some() && state.unwrap().state_diff.is_some() {
-                            let state_diff = state.unwrap().state_diff.as_ref().unwrap();
-                            for contract in state_diff.deployed_contracts.iter() {
-                                if Ordering::is_eq(
-                                    contract
-                                        .contract_address
-                                        .as_ref()
-                                        .unwrap()
-                                        .to_biguint()
-                                        .cmp(&world),
-                                ) {
-                                    world_deployed = true;
-                                    break;
-                                }
-                            }
-                        }
+            process_transaction(pool, provider, &transaction_processors, &receipt.clone()).await?;
 
-                        if !world_deployed {
-                            continue;
-                        }
-                    }
-
-                    for transaction in &block.transactions {
-                        match &transaction.receipt {
-                            Some(_tx) => {
-                                for processor in &processors.transaction_processors {
-                                    processor
-                                        .process(pool, provider, transaction.clone())
-                                        .await
-                                        .unwrap_or_else(|op| {
-                                            panic!("Failed processing transaction: {op:?}");
-                                        })
-                                }
-                            }
-                            None => {}
-                        }
-                    }
-
-                    for event_w_tx in block.events {
-                        let event = event_w_tx.clone().event.unwrap();
-                        let event_key = event.keys[0].to_biguint();
-                        if let Some(processor) = event_processors.get(
-                            &FieldElement::from_byte_slice_be(&event_key.to_bytes_be()).unwrap(),
-                        ) {
-                            processor.process(pool, provider, event_w_tx).await.unwrap_or_else(
-                                |op| {
-                                    panic!("Failed processing event: {op:?}");
-                                },
-                            );
-                        }
-                    }
+            if let TransactionReceipt::Invoke(invoke_receipt) = receipt.clone() {
+                for event in &invoke_receipt.events {
+                    process_event(pool, provider, &event_processors, &receipt, event).await?;
                 }
             }
         }
+
+        current_block_number += 1;
+        sleep(Duration::from_secs(15)).await;
+    }
+}
+
+async fn process_block<T: starknet::providers::jsonrpc::JsonRpcTransport>(
+    pool: &Pool<Sqlite>,
+    provider: &JsonRpcClient<T>,
+    processors: &[Arc<dyn BlockProcessor<T>>],
+    block: &BlockWithTxs,
+) -> Result<(), Box<dyn Error>> {
+    for processor in processors {
+        processor.process(pool, provider, block).await?;
+    }
+    Ok(())
+}
+
+async fn process_transaction<T: starknet::providers::jsonrpc::JsonRpcTransport>(
+    pool: &Pool<Sqlite>,
+    provider: &JsonRpcClient<T>,
+    processors: &[Arc<dyn TransactionProcessor<T>>],
+    receipt: &TransactionReceipt,
+) -> Result<(), Box<dyn Error>> {
+    for processor in processors {
+        processor.process(pool, provider, receipt).await?;
+    }
+
+    Ok(())
+}
+
+async fn process_event<T: starknet::providers::jsonrpc::JsonRpcTransport>(
+    pool: &Pool<Sqlite>,
+    provider: &JsonRpcClient<T>,
+    processors: &[Arc<dyn EventProcessor<T>>],
+    _receipt: &TransactionReceipt,
+    event: &Event,
+) -> Result<(), Box<dyn Error>> {
+    for processor in processors {
+        processor.process(pool, provider, event).await?;
     }
 
     Ok(())
@@ -190,6 +137,5 @@ pub async fn start_indexer<T: JsonRpcTransport + Sync + Send>(
 //     let world = BigUint::from(0x1234567890);
 //     let provider = JsonRpcClient::new(HttpTransport::new(Uri::parse(rpc_url)));
 
-//     start_apibara(ct, rpc_url.into());
 //     start_indexer(ct, world, Uri::from_str("http://localhost:7171").unwrap(), pool, &provider)
 // }
