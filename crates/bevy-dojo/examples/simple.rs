933df330592607f29cb04fd8ac3676e552a50023
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2023-05-27 03:18:14 +0000

    unused importS

--- crates/bevy-dojo/examples/simple.rs
+++ crates/bevy-dojo/examples/simple.rs
@@ -3,7 +3,7 @@ use bevy::ecs::system::Query;
 use bevy::log;
 use bevy::log::LogPlugin;
 use bevy_dojo::apibara::core::node::v1alpha2::DataFinality;
-use bevy_dojo::apibara::core::starknet::v1alpha2::{Block, FieldElement, Filter, HeaderFilter};
+use bevy_dojo::apibara::core::starknet::v1alpha2::{Block, Filter};
 use bevy_dojo::apibara::sdk::{Configuration, DataMessage};
 use bevy_dojo::{IndexerMessage, IndexerPlugin};
 use chrono::{DateTime, Utc};
