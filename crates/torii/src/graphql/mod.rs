902ed0495c2016ccfdd41ea3e5a9fb26f45c332e
broody <broody@gmail.com>
2023-05-29 17:29:51 -0700

    chore(torii): better readability and err handling

--- crates/torii/src/graphql/mod.rs
+++ crates/torii/src/graphql/mod.rs
@@ -11,41 +11,46 @@ use async_graphql::dynamic::{Field, FieldFuture, Object, TypeRef};
 use async_graphql::Value;
 use indexmap::IndexMap;
 
+// Type aliases for GraphQL fields
 pub type TypeMapping = IndexMap<String, String>;
 pub type ValueMapping = IndexMap<String, Value>;
 
 pub trait ObjectTraitStatic {
     fn new() -> Self;
-
     fn from(field_type_mapping: TypeMapping) -> Self;
 }
 
 pub trait ObjectTraitInstance {
     fn name(&self) -> &str;
-
     fn type_name(&self) -> &str;
-
     fn field_type_mapping(&self) -> &TypeMapping;
-
     fn field_resolvers(&self) -> Vec<Field>;
 
-    // creates the graphql object based on the provided field type mapping
+    // Create a new GraphQL object
     fn create(&self) -> Object {
-        (*self.field_type_mapping()).iter().fold(
-            Object::new(self.type_name()),
-            |obj, (field_name, field_type)| {
-                let inner_name = field_name.clone();
+        let mut object = Object::new(self.type_name());
 
-                obj.field(Field::new(field_name, TypeRef::named_nn(field_type), move |ctx| {
-                    let field_name = inner_name.clone();
-
-                    FieldFuture::new(async move {
-                        let mapping = ctx.parent_value.try_downcast_ref::<ValueMapping>()?;
-                        let value = mapping.get(field_name.as_str()).expect("field not found");
-                        Ok(Some(value.clone()))
-                    })
-                }))
-            },
-        )
+        for (field_name, field_type) in self.field_type_mapping() {
+            let field = create_field(field_name, field_type);
+            object = object.field(field);
+        }
+        object
     }
+}
+
+fn create_field(name: &str, field_type: &str) -> Field {
+    let outer_name = name.to_owned();
+
+    Field::new(name, TypeRef::named_nn(field_type), move |ctx| {
+        let inner_name = outer_name.to_owned();
+
+        FieldFuture::new(async move {
+            let mapping = ctx.parent_value.try_downcast_ref::<ValueMapping>()?;
+
+            match mapping.get(inner_name.as_str()) {
+                Some(value) => Ok(Some(value.clone())),
+                None => Ok(Some(Value::Null)),
+            }
+        })
+    })
 }
