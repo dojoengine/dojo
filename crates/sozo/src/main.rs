cdc9e659b73f997c5052b031b905a1faf19c6e45
Ammar Arif <evergreenkary@gmail.com>
2023-05-30 14:08:19 +0800

    Default to only output logs from `sozo`

--- crates/sozo/src/main.rs
+++ crates/sozo/src/main.rs
@@ -13,7 +13,7 @@ use cli::{App, Commands};
 
 // #[tokio::main]
 fn main() {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    env_logger::Builder::from_env(Env::default().default_filter_or("sozo=info")).init();
 
     let cli = App::parse();
 
