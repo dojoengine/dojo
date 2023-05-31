92684dfac55f5056bcd6879ae06567ff9245fefa
Tarrence van As <tarrence@vanas.family>
2023-05-31 13:48:28 -0400

    Move ecs example into folder

--- scripts/cairo_test.sh
+++ scripts/cairo_test.sh
@@ -6,4 +6,4 @@ cargo +nightly-2023-05-28 run --bin dojo-test -- crates/dojo-erc
 #cargo +nightly-2023-05-28 run --bin dojo-test -- crates/dojo-physics
 cargo +nightly-2023-05-28 run --bin dojo-test -- crates/dojo-core/tests
 # cargo +nightly-2023-05-28 run --bin dojo-test -- crates/dojo-defi
-cargo +nightly-2023-05-28 run --bin dojo-test -- examples
+cargo +nightly-2023-05-28 run --bin dojo-test -- examples/ecs
