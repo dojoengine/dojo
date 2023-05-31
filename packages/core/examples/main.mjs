499ddb8a01345f6581cb925b36a38d86a4e3682c
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2023-05-31 19:13:57 +1000

    sdk work

--- packages/core/examples/main.mjs
+++ packages/core/examples/main.mjs
@@ -1,7 +1,7 @@
 import { RPCProvider } from '../dist/provider/RPCProvider.js';
 import { HotAccount } from '../dist/account/index.js'
 
-const WorldContractAddress = '0x2a79e6863214cfb96bdcb42b70eb39cdb74dd7787d2b1e792b673600892eeb2';
+const WorldContractAddress = '0x3f4b4b87bb1c2e6ce758ab610fa2b73cbd1afe554d1d70701b1545b1f29220c';
 const address = "0x06f62894bfd81d2e396ce266b2ad0f21e0668d604e5bb1077337b6d570a54aea"
 const privateKey = "0x07230b49615d175307d580c33d6fda61fc7b9aec91df0f5c1a5ebe3b8cbfee02"
 
@@ -35,12 +35,12 @@ const main = async () => {
         console.error('An error occurred:', error);
     }
 
-    try {
-        const response = await rpcProvider.component('Position');
-        console.log('Response:', response);
-    } catch (error) {
-        console.error('An error occurred:', error);
-    }
+    // try {
+    //     const response = await rpcProvider.component('Position');
+    //     console.log('Response:', response);
+    // } catch (error) {
+    //     console.error('An error occurred:', error);
+    // }
 };
 
 main();
