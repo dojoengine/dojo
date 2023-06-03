849574eb40e6273296f79e3fe3e8e3952bc7b978
Tarrence van As <tarrence@vanas.family>
2023-06-03 12:10:37 -0400

    Implement sozo test

--- scripts/cairo_test.sh
+++ scripts/cairo_test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -euxo pipefail
 
-cargo +nightly-2023-05-28 run --bin dojo-test -- crates/dojo-core
-cargo +nightly-2023-05-28 run --bin dojo-test -- crates/dojo-erc
-#cargo +nightly-2023-05-28 run --bin dojo-test -- crates/dojo-physics
-cargo +nightly-2023-05-28 run --bin dojo-test -- crates/dojo-core/tests
-# cargo +nightly-2023-05-28 run --bin dojo-test -- crates/dojo-defi
-cargo +nightly-2023-05-28 run --bin dojo-test -- examples/ecs
+cargo +nightly-2023-05-28 run --bin sozo -- test crates/dojo-core
+cargo +nightly-2023-05-28 run --bin sozo -- test crates/dojo-erc
+#cargo +nightly-2023-05-28 run --bin sozo -- test crates/dojo-physics
+cargo +nightly-2023-05-28 run --bin sozo -- test crates/dojo-core/tests
+# cargo +nightly-2023-05-28 run --bin sozo -- test crates/dojo-defi
+cargo +nightly-2023-05-28 run --bin sozo -- test examples/ecs
