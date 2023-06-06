43e55fbd054552c0355368045c18ed3f8a26e8df
Tarrence van As <tarrence@vanas.family>
2023-06-06 17:04:17 -0400

    Fix katana getEvents filter key matching

--- crates/dojo-world/src/manifest_test.rs
+++ crates/dojo-world/src/manifest_test.rs
@@ -4,7 +4,7 @@ use starknet::core::types::FieldElement;
 use starknet::providers::jsonrpc::{JsonRpcClient, JsonRpcMethod};
 
 use super::Manifest;
-use crate::manifest::{Contract, ManifestError, EXECUTOR_ADDRESS_SLOT};
+use crate::manifest::ManifestError;
 
 #[tokio::test]
 async fn test_manifest_from_remote_throw_error_on_not_deployed() {
@@ -30,60 +30,4 @@ async fn test_manifest_from_remote_throw_error_on_not_deployed() {
         }
         err => panic!("Unexpected error: {err}"),
     }
-}
-
-#[tokio::test]
-async fn test_manifest_loads_empty_world_from_mock_remote() {
-    let world_address = FieldElement::ONE;
-    let world_class_hash = FieldElement::TWO;
-    let executor_address = FieldElement::THREE;
-    let executor_class_hash: FieldElement = FieldElement::from_hex_be("0x4").unwrap();
-
-    let mut mock_transport = MockJsonRpcTransport::new();
-    mock_transport.set_response(
-        JsonRpcMethod::GetClassHashAt,
-        json!(["pending", format!("{world_address:#x}")]),
-        json!({
-            "id": 1,
-            "result": format!("{world_class_hash:#x}")
-        }),
-    );
-
-    mock_transport.set_response(
-        JsonRpcMethod::GetStorageAt,
-        json!(["0x1", format!("{EXECUTOR_ADDRESS_SLOT:#x}"), "pending"]),
-        json!({
-            "id": 1,
-            "result": format!("{executor_address:#x}")
-        }),
-    );
-
-    mock_transport.set_response(
-        JsonRpcMethod::GetClassHashAt,
-        json!(["pending", format!("{executor_address:#x}")]),
-        json!({
-            "id": 1,
-            "result": format!("{executor_class_hash:#x}")
-        }),
-    );
-
-    let rpc = JsonRpcClient::new(mock_transport);
-    let manifest = Manifest::from_remote(rpc, FieldElement::ONE, None).await.unwrap();
-
-    assert_eq!(
-        manifest,
-        Manifest {
-            world: Contract {
-                name: "World".into(),
-                class_hash: world_class_hash,
-                address: Some(world_address)
-            },
-            executor: Contract {
-                name: "Executor".into(),
-                class_hash: executor_class_hash,
-                address: Some(executor_address)
-            },
-            ..Manifest::default()
-        }
-    )
 }
