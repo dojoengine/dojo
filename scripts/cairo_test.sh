324e112c3053c55367ccb8f749704861c66b17ef
Ammar Arif <evergreenkary@gmail.com>
2023-06-10 22:31:44 +0800

    fix cairo test scripts

--- scripts/cairo_test.sh
+++ scripts/cairo_test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -euxo pipefail
 
-cargo +nightly-2023-05-28 run --bin sozo -- test crates/dojo-core
-cargo +nightly-2023-05-28 run --bin sozo -- test crates/dojo-erc
+cargo +nightly-2023-05-28 run --bin sozo -- --manifest-path crates/dojo-core/Scarb.toml test
+cargo +nightly-2023-05-28 run --bin sozo -- --manifest-path crates/dojo-erc/Scarb.toml test
 #cargo +nightly-2023-05-28 run --bin sozo -- test crates/dojo-physics
-cargo +nightly-2023-05-28 run --bin sozo -- test crates/dojo-core/tests
+cargo +nightly-2023-05-28 run --bin sozo -- --manifest-path crates/dojo-core/tests/Scarb.toml test
 # cargo +nightly-2023-05-28 run --bin sozo -- test crates/dojo-defi
-cargo +nightly-2023-05-28 run --bin sozo -- test examples/ecs
+cargo +nightly-2023-05-28 run --bin sozo -- --manifest-path examples/ecs/Scarb.toml test
