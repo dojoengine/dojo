7c68be63e2ed982020ab38dc705ad5cf278e29e5
Jonathan LEI <xJonathan@outlook.com>
2023-06-13 01:20:09 +0800

    fix: param name in starknet_addDeclareTransaction (#483)
    The param name should be `declare_transaction` instead of `transaction`.
    This affects applications that send params as object instead of array.

--- crates/katana-rpc/src/starknet/rpc.rs
+++ crates/katana-rpc/src/starknet/rpc.rs
@@ -857,11 +857,11 @@ impl<S: Sequencer + Send + Sync + 'static> StarknetApiServer for StarknetRpc<S> 
 
     async fn add_declare_transaction(
         &self,
-        transaction: BroadcastedDeclareTransaction,
+        declare_transaction: BroadcastedDeclareTransaction,
     ) -> Result<DeclareTransactionResult, Error> {
         let chain_id = FieldElement::from_hex_be(&self.sequencer.chain_id().await.as_hex())
             .map_err(|_| Error::from(StarknetApiError::InternalServerError))?;
-        let (transaction_hash, class_hash, transaction) = match transaction {
+        let (transaction_hash, class_hash, transaction) = match declare_transaction {
             BroadcastedDeclareTransaction::V1(tx) => {
                 let raw_class_str = serde_json::to_string(&tx.contract_class)
                     .map_err(|_| Error::from(StarknetApiError::InvalidContractClass))?;
