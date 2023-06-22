51bb31d305a04f7cfab6551b56cb9eb9f21938c8
aymericdelab <aymericdelabrousse@gmail.com>
2023-06-23 00:26:39 +0200

    feat: sync contract + client and update react example

--- packages/examples/react-example/src/App.tsx
+++ packages/examples/react-example/src/App.tsx
@@ -1,45 +1,36 @@
-import { useEffect } from 'react';
-import './App.css'
-import { useDojo } from './DojoContext'
+import './App.css';
+import { useDojo } from './DojoContext';
 import { useComponentValue } from "@dojoengine/react";
-import { Query } from '@dojoengine/core';
+import { Direction } from './dojo/createSystemCalls'
+import { Utils } from '@dojoengine/core';
 
 function App() {
-
   const {
-    systemCalls: { spawn },
-    components: { Position, Moves },
-    network: { world, signer, entity }
-  } = useDojo()
+    systemCalls: { spawn, move },
+    components: { Moves, Position },
+  } = useDojo();
 
-  // world.registerEntity({ id: 1 as any })
-
-  const query: Query = { address_domain: "0", partition: "0", keys: [BigInt(signer.address)] }
-
-  async function getEntity() {
-    try {
-      const va = await entity(Position.metadata.name, query);
-      return va
-    } catch (e) {
-      console.log(e)
-    } finally {
-      console.log('done')
-    }
-  }
-
-  useEffect(() => {
-    getEntity().then(va => console.log(va));
-  }, [])
+  const entityId = BigInt('0x06f62894bfd81d2e396ce266b2ad0f21e0668d604e5bb1077337b6d570a54aea');
+  const position = useComponentValue(Position, Utils.getEntityIdFromKeys([entityId]));
+  const moves = useComponentValue(Moves, Utils.getEntityIdFromKeys([entityId]));
 
   return (
     <>
       <div className="card">
-        <button onClick={() => spawn()}>
-          spawn
-        </button>
+        <button onClick={() => spawn()}>Spawn</button>
+      </div>
+      <div className="card">
+        {/* 3 == move up */}
+        <button onClick={() => move(Direction.Up)}>Move Up</button>
+      </div>
+      <div className="card">
+        <div>Moves Left: {moves ? `${moves['remaining']}` : 'Need to Spawn'}</div>
+      </div>
+      <div className="card">
+        <div>Position: {position ? `${position['x']}, ${position['y']}` : 'Need to Spawn'}</div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
