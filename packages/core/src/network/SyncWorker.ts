dab0367c4d8f6cb3a5c349d500ab6d067aea085c
raschel <38816784+aymericdelab@users.noreply.github.com>
2023-06-24 02:12:40 +0200

    feat: sync contract + client and update react example (#545)
    * feat: sync contract + client and update react example
    
    * fix: fix inital sync

---
+++ packages/core/src/network/SyncWorker.ts
@@ -0,0 +1,44 @@
+import { Components } from "@latticexyz/recs";
+import { Providers } from "..";
+import { setComponentFromEntitiesQuery, setComponentFromEvent } from "../utils";
+import { Event } from "starknet";
+
+export class SyncWorker<C extends Components> {
+  private provider: Providers.RPCProvider;
+  private components: C;
+  private event_key: String;
+
+
+  constructor(provider: Providers.RPCProvider, components: C, event_key: String) {
+    console.log("Creating SyncWorker...");
+    this.provider = provider;
+    this.components = components;
+    this.event_key = event_key;
+    this.init();
+  }
+
+  private async init() {
+   for (const key of Object.keys(this.components)) {
+        const component = this.components[key];
+        if (component.metadata && component.metadata.name) {
+            // call provider.entities for each component to get all entities linked to that component
+            const entities = await this.provider.entities(component.metadata.name as string, "0");
+            setComponentFromEntitiesQuery(component, entities);
+            }
+        }
+    console.log('SyncWorker initialized');
+    }
+
+    public async sync(txHash: string) {
+        this.provider.provider.getTransactionReceipt(txHash).then((receipt) => {
+            receipt.events.filter((event) => {
+                return event.keys.length === 1 &&
+                event.keys[0] === this.event_key;
+        }
+        ).map((event: Event) => {
+            setComponentFromEvent(this.components, event.data);
+            });
+
+        })
+    }
+}
