018045e2ab45a9543e43a3cf7c376cc2233fdc8d
Kariy <ammr.arf@gmail.com>
2023-07-04 01:57:32 +0300

    refactor(katana-rpc): simplify server spawning
    commit-id:0c4cd667

--- crates/katana/rpc/src/lib.rs
+++ crates/katana/rpc/src/lib.rs
@@ -5,70 +5,68 @@ mod starknet;
 mod utils;
 
 use std::net::SocketAddr;
-use std::sync::Arc;
+use std::time::Instant;
 
 use anyhow::Result;
 use config::ServerConfig;
 use hyper::Method;
+use jsonrpsee::server::logger::{Logger, MethodKind, TransportProtocol};
 use jsonrpsee::server::{AllowHosts, ServerBuilder, ServerHandle};
+use jsonrpsee::tracing::debug;
+use jsonrpsee::types::Params;
+use jsonrpsee::RpcModule;
 use katana_core::sequencer::Sequencer;
 use tower_http::cors::{Any, CorsLayer};
 
 use crate::api::katana::KatanaApiServer;
 use crate::api::starknet::StarknetApiServer;
-use crate::katana::KatanaApi;
-use crate::starknet::StarknetApi;
+pub use crate::katana::KatanaApi;
+pub use crate::starknet::StarknetApi;
 
-#[derive(Debug, Clone)]
-pub struct KatanaNodeRpc<S> {
-    pub config: ServerConfig,
-    pub sequencer: Arc<S>,
-}
-
-impl<S> KatanaNodeRpc<S>
+pub async fn spawn<S>(
+    katana_api: KatanaApi<S>,
+    starknet_api: StarknetApi<S>,
+    config: ServerConfig,
+) -> Result<NodeHandle>
 where
     S: Sequencer + Send + Sync + 'static,
 {
-    pub fn new(sequencer: Arc<S>, config: ServerConfig) -> Self {
-        Self { config, sequencer }
-    }
+    let mut methods = RpcModule::new(());
+    methods.merge(starknet_api.into_rpc())?;
+    methods.merge(katana_api.into_rpc())?;
 
-    pub async fn run(self) -> Result<(SocketAddr, ServerHandle)> {
-        let mut methods = KatanaApi::new(self.sequencer.clone()).into_rpc();
-        methods.merge(StarknetApi::new(self.sequencer.clone()).into_rpc())?;
-
-        let cors = CorsLayer::new()
+    let cors = CorsLayer::new()
             // Allow `POST` when accessing the resource
             .allow_methods([Method::POST])
             // Allow requests from any origin
             .allow_origin(Any)
             .allow_headers([hyper::header::CONTENT_TYPE]);
-        let middleware = tower::ServiceBuilder::new().layer(cors);
+    let middleware = tower::ServiceBuilder::new().layer(cors);
 
-        let server = ServerBuilder::new()
-            .set_logger(KatanaNodeRpcLogger)
-            .set_host_filtering(AllowHosts::Any)
-            .set_middleware(middleware)
-            .build(self.config.addr())
-            .await?;
+    let server = ServerBuilder::new()
+        .set_logger(RpcLogger)
+        .set_host_filtering(AllowHosts::Any)
+        .set_middleware(middleware)
+        .build(config.addr())
+        .await?;
 
-        let addr = server.local_addr()?;
-        let handle = server.start(methods)?;
+    let addr = server.local_addr()?;
+    let handle = server.start(methods)?;
 
-        Ok((addr, handle))
-    }
+    Ok(NodeHandle { config, handle, addr })
 }
 
-use std::time::Instant;
-
-use jsonrpsee::server::logger::{Logger, MethodKind, TransportProtocol};
-use jsonrpsee::tracing::debug;
-use jsonrpsee::types::Params;
+#[derive(Debug, Clone)]
+pub struct NodeHandle {
+    pub addr: SocketAddr,
+    pub config: ServerConfig,
+    pub handle: ServerHandle,
+}
 
 #[derive(Debug, Clone)]
-pub struct KatanaNodeRpcLogger;
+pub struct RpcLogger;
 
-impl Logger for KatanaNodeRpcLogger {
+impl Logger for RpcLogger {
     type Instant = std::time::Instant;
 
     fn on_connect(
