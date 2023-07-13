5ed6cc42bc9c03f1d07f6acf63acfc6ab0c5c0e0
aymericdelab <aymericdelabrousse@gmail.com>
2023-07-13 23:34:29 +0200

    feat: added sync through  indexer

--- packages/core/src/network/SyncWorker.ts
+++ packages/core/src/network/SyncWorker.ts
@@ -1,19 +1,26 @@
 import { Components } from "@latticexyz/recs";
 import { Providers } from "..";
-import { setComponentFromEntitiesQuery, setComponentFromEvent } from "../utils";
+import { setComponentFromEntitiesGraphqlQuery, setComponentFromEntitiesQuery, setComponentFromEvent } from "../utils";
 import { Event } from "starknet";
+import { getEntities } from "./graphql";
 
 export class SyncWorker<C extends Components> {
   private provider: Providers.RPCProvider;
   private components: C;
-  private event_key: String;
+  private event_key: string;
+  private url?: string;
+  private useIndexer?: boolean;
+  private waitForTx?: boolean;
 
 
-  constructor(provider: Providers.RPCProvider, components: C, event_key: String) {
+  constructor(provider: Providers.RPCProvider, components: C, event_key: string, url: string, useIndexer: boolean = false, waitForTx: boolean = true) {
     console.log("Creating SyncWorker...");
     this.provider = provider;
     this.components = components;
     this.event_key = event_key;
+    this.url = url;
+    this.useIndexer = useIndexer;
+    this.waitForTx = waitForTx;
     this.init();
   }
 
@@ -21,15 +28,24 @@ export class SyncWorker<C extends Components> {
    for (const key of Object.keys(this.components)) {
         const component = this.components[key];
         if (component.metadata && component.metadata.name) {
-            // call provider.entities for each component to get all entities linked to that component
-            const entities = await this.provider.entities(component.metadata.name as string, "0", Object.keys(component.schema).length);
-            setComponentFromEntitiesQuery(component, entities);
+            if (this.useIndexer && this.url) {
+                const entities = await getEntities(this.url, component.metadata.name as string, component.schema);
+                entities && setComponentFromEntitiesGraphqlQuery(component, entities);
+            } else {
+                // call provider.entities for each component to get all entities linked to that component
+                const entities = await this.provider.entities(component.metadata.name as string, "0", Object.keys(component.schema).length);
+                setComponentFromEntitiesQuery(component, entities);
+            }
             }
         }
     console.log('SyncWorker initialized');
     }
 
+
     public async sync(txHash: string) {
+        if (this.waitForTx) {
+            await this.provider.provider.waitForTransaction(txHash);
+        }
         this.provider.provider.getTransactionReceipt(txHash).then((receipt) => {
             receipt.events.filter((event) => {
                 return event.keys.length === 1 &&
