ade5da061efb68726955e575bc955c4f0eb17355
Kariy <ammr.arf@gmail.com>
2023-07-24 23:05:36 +0800

    refactor(katana-core): add in memory state manager

--- crates/katana/core/src/backend/storage/mod.rs
+++ crates/katana/core/src/backend/storage/mod.rs
@@ -1,13 +1,68 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, VecDeque};
 
 use blockifier::block_context::BlockContext;
 use starknet::core::types::{BlockId, BlockTag, FieldElement};
 
 use self::{block::Block, transaction::KnownTransaction};
+use super::state::MemDb;
 use crate::backend::storage::block::PartialHeader;
 
 pub mod block;
 pub mod transaction;
+
+/// Represents the complete state of a single block
+pub struct InMemoryBlockStates {
+    /// The states at a certain block
+    states: HashMap<FieldElement, MemDb>,
+    /// How many states to store at most
+    in_memory_limit: usize,
+    /// minimum amount of states we keep in memory
+    min_in_memory_limit: usize,
+    /// all states present, used to enforce `in_memory_limit`
+    present: VecDeque<FieldElement>,
+}
+
+impl InMemoryBlockStates {
+    pub fn new(limit: usize) -> Self {
+        Self {
+            states: Default::default(),
+            in_memory_limit: limit,
+            min_in_memory_limit: limit.min(10),
+            present: Default::default(),
+        }
+    }
+
+    /// Inserts a new (hash -> state) pair
+    ///
+    /// When the configured limit for the number of states that can be stored in memory is reached,
+    /// the oldest state is removed.
+    ///
+    /// Since we keep a snapshot of the entire state as history, the size of the state will increase
+    /// with the transactions processed. To counter this, we gradually decrease the cache limit with
+    /// the number of states/blocks until we reached the `min_limit`.
+    pub fn insert(&mut self, hash: FieldElement, state: MemDb) {
+        if self.present.len() >= self.in_memory_limit {
+            // once we hit the max limit we gradually decrease it
+            self.in_memory_limit =
+                self.in_memory_limit.saturating_sub(1).max(self.min_in_memory_limit);
+        }
+
+        self.enforce_limits();
+        self.states.insert(hash, state);
+        self.present.push_back(hash);
+    }
+
+    /// Enforces configured limits
+    fn enforce_limits(&mut self) {
+        // enforce memory limits
+        while self.present.len() >= self.in_memory_limit {
+            // evict the oldest block in memory
+            if let Some(hash) = self.present.pop_front() {
+                self.states.remove(&hash);
+            }
+        }
+    }
+}
 
 // TODO: can we wrap all the fields in a `RwLock` to prevent read blocking?
 #[derive(Debug, Default)]
@@ -61,5 +116,30 @@ impl BlockchainStorage {
             BlockId::Hash(hash) => Some(hash),
             BlockId::Number(num) => self.hashes.get(&num).copied(),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+
+    use super::*;
+
+    #[test]
+    fn remove_old_state_when_limit_is_reached() {
+        let mut in_memory_state = InMemoryBlockStates::new(2);
+
+        in_memory_state.insert(FieldElement::from_str("0x1").unwrap(), MemDb::default());
+        in_memory_state.insert(FieldElement::from_str("0x2").unwrap(), MemDb::default());
+        assert!(in_memory_state.states.get(&FieldElement::from_str("0x1").unwrap()).is_some());
+        assert!(in_memory_state.states.get(&FieldElement::from_str("0x2").unwrap()).is_some());
+        assert_eq!(in_memory_state.present.len(), 2);
+
+        in_memory_state.insert(FieldElement::from_str("0x3").unwrap(), MemDb::default());
+
+        assert_eq!(in_memory_state.present.len(), 2);
+        assert!(in_memory_state.states.get(&FieldElement::from_str("0x1").unwrap()).is_none());
+        assert!(in_memory_state.states.get(&FieldElement::from_str("0x2").unwrap()).is_some());
+        assert!(in_memory_state.states.get(&FieldElement::from_str("0x3").unwrap()).is_some());
     }
 }
