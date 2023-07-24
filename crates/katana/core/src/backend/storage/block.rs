8028cae3ff6aa8b6941de92a5e6894bd01c952f3
Kariy <ammr.arf@gmail.com>
2023-07-24 22:00:33 +0800

    refactor(katana-core): add partial header

--- crates/katana/core/src/backend/storage/block.rs
+++ crates/katana/core/src/backend/storage/block.rs
@@ -2,17 +2,38 @@ use starknet::core::types::{BlockStatus, FieldElement};
 
 use super::transaction::{IncludedTransaction, TransactionOutput};
 
+/// Header of a pending block
+#[derive(Debug)]
+pub struct PartialHeader {
+    pub parent_hash: FieldElement,
+    pub number: u64,
+    pub gas_price: u128,
+    pub timestamp: u64,
+    pub sequencer_address: FieldElement,
+}
+
 #[derive(Debug, Clone)]
 pub struct Header {
     pub parent_hash: FieldElement,
     pub number: u64,
     pub gas_price: u128,
-    pub timestamp: u128,
+    pub timestamp: u64,
     pub state_root: FieldElement,
     pub sequencer_address: FieldElement,
 }
 
 impl Header {
+    pub fn new(partial_header: PartialHeader, state_root: FieldElement) -> Self {
+        Self {
+            state_root,
+            number: partial_header.number,
+            gas_price: partial_header.gas_price,
+            timestamp: partial_header.timestamp,
+            parent_hash: partial_header.parent_hash,
+            sequencer_address: partial_header.sequencer_address,
+        }
+    }
+
     pub fn hash(&self) -> FieldElement {
         unimplemented!("compute the block hash from its header")
     }
@@ -24,4 +45,22 @@ pub struct Block {
     pub status: BlockStatus,
     pub transactions: Vec<IncludedTransaction>,
     pub outputs: Vec<TransactionOutput>,
+}
+
+impl Block {
+    pub fn new(
+        partial_header: PartialHeader,
+        transactions: Vec<IncludedTransaction>,
+        outputs: Vec<TransactionOutput>,
+    ) -> Self {
+        // TODO: compute state root
+        let state_root = FieldElement::ZERO;
+
+        Self {
+            header: Header::new(partial_header, state_root),
+            status: BlockStatus::AcceptedOnL2,
+            transactions,
+            outputs,
+        }
+    }
 }
