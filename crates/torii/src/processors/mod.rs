3d34ec24461093b27a3d6e6408915a90cd319aa2
Pia <76558220+rkdud007@users.noreply.github.com>
2023-08-05 19:56:31 +0800

    implement StoreSetRecordStateDiffProcessor

--- crates/torii/src/processors/mod.rs
+++ crates/torii/src/processors/mod.rs
@@ -1,13 +1,28 @@
 use anyhow::{Error, Result};
 use async_trait::async_trait;
-use starknet::core::types::{BlockWithTxs, Event, TransactionReceipt};
+use starknet::core::types::{BlockId, BlockWithTxs, Event, StateDiff, TransactionReceipt};
 use starknet::providers::jsonrpc::{JsonRpcClient, JsonRpcTransport};
+use starknet_crypto::FieldElement;
 
 use crate::state::State;
 
 pub mod register_component;
 pub mod register_system;
 pub mod store_set_record;
+pub mod store_set_record_state_diff;
+
+#[async_trait]
+pub trait StateDiffProcessor<S: State> {
+    async fn process(
+        &self,
+        storage: &S,
+        component: String,
+        world: FieldElement,
+        length: usize,
+        keys: Vec<FieldElement>,
+        state_diff: &StateDiff,
+    ) -> Result<(), Error>;
+}
 
 #[async_trait]
 pub trait EventProcessor<S: State, T: JsonRpcTransport> {
