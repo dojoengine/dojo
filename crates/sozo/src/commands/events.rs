6db6d6c354029963d9978609ca14e205f1231a7b
Kariy <ammr.arf@gmail.com>
2023-08-05 16:46:52 +0800

    fmt

--- crates/sozo/src/commands/events.rs
+++ crates/sozo/src/commands/events.rs
@@ -40,7 +40,7 @@ impl EventsArgs {
     pub fn run(self, config: &Config) -> Result<()> {
         let env_metadata = if config.manifest_path().exists() {
             let ws = scarb::ops::read_workspace(config.manifest_path(), config)?;
-            
+
             // TODO: Check the updated scarb way to read profile specific values
             dojo_metadata_from_workspace(&ws).and_then(|inner| inner.env().cloned())
         } else {
