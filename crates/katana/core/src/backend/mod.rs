723d36d5ab4d3114cf1a3defe371848c131514af
Kariy <ammr.arf@gmail.com>
2023-08-13 14:04:28 +0800

    refactor(katana): add unified tx type to simplify conversion

--- crates/katana/core/src/backend/mod.rs
+++ crates/katana/core/src/backend/mod.rs
@@ -12,11 +12,11 @@ use blockifier::state::state_api::State;
 use blockifier::transaction::account_transaction::AccountTransaction;
 use blockifier::transaction::errors::TransactionExecutionError;
 use blockifier::transaction::objects::AccountTransactionContext;
-use blockifier::transaction::transaction_execution::Transaction;
+use blockifier::transaction::transaction_execution::Transaction as ExecutionTransaction;
 use flate2::write::GzEncoder;
 use flate2::Compression;
 use parking_lot::RwLock;
-use starknet::core::types::{BlockId, BlockTag, FeeEstimate, FieldElement};
+use starknet::core::types::{BlockId, BlockTag, FeeEstimate};
 use starknet_api::block::BlockTimestamp;
 use starknet_api::core::{ContractAddress, EntryPointSelector};
 use starknet_api::hash::StarkFelt;
@@ -34,7 +34,7 @@ pub mod storage;
 use self::config::StarknetConfig;
 use self::executor::{execute_transaction, PendingBlockExecutor};
 use self::storage::block::{Block, PartialHeader};
-use self::storage::transaction::{IncludedTransaction, KnownTransaction, TransactionStatus};
+use self::storage::transaction::{IncludedTransaction, Transaction, TransactionStatus};
 use self::storage::{BlockchainStorage, InMemoryBlockStates};
 use crate::accounts::{Account, DevAccountGenerator};
 use crate::backend::state::{MemDb, StateExt};
@@ -128,7 +128,7 @@ impl Backend {
         let mut state = CachedState::new(state);
 
         let exec_info = execute_transaction(
-            Transaction::AccountTransaction(transaction),
+            ExecutionTransaction::AccountTransaction(transaction),
             &mut state,
             &self.env.read().block,
             true,
@@ -184,12 +184,24 @@ impl Backend {
             Block::new(partial_block.header, partial_block.transactions, partial_block.outputs)
         };
 
-        let pending_txs = block.transactions.clone();
-
         let block_hash = block.header.hash();
         let block_number = block.header.number;
+        let tx_count = block.transactions.len();
 
-        info!("⛏️ Block {block_number} mined with {} transactions", pending_txs.len());
+        // Stores the pending transaction in storage
+        for tx in &block.transactions {
+            let transaction_hash = tx.inner.hash();
+            self.storage.write().await.transactions.insert(
+                transaction_hash,
+                IncludedTransaction {
+                    block_number,
+                    block_hash,
+                    transaction: tx.clone(),
+                    status: TransactionStatus::AcceptedOnL2,
+                }
+                .into(),
+            );
+        }
 
         // get state diffs
         let pending_state_diff = pending.state.to_state_diff();
@@ -198,26 +210,14 @@ impl Backend {
         // store block and the state diff
         self.storage.write().await.append_block(block_hash, block, state_diff);
 
+        info!("⛏️ Block {block_number} mined with {tx_count} transactions");
+
         // apply the pending state to the current state
         self.state.write().await.apply_state(&mut pending.state);
 
         // store the current state
         let state = self.state.read().await.clone();
         self.states.write().await.insert(block_hash, state);
-
-        // Stores the pending transaction in storage
-        for tx in pending_txs {
-            let hash: FieldElement = tx.transaction.transaction_hash().0.into();
-            self.storage.write().await.transactions.insert(
-                hash,
-                KnownTransaction::Included(IncludedTransaction {
-                    block_number,
-                    block_hash,
-                    transaction: tx.clone(),
-                    status: TransactionStatus::AcceptedOnL2,
-                }),
-            );
-        }
     }
 
     pub async fn open_pending_block(&self) {
