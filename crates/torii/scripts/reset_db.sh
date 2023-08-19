a2475c0c76d96d85323aa028fae94bb341fbf071
broody <broody@gmail.com>
2023-08-18 16:27:10 -1000

    chore(torii): update scripts

--- crates/torii/scripts/reset_db.sh
+++ crates/torii/scripts/reset_db.sh
@@ -5,4 +5,3 @@ pushd $(dirname "$0")/..
 
 yes | cargo sqlx database reset --database-url sqlite://$PWD/indexer.db
 cargo sqlx migrate run --database-url sqlite://$PWD/indexer.db
-cargo sqlx prepare --database-url sqlite://$PWD/indexer.db
