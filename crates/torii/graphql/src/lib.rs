904baf77b1b79cdf4e41a3e26be5434a805ce2ee
gianmarcoalarcon <gian.alarcon@unmsm.edu.pe>
2023-08-23 10:09:08 +0800

    Fix merge conflict

--- crates/torii/graphql/src/lib.rs
+++ crates/torii/graphql/src/lib.rs
@@ -1,10 +1,7 @@
 mod constants;
-<<<<<<< HEAD
 pub mod object;
-=======
-mod object;
+
 mod query;
->>>>>>> bec0ca26233018c1a1a62d038320567e38b1461c
 pub mod schema;
 pub mod server;
 pub mod simple_broker;
