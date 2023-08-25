96b41938bae0089f64991300df2ec6d335caab26
gianmarcoalarcon <gian.alarcon@unmsm.edu.pe>
2023-08-25 17:47:40 +0800

    Feat: Add script test for entity subscription and component subscription

--- crates/torii/graphql/src/tests/components_test.rs
+++ crates/torii/graphql/src/tests/components_test.rs
@@ -1,9 +1,18 @@
 #[cfg(test)]
 mod tests {
+    use std::time::Duration;
+
+    use async_graphql::value;
+    use dojo_types::component::Member;
+    use dojo_world::manifest::Component;
     use sqlx::SqlitePool;
+    use starknet_crypto::FieldElement;
+    use tokio::sync::mpsc;
+    use torii_core::State;
 
     use crate::tests::common::{
-        entity_fixtures, run_graphql_query, Connection, Edge, Moves, Position,
+        entity_fixtures, init, run_graphql_query, run_graphql_subscription, Connection, Edge,
+        Moves, Position,
     };
 
     type OrderTestFn = dyn Fn(&Vec<Edge<Position>>) -> bool;
@@ -197,5 +206,50 @@ mod tests {
         let connection: Connection<Position> = serde_json::from_value(positions.clone()).unwrap();
         let entity = connection.edges[0].node.entity.as_ref().unwrap();
         assert_eq!(entity.component_names, "Position".to_string());
+    }
+    #[sqlx::test(migrations = "../migrations")]
+    async fn test_component_subscription(pool: SqlitePool) {
+        let state = init(&pool).await;
+        // 0. Preprocess component value
+        let name = "Test".to_string();
+        let component_id = name.to_lowercase();
+        let class_hash = FieldElement::TWO;
+        let hex_class_hash = format!("{:#x}", class_hash);
+        let expected_value: async_graphql::Value = value!({
+         "componentAdded": { "id": component_id.clone(), "name":name, "classHash": hex_class_hash }
+        });
+        let (tx, mut rx) = mpsc::channel(7);
+
+        tokio::spawn(async move {
+            // 1. Open process and sleep.Go to execute subscription
+            tokio::time::sleep(Duration::from_secs(1)).await;
+
+            let component = Component {
+                name,
+                members: vec![Member { name: "test".into(), ty: "u32".into(), key: false }],
+                class_hash,
+                ..Default::default()
+            };
+            state.register_component(component).await.unwrap();
+            // 3. fn publish() is called from state.set_entity()
+
+            tx.send(()).await.unwrap();
+        });
+
+        // 2. The subscription is executed and it is listeing, waiting for publish() to be executed
+        let response_value = run_graphql_subscription(
+            &pool,
+            r#"
+            subscription {
+							componentAdded {
+									id, name, classHash
+                }
+            }"#,
+        )
+        .await;
+        // 4. The subcription has received the message from publish()
+        // 5. Compare values
+        assert_eq!(expected_value, response_value);
+        rx.recv().await.unwrap();
     }
 }
--- crates/torii/graphql/src/tests/entities_test.rs
+++ crates/torii/graphql/src/tests/entities_test.rs
@@ -1,10 +1,16 @@
 #[cfg(test)]
 mod tests {
+    use std::time::Duration;
+
+    use async_graphql::value;
     use sqlx::SqlitePool;
     use starknet_crypto::{poseidon_hash_many, FieldElement};
+    use tokio::sync::mpsc;
+    use torii_core::State;
 
     use crate::tests::common::{
-        entity_fixtures, paginate, run_graphql_query, Direction, Entity, Moves, Position,
+        entity_fixtures, init, paginate, run_graphql_query, run_graphql_subscription, Direction,
+        Entity, Moves, Position,
     };
 
     #[sqlx::test(migrations = "../migrations")]
@@ -97,6 +103,48 @@ mod tests {
         assert_eq!(entities_connection.total_count, 3);
         assert_eq!(entities_connection.edges.len(), page_size);
         assert_eq!(entities_connection.edges[0].cursor, next_cursor);
+    }
+
+    #[sqlx::test(migrations = "../migrations")]
+    async fn test_entity_subscription(pool: SqlitePool) {
+        let state = init(&pool).await;
+        // 0. Preprocess expected entity value
+        let key = vec![FieldElement::ONE];
+        let entity_id = format!("{:#x}", poseidon_hash_many(&key));
+        let keys_str = key.iter().map(|k| format!("{:#x}", k)).collect::<Vec<String>>().join(",");
+        let expected_value: async_graphql::Value = value!({
+                            "entityAdded": { "id": entity_id.clone(), "keys":vec![keys_str.clone()], "componentNames": "Moves" }
+        });
+        let (tx, mut rx) = mpsc::channel(10);
+
+        tokio::spawn(async move {
+            // 1. Open process and sleep.Go to execute subscription
+            tokio::time::sleep(Duration::from_secs(1)).await;
+
+            // Set entity with one moves component
+            // remaining: 10
+            let moves_values = vec![FieldElement::from_hex_be("0xa").unwrap()];
+            state.set_entity("Moves".to_string(), key, moves_values).await.unwrap();
+            // 3. fn publish() is called from state.set_entity()
+
+            tx.send(()).await.unwrap();
+        });
+
+        // 2. The subscription is executed and it is listeing, waiting for publish() to be executed
+        let response_value = run_graphql_subscription(
+            &pool,
+            r#"
+            subscription {
+                entityAdded {
+									id, keys, componentNames
+                }
+            }"#,
+        )
+        .await;
+        // 4. The subcription has received the message from publish()
+        // 5. Compare values
+        assert_eq!(expected_value, response_value);
+        rx.recv().await.unwrap();
     }
 
     // FIXME: Enable when `WhereInput` param implemented
