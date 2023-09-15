d1db9038aa051a4ef78726eafec7df667efd624b
Kariy <ammr.arf@gmail.com>
2023-09-15 13:11:12 +0900

    fix

--- crates/katana/rpc/src/api/starknet.rs
+++ crates/katana/rpc/src/api/starknet.rs
@@ -167,7 +167,7 @@ pub trait StarknetApi {
     #[method(name = "estimateFee")]
     async fn estimate_fee(
         &self,
-        transactions: Vec<BroadcastedTransaction>,
+        request: Vec<BroadcastedTransaction>,
         block_id: BlockId,
     ) -> Result<Vec<FeeEstimate>, Error>;
 
