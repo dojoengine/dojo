96770d43b9e71c7eb062f8ccb623132842834b5f
broody <broody@gmail.com>
2023-09-28 08:58:51 -0700

    use strum macros

--- crates/dojo-types/src/core.rs
+++ crates/dojo-types/src/core.rs
@@ -1,9 +1,8 @@
-use core::fmt;
-use std::slice::Iter;
-use std::str::FromStr;
-
 use starknet::core::types::FieldElement;
+use strum_macros::{AsRefStr, Display, EnumIter, EnumString};
 
+#[derive(AsRefStr, Display, EnumIter, EnumString, Debug)]
+#[strum(serialize_all = "lowercase")]
 pub enum CairoType {
     U8,
     U16,
@@ -13,9 +12,11 @@ pub enum CairoType {
     U256,
     USize,
     Bool,
-    ContractAddress,
-    ClassHash,
     Felt252,
+    #[strum(serialize = "ClassHash")]
+    ClassHash,
+    #[strum(serialize = "ContractAddress")]
+    ContractAddress,
 }
 
 #[derive(Debug, thiserror::Error)]
@@ -28,63 +29,7 @@ pub enum CairoTypeError {
     UnsupportedType,
 }
 
-impl FromStr for CairoType {
-    type Err = ();
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
-            "u8" => Ok(CairoType::U8),
-            "u16" => Ok(CairoType::U16),
-            "u32" => Ok(CairoType::U32),
-            "u64" => Ok(CairoType::U64),
-            "u128" => Ok(CairoType::U128),
-            "u256" => Ok(CairoType::U256),
-            "usize" => Ok(CairoType::USize),
-            "bool" => Ok(CairoType::Bool),
-            "ContractAddress" => Ok(CairoType::ContractAddress),
-            "ClassHash" => Ok(CairoType::ClassHash),
-            "felt252" => Ok(CairoType::Felt252),
-            _ => Err(()),
-        }
-    }
-}
-
-impl fmt::Display for CairoType {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match *self {
-            CairoType::U8 => write!(f, "u8"),
-            CairoType::U16 => write!(f, "u16"),
-            CairoType::U32 => write!(f, "u32"),
-            CairoType::U64 => write!(f, "u64"),
-            CairoType::U128 => write!(f, "u128"),
-            CairoType::U256 => write!(f, "u256"),
-            CairoType::USize => write!(f, "usize"),
-            CairoType::Bool => write!(f, "bool"),
-            CairoType::ContractAddress => write!(f, "ContractAddress"),
-            CairoType::ClassHash => write!(f, "ClassHash"),
-            CairoType::Felt252 => write!(f, "felt252"),
-        }
-    }
-}
-
 impl CairoType {
-    pub fn iter() -> Iter<'static, CairoType> {
-        static VARIANTS: [CairoType; 11] = [
-            CairoType::U8,
-            CairoType::U16,
-            CairoType::U32,
-            CairoType::U64,
-            CairoType::U128,
-            CairoType::U256,
-            CairoType::USize,
-            CairoType::Bool,
-            CairoType::ContractAddress,
-            CairoType::ClassHash,
-            CairoType::Felt252,
-        ];
-        VARIANTS.iter()
-    }
-
     pub fn to_sql_type(&self) -> String {
         match self {
             CairoType::U8
