ffc6f006f0fe51330aa0a90696ce6e092143c7ca
Yun <broody@gmail.com>
2023-10-06 09:31:37 -0700

    Torii graphql enum fix (#976)
    * Restart torii server on new model registration
    
    * Added type enum col to torii indexer
    
    * Added type enum col to torii indexer
    
    * Torii graphql handle enum
    
    * Revert "Restart torii server on new model registration"
    
    This reverts commit c1ed2796c98aceae732904a6f0bf0cced94d0a74.
    
    * clean up

--- crates/torii/graphql/src/query/mod.rs
+++ crates/torii/graphql/src/query/mod.rs
@@ -1,8 +1,5 @@
-use std::str::FromStr;
-
 use async_graphql::dynamic::TypeRef;
 use async_graphql::Name;
-use dojo_types::primitive::Primitive;
 use sqlx::pool::PoolConnection;
 use sqlx::sqlite::SqliteRow;
 use sqlx::{FromRow, QueryBuilder, Result, Sqlite};
@@ -71,7 +68,19 @@ pub async fn type_mapping_query(
     conn: &mut PoolConnection<Sqlite>,
     model_id: &str,
 ) -> sqlx::Result<TypeMapping> {
-    let model_members: Vec<ModelMember> = sqlx::query_as(
+    let model_members = fetch_model_members(conn, model_id).await?;
+
+    let (root_members, nested_members): (Vec<&ModelMember>, Vec<&ModelMember>) =
+        model_members.iter().partition(|member| member.model_idx == 0);
+
+    build_type_mapping(&root_members, &nested_members)
+}
+
+async fn fetch_model_members(
+    conn: &mut PoolConnection<Sqlite>,
+    model_id: &str,
+) -> sqlx::Result<Vec<ModelMember>> {
+    sqlx::query_as(
         r#"
         SELECT
             id,
@@ -79,6 +88,7 @@ pub async fn type_mapping_query(
             model_idx,
             name,
             type AS ty,
+            type_enum,
             key,
             created_at
         from model_members WHERE model_id = ?
@@ -86,41 +96,43 @@ pub async fn type_mapping_query(
     )
     .bind(model_id)
     .fetch_all(conn)
-    .await?;
+    .await
+}
 
-    let (root_members, nested_members): (Vec<&ModelMember>, Vec<&ModelMember>) =
-        model_members.iter().partition(|member| member.model_idx == 0);
-
+fn build_type_mapping(
+    root_members: &[&ModelMember],
+    nested_members: &[&ModelMember],
+) -> sqlx::Result<TypeMapping> {
     let type_mapping: TypeMapping = root_members
         .iter()
-        .map(|member| {
-            let type_data = match Primitive::from_str(&member.ty) {
-                Ok(_) => TypeData::Simple(TypeRef::named(member.ty.clone())),
-                _ => parse_nested_type(&member.model_id, &member.ty, &nested_members),
-            };
-
-            (Name::new(&member.name), type_data)
+        .map(|&member| {
+            let type_data = member_to_type_data(member, nested_members);
+            Ok((Name::new(&member.name), type_data))
         })
-        .collect();
+        .collect::<sqlx::Result<TypeMapping>>()?;
 
     Ok(type_mapping)
+}
+
+fn member_to_type_data(member: &ModelMember, nested_members: &[&ModelMember]) -> TypeData {
+    // TODO: convert sql -> Ty directly
+    match member.type_enum.as_str() {
+        "Primitive" => TypeData::Simple(TypeRef::named(&member.ty)),
+        "Enum" => TypeData::Simple(TypeRef::named("Enum")),
+        _ => parse_nested_type(&member.model_id, &member.ty, nested_members),
+    }
 }
 
 fn parse_nested_type(
     target_id: &str,
     target_type: &str,
-    nested_members: &Vec<&ModelMember>,
+    nested_members: &[&ModelMember],
 ) -> TypeData {
     let nested_mapping: TypeMapping = nested_members
         .iter()
-        .filter_map(|member| {
-            // search for target type in nested members
+        .filter_map(|&member| {
             if target_id == member.model_id && member.id.ends_with(target_type) {
-                let type_data = match Primitive::from_str(&member.ty) {
-                    Ok(_) => TypeData::Simple(TypeRef::named(member.ty.clone())),
-                    _ => parse_nested_type(&member.model_id, &member.ty, nested_members),
-                };
-
+                let type_data = member_to_type_data(member, nested_members);
                 Some((Name::new(&member.name), type_data))
             } else {
                 None
