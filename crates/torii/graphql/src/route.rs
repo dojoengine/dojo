fa831b98065403560f5475977e3b0dcdc484193c
Tarrence van As <tarrence@vanas.family>
2023-10-09 11:05:49 -0400

    Add graphql subscription support

--- crates/torii/graphql/src/route.rs
+++ crates/torii/graphql/src/route.rs
@@ -2,6 +2,7 @@ use std::convert::Infallible;
 
 use async_graphql::dynamic::Schema;
 use async_graphql::http::{playground_source, GraphQLPlaygroundConfig};
+use async_graphql_warp::graphql_subscription;
 use sqlx::{Pool, Sqlite};
 use warp::Filter;
 
@@ -11,14 +12,14 @@ pub async fn filter(
     pool: &Pool<Sqlite>,
 ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
     let schema = build_schema(pool).await.unwrap();
-    let graphql_filter = warp::path("graphql").and(async_graphql_warp::graphql(schema)).and_then(
-        |(schema, request): (Schema, async_graphql::Request)| async move {
+    let graphql_post = warp::path("graphql")
+        .and(async_graphql_warp::graphql(schema.clone()))
+        .and_then(|(schema, request): (Schema, async_graphql::Request)| async move {
             // Execute query
             let response = schema.execute(request).await;
             // Return result
             Ok::<_, Infallible>(warp::reply::json(&response))
-        },
-    );
+        });
 
     let graphiql_filter = warp::path("graphql").map(|| {
         warp::reply::html(playground_source(
@@ -26,5 +27,5 @@ pub async fn filter(
         ))
     });
 
-    graphql_filter.or(graphiql_filter)
+    graphql_subscription(schema).or(graphql_post).or(graphiql_filter)
 }
