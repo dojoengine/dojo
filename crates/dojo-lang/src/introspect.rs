ed162d5e052ab86247c1e4441b27a15300f4216c
Shramee Srivastav <shramee.srivastav@gmail.com>
2023-10-09 17:05:38 +0700

    schema: optimise with precompute
    with clippy and tests

--- crates/dojo-lang/src/introspect.rs
+++ crates/dojo-lang/src/introspect.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use cairo_lang_defs::patcher::RewriteNode;
 use cairo_lang_syntax::node::ast::ItemStruct;
 use cairo_lang_syntax::node::db::SyntaxGroup;
@@ -5,7 +7,24 @@ use cairo_lang_syntax::node::helpers::QueryAttrs;
 use cairo_lang_syntax::node::{Terminal, TypedSyntaxNode};
 use cairo_lang_utils::unordered_hash_map::UnorderedHashMap;
 use dojo_world::manifest::Member;
-use itertools::Itertools;
+
+#[derive(Clone, Default)]
+struct TypeIntrospection(usize, Vec<usize>);
+
+fn primitive_type_introspection() -> HashMap<String, TypeIntrospection> {
+    HashMap::from([
+        ("felt252".into(), TypeIntrospection(1, vec![251])),
+        ("bool".into(), TypeIntrospection(1, vec![1])),
+        ("u8".into(), TypeIntrospection(1, vec![8])),
+        ("u16".into(), TypeIntrospection(1, vec![16])),
+        ("u32".into(), TypeIntrospection(1, vec![32])),
+        ("u64".into(), TypeIntrospection(1, vec![64])),
+        ("u128".into(), TypeIntrospection(1, vec![128])),
+        ("u256".into(), TypeIntrospection(2, vec![128, 128])),
+        ("ContractAddress".into(), TypeIntrospection(1, vec![251])),
+        ("ClassHash".into(), TypeIntrospection(1, vec![251])),
+    ])
+}
 
 /// A handler for Dojo code derives Introspect for a struct
 /// Parameters:
@@ -25,19 +44,35 @@ pub fn handle_introspect_struct(db: &dyn SyntaxGroup, struct_ast: ItemStruct) ->
         })
         .collect::<_>();
 
-    let layout: Vec<_> = members
-        .iter()
-        .filter_map(|m| {
-            if m.key {
-                return None;
-            }
+    let primitive_sizes = primitive_type_introspection();
+    let mut size_precompute = 0;
 
-            Some(RewriteNode::Text(format!(
+    let mut size = vec![];
+    let mut layout = vec![];
+    members.iter().for_each(|m| {
+        if m.key {
+            return;
+        }
+
+        let primitive_intro = primitive_sizes.get(&m.ty);
+
+        if let Some(p_ty) = primitive_intro {
+            size_precompute += p_ty.0;
+            p_ty.1
+                .iter()
+                .for_each(|l| layout.push(RewriteNode::Text(format!("layout.append({});\n", l))));
+        } else {
+            size.push(format!("dojo::database::schema::SchemaIntrospection::<{}>::size()", m.ty,));
+            layout.push(RewriteNode::Text(format!(
                 "dojo::database::schema::SchemaIntrospection::<{}>::layout(ref layout);\n",
                 m.ty
             )))
-        })
-        .collect::<_>();
+        }
+    });
+
+    if size_precompute > 0 {
+        size.push(format!("{}", size_precompute));
+    }
 
     let member_types: Vec<_> = members
         .iter()
@@ -85,26 +120,7 @@ pub fn handle_introspect_struct(db: &dyn SyntaxGroup, struct_ast: ItemStruct) ->
         ",
         UnorderedHashMap::from([
             ("name".to_string(), RewriteNode::new_trimmed(struct_ast.name(db).as_syntax_node())),
-            (
-                "size".to_string(),
-                RewriteNode::Text(
-                    struct_ast
-                        .members(db)
-                        .elements(db)
-                        .iter()
-                        .filter_map(|member| {
-                            if member.has_attr(db, "key") {
-                                return None;
-                            }
-
-                            Some(format!(
-                                "dojo::database::schema::SchemaIntrospection::<{}>::size()",
-                                member.type_clause(db).ty(db).as_syntax_node().get_text(db),
-                            ))
-                        })
-                        .join(" + "),
-                ),
-            ),
+            ("size".to_string(), RewriteNode::Text(size.join(" + "))),
             ("layout".to_string(), RewriteNode::new_modified(layout)),
             ("member_types".to_string(), RewriteNode::Text(member_types.join(","))),
         ]),
