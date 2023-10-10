3660510838dce316e7821ea2a3a743ecc49bea54
0xicosahedron <btc_icosahedron@protonmail.com>
2023-10-11 00:51:32 +0530

    try to canonicalize path

--- crates/sozo/tests/test_build.rs
+++ crates/sozo/tests/test_build.rs
@@ -2,14 +2,15 @@ mod utils;
 
 use utils::snapbox::get_snapbox;
 use utils::stdout::expected_stdout;
+use std::fs;
 
 #[test]
 fn test_invalid_cairo_version() {
+    let path = fs::canonicalize("./tests/data/invalid_cairo_version");
     let assert = get_snapbox()
         .arg("build")
-        .current_dir("./tests/data/invalid_cairo_version")
+        .current_dir(path.unwrap())
         .assert()
         .failure();
-
     assert.stdout_eq(expected_stdout("wrong-cairo-version"));
 }
