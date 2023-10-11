2689e6af3f956e68fbddc550813016cea3149b71
broody <broody@gmail.com>
2023-10-10 17:03:46 -0700

    Add optional limit/offset to plural graphql queries

--- crates/torii/graphql/src/utils/parse_argument.rs
+++ crates/torii/graphql/src/utils/parse_argument.rs
@@ -2,19 +2,19 @@ use async_graphql::dynamic::ResolverContext;
 use async_graphql::Result;
 
 pub trait ParseArgument: Sized {
-    fn parse(ctx: &ResolverContext<'_>, input: String) -> Result<Self>;
+    fn parse(ctx: &ResolverContext<'_>, input: &str) -> Result<Self>;
 }
 
 impl ParseArgument for u64 {
-    fn parse(ctx: &ResolverContext<'_>, input: String) -> Result<Self> {
-        let arg = ctx.args.try_get(input.as_str());
+    fn parse(ctx: &ResolverContext<'_>, input: &str) -> Result<Self> {
+        let arg = ctx.args.try_get(input);
         arg?.u64()
     }
 }
 
 impl ParseArgument for String {
-    fn parse(ctx: &ResolverContext<'_>, input: String) -> Result<Self> {
-        let arg = ctx.args.try_get(input.as_str());
+    fn parse(ctx: &ResolverContext<'_>, input: &str) -> Result<Self> {
+        let arg = ctx.args.try_get(input);
         Ok(arg?.string()?.to_string())
     }
 }
