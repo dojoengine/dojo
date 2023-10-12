3521b9b72b40ca1c645eb58d479262211a858aa6
0xicosahedron <btc_icosahedron@protonmail.com>
2023-10-12 21:41:31 +0530

    rename data to test_data

--- crates/sozo/tests/test_build.rs
+++ crates/sozo/tests/test_build.rs
@@ -7,7 +7,7 @@ use utils::stdout::expected_stdout;
 
 #[test]
 fn test_invalid_cairo_version() {
-    let path = fs::canonicalize("./tests/data/invalid_cairo_version");
+    let path = fs::canonicalize("./tests/test_data/invalid_cairo_version");
     let assert = get_snapbox().arg("build").current_dir(path.unwrap()).assert().failure();
     assert.stdout_eq(expected_stdout("wrong-cairo-version"));
 }
