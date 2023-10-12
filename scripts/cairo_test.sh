e2a8470ade3e47bd799d61f9670543d97ab43b80
Tarrence van As <tarrence@vanas.family>
2023-10-12 10:08:30 -0400

    Add defi tests to ci

--- scripts/cairo_test.sh
+++ scripts/cairo_test.sh
@@ -4,5 +4,5 @@ set -euxo pipefail
 cargo run -r --bin sozo -- --manifest-path crates/dojo-core/Scarb.toml test
 # cargo run --bin sozo -- test crates/dojo-physics
 cargo run -r --bin sozo -- --manifest-path crates/dojo-erc/Scarb.toml test
-# cargo run --bin sozo -- test crates/dojo-defi
+cargo run -r --bin sozo -- --manifest-path crates/dojo-defi/Scarb.toml test
 cargo run -r --bin sozo -- --manifest-path examples/ecs/Scarb.toml test
