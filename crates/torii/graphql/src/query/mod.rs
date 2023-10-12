ae1878c7d366afcd4e271ceb6a7e17c390e91f5d
akhercha <akherchache@pm.me>
2023-10-12 16:12:35 +0200

    feat(torii_trim_leading_zeros): Added 0x prefix to U256 values + Reverted fn

--- crates/torii/graphql/src/query/mod.rs
+++ crates/torii/graphql/src/query/mod.rs
@@ -96,11 +96,12 @@ fn parse_nested_type(
 }
 
 fn remove_hex_leading_zeros(value: Value) -> Value {
-    if let Value::String(mut str_val) = value {
+    if let Value::String(str_val) = &value {
         if !str_val.starts_with("0x") {
-            str_val = str_val.trim_start_matches("0x").to_string();
+            return value;
         }
-        let trimmed_hex = str_val.trim_start_matches('0');
+        let hex_part = str_val.trim_start_matches("0x");
+        let trimmed_hex = hex_part.trim_start_matches('0');
         Value::String(format!("0x{:0>1}", trimmed_hex))
     } else {
         value
