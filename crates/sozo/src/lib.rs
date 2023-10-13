14ef072dbfac30b7d2bef76da540cf6d99f54514
Mateusz Zając <matzayon@gmail.com>
2023-10-13 16:33:05 +0000

    used cli methods in benches

--- crates/sozo/src/lib.rs
+++ crates/sozo/src/lib.rs
@@ -1,3 +1,6 @@
 pub mod args;
 pub mod commands;
 pub mod ops;
+
+mod cli_main;
+pub use cli_main::cli_main;
--- crates/sozo/src/main.rs
+++ crates/sozo/src/main.rs
@@ -1,14 +1,8 @@
-use std::env;
 use std::process::exit;
 
-use anyhow::Result;
 use clap::Parser;
-use dojo_lang::compiler::DojoCompiler;
-use dojo_lang::plugin::CairoPluginRepository;
-use scarb::compiler::CompilerRepository;
-use scarb::core::Config;
 use scarb_ui::{OutputFormat, Ui};
-use sozo::args::{Commands, SozoArgs};
+use sozo::{args::SozoArgs, cli_main};
 
 fn main() {
     let args = SozoArgs::parse();
@@ -19,27 +13,4 @@ fn main() {
         ui.anyhow(&err);
         exit(1);
     }
-}
-
-fn cli_main(args: SozoArgs) -> Result<()> {
-    let mut compilers = CompilerRepository::std();
-    let cairo_plugins = CairoPluginRepository::default();
-
-    match &args.command {
-        Commands::Build(_) | Commands::Dev(_) => compilers.add(Box::new(DojoCompiler)).unwrap(),
-        _ => {}
-    }
-
-    let manifest_path = scarb::ops::find_manifest_path(args.manifest_path.as_deref())?;
-
-    let config = Config::builder(manifest_path)
-        .log_filter_directive(env::var_os("SCARB_LOG"))
-        .profile(args.profile_spec.determine()?)
-        .offline(args.offline)
-        .cairo_plugins(cairo_plugins.into())
-        .ui_verbosity(args.ui_verbosity())
-        .compilers(compilers)
-        .build()?;
-
-    sozo::commands::run(args.command, &config)
 }
