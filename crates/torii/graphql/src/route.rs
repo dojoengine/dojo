5f5337921b4310a8eace6407cb9dad2a5d3c882e
Tarrence van As <tarrencev@users.noreply.github.com>
2023-10-15 15:15:12 -0400

    Allow configuring external url for graphql playground websocket (#1046)

--- crates/torii/graphql/src/route.rs
+++ crates/torii/graphql/src/route.rs
@@ -4,12 +4,14 @@ use async_graphql::dynamic::Schema;
 use async_graphql::http::{playground_source, GraphQLPlaygroundConfig};
 use async_graphql_warp::graphql_subscription;
 use sqlx::{Pool, Sqlite};
+use url::Url;
 use warp::Filter;
 
 use super::schema::build_schema;
 
 pub async fn filter(
     pool: &Pool<Sqlite>,
+    external_url: Option<Url>,
 ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
     let schema = build_schema(pool).await.unwrap();
     let graphql_post = warp::path("graphql")
@@ -21,9 +23,17 @@ pub async fn filter(
             Ok::<_, Infallible>(warp::reply::json(&response))
         });
 
-    let playground_filter = warp::path("graphql").map(|| {
+    let subscription_endpoint = if let Some(external_url) = external_url {
+        format!("{external_url}/graphql/ws").replace("http", "ws")
+    } else {
+        "/graphql/ws".to_string()
+    };
+
+    let playground_filter = warp::path("graphql").map(move || {
         warp::reply::html(playground_source(
-            GraphQLPlaygroundConfig::new("").subscription_endpoint("ws"),
+            // NOTE: GraphQL Playground currently doesn't support relative urls for the
+            // subscription endpoint.
+            GraphQLPlaygroundConfig::new("").subscription_endpoint(subscription_endpoint.as_str()),
         ))
     });
 
