a8244866a3a3a108dafaf092e020cfa991037d85
Tarrence van As <tarrencev@users.noreply.github.com>
2023-10-20 16:51:48 -0400

    Add missing plugins in language server (#1076)

--- crates/dojo-language-server/src/bin/language_server.rs
+++ crates/dojo-language-server/src/bin/language_server.rs
@@ -3,9 +3,14 @@ use std::sync::Arc;
 use cairo_lang_compiler::db::RootDatabase;
 use cairo_lang_filesystem::cfg::{Cfg, CfgSet};
 use cairo_lang_language_server::Backend;
+use cairo_lang_starknet::inline_macros::selector::SelectorMacro;
 use cairo_lang_starknet::plugin::StarkNetPlugin;
+use cairo_lang_test_runner::plugin::TestPlugin;
 use cairo_lang_utils::logging::init_logging;
 use clap::Parser;
+use dojo_lang::inline_macros::emit::EmitMacro;
+use dojo_lang::inline_macros::get::GetMacro;
+use dojo_lang::inline_macros::set::SetMacro;
 use dojo_lang::plugin::BuiltinDojoPlugin;
 use tower_lsp::{LspService, Server};
 
@@ -29,8 +34,13 @@ async fn main() {
 
     let db = RootDatabase::builder()
         .with_cfg(CfgSet::from_iter([Cfg::name("test")]))
+        .with_macro_plugin(Arc::new(TestPlugin::default()))
         .with_macro_plugin(Arc::new(BuiltinDojoPlugin))
         .with_macro_plugin(Arc::new(StarkNetPlugin::default()))
+        .with_inline_macro_plugin(EmitMacro::NAME, Arc::new(EmitMacro))
+        .with_inline_macro_plugin(GetMacro::NAME, Arc::new(GetMacro))
+        .with_inline_macro_plugin(SetMacro::NAME, Arc::new(SetMacro))
+        .with_inline_macro_plugin(SelectorMacro::NAME, Arc::new(SelectorMacro))
         .build()
         .unwrap_or_else(|error| {
             panic!("Problem creating language database: {error:?}");
