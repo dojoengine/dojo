e40463baf487bec560c5c9c103d3ec0d08573ce0
broody <broody@gmail.com>
2023-10-25 13:52:34 -0700

    Torii stores metadata files

--- crates/torii/core/src/processors/metadata_update.rs
+++ crates/torii/core/src/processors/metadata_update.rs
@@ -1,13 +1,24 @@
-use anyhow::{Error, Ok, Result};
+use std::time::Duration;
+
+use anyhow::{Error, Result};
 use async_trait::async_trait;
+use base64::engine::general_purpose;
+use base64::Engine as _;
 use dojo_world::contracts::world::WorldContractReader;
+use dojo_world::metadata::{Uri, WorldMetadata};
+use reqwest::Client;
 use starknet::core::types::{BlockWithTxs, Event, InvokeTransactionReceipt};
 use starknet::core::utils::parse_cairo_short_string;
 use starknet::providers::Provider;
-use tracing::info;
+use starknet_crypto::FieldElement;
+use tokio_util::bytes::Bytes;
+use tracing::{error, info};
 
 use super::EventProcessor;
 use crate::sql::Sql;
+
+const IPFS_URL: &str = "https://ipfs.io/ipfs/";
+const MAX_RETRY: u8 = 3;
 
 #[derive(Default)]
 pub struct MetadataUpdateProcessor;
@@ -33,7 +44,7 @@ where
         let resource = &event.data[0];
         let uri_len: u8 = event.data[1].try_into().unwrap();
 
-        let uri = if uri_len > 0 {
+        let uri_str = if uri_len > 0 {
             event.data[2..=uri_len as usize + 1]
                 .iter()
                 .map(parse_cairo_short_string)
@@ -43,10 +54,71 @@ where
             "".to_string()
         };
 
-        info!("Resource {:#x} metadata set: {}", resource, uri);
+        info!("Resource {:#x} metadata set: {}", resource, uri_str);
+        db.set_metadata(resource, &uri_str);
 
-        db.set_metadata(resource, uri);
-
+        spawn_retrieve(db.clone(), *resource, uri_str);
         Ok(())
     }
+}
+
+fn spawn_retrieve(mut db: Sql, resource: FieldElement, uri_str: String) {
+    tokio::spawn(async move {
+        match metadata(uri_str.clone()).await {
+            Ok((metadata, icon_data, cover_data)) => {
+                db.update_metadata(&resource, &uri_str, &metadata, &icon_data, &cover_data)
+                    .await
+                    .unwrap();
+                info!("Updated resource {resource:#x} metadata from ipfs");
+            }
+            Err(e) => {
+                error!("{}", e)
+            }
+        }
+    });
+}
+
+async fn metadata(uri_str: String) -> Result<(WorldMetadata, Option<String>, Option<String>)> {
+    let uri = Uri::Ipfs(uri_str);
+    let cid = uri.cid().ok_or("Uri is malformed").map_err(Error::msg)?;
+
+    let bytes = fetch_cid(cid, MAX_RETRY).await?;
+    let metadata: WorldMetadata = serde_json::from_str(std::str::from_utf8(&bytes)?)?;
+
+    let icon_data = fetch_image(&metadata.icon_uri).await;
+    let cover_data = fetch_image(&metadata.cover_uri).await;
+
+    Ok((metadata, icon_data, cover_data))
+}
+
+async fn fetch_image(image_uri: &Option<Uri>) -> Option<String> {
+    if let Some(uri) = image_uri {
+        let data = fetch_cid(uri.cid()?, MAX_RETRY).await.ok()?;
+        let encoded = general_purpose::STANDARD.encode(data);
+        return Some(encoded);
+    }
+
+    None
+}
+
+async fn fetch_cid(cid: &str, mut retries: u8) -> Result<Bytes> {
+    while retries > 0 {
+        let response = Client::new().get(format!("{IPFS_URL}{}", cid)).send().await;
+
+        match response {
+            Ok(response) => return response.bytes().await.map_err(|e| e.into()),
+            Err(e) => {
+                retries -= 1;
+                if retries > 0 {
+                    info!("Fetch uri failure: {}", e);
+                    tokio::time::sleep(Duration::from_secs(3)).await;
+                }
+            }
+        }
+    }
+
+    Err(Error::msg(format!(
+        "Failed to pull data from IPFS after {} attempts, cid: {}",
+        MAX_RETRY, cid
+    )))
 }
--- crates/torii/graphql/src/object/metadata.rs
+++ crates/torii/graphql/src/object/metadata.rs
@@ -1,5 +1,3 @@
-use async_graphql::dynamic::Field;
-
 use super::{ObjectTrait, TypeMapping};
 use crate::mapping::METADATA_TYPE_MAPPING;
 use crate::query::constants::METADATA_TABLE;
@@ -21,9 +19,5 @@ impl ObjectTrait for MetadataObject {
 
     fn table_name(&self) -> Option<&str> {
         Some(METADATA_TABLE)
-    }
-
-    fn related_fields(&self) -> Option<Vec<Field>> {
-        None
     }
 }
