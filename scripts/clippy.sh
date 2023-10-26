34bb05a449f577a91f550168fb6c574c7ef09565
Kariy <ammr.arf@gmail.com>
2023-10-27 00:37:35 +0900

    remove wasm client

--- scripts/clippy.sh
+++ scripts/clippy.sh
@@ -6,5 +6,5 @@ run_clippy() {
   cargo clippy --all-targets --all-features "$@" -- -D warnings -D future-incompatible -D nonstandard-style -D rust-2018-idioms -D unused
 }
 
-run_clippy --manifest-path crates/torii/client/wasm/Cargo.toml --target wasm32-unknown-unknown && run_clippy
+run_clippy
 
