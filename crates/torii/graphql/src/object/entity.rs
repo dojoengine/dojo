8dd1ee7604d69db0d3b2b5e55b8136bbbdf1d6bf
gianmarcoalarcon <gian.alarcon@unmsm.edu.pe>
2023-10-26 11:27:32 +0700

    Refactor: Clean and optimize code

--- crates/torii/graphql/src/object/entity.rs
+++ crates/torii/graphql/src/object/entity.rs
@@ -174,13 +174,9 @@ pub async fn model_data_recursive_query(
     entity_id: &str,
     type_mapping: &TypeMapping,
 ) -> sqlx::Result<ValueMapping> {
-    let mut table_name = path_array[0].clone();
-    // For nested types, we need to remove prefix in path array
-    if path_array.len() > 1 {
-        let namespace = format!("{}_", path_array[0]);
-        table_name = path_array.join("$").replace(&namespace, "");
-    }
-    let query = format!("SELECT * FROM {} WHERE entity_id = '{}'", &table_name, entity_id);
+    let namespace = format!("{}_", path_array[0]); 
+    let table_name = path_array.join("$").replace(&namespace, "");
+    let query = format!("SELECT * FROM {} WHERE entity_id = '{}'", table_name, entity_id);
     let row = sqlx::query(&query).fetch_one(conn.as_mut()).await?;
     let mut value_mapping = value_mapping_from_row(&row, type_mapping, true)?;
 
