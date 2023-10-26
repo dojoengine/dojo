9769005d5ff09368ed8f16e0e400b06582c032f1
Tarrence van As <tarrence@vanas.family>
2023-10-26 10:21:05 -0400

    Add prepare release script

---
+++ scripts/prepare_release.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -euxo pipefail
+
+prev_version=$(cargo get workspace.package.version)
+next_version=$1
+
+find . -type f -name "*.toml" -exec sed -i "" "s/version = \"$prev_version\"/version = \"$next_version\"/g" {} \;
+find . -type f -name "*.toml" -exec sed -i "" "s/dojo_plugin = \"$prev_version\"/dojo_plugin = \"$next_version\"/g" {} \;
