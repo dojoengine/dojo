921809ac8a371afdef30ff5142037b549ea74821
Mateusz Zając <matzayonc@gmail.com>
2023-11-12 18:57:58 +0000

    hardcoded value comment

--- crates/torii/graphql/src/tests/mod.rs
+++ crates/torii/graphql/src/tests/mod.rs
@@ -278,7 +278,7 @@ pub async fn spinup_types_test() -> Result<SqlitePool> {
     execute_strategy(&ws, &migration, &account, None).await.unwrap();
 
     //  Execute `create` and insert 10 records into storage
-    let records_contract = "0x5cb354584b1b49308e6dd2540dcde5c408a8ee282ce74949438d7da7f266a92";
+    let records_contract = "0x5cb354584b1b49308e6dd2540dcde5c408a8ee282ce74949438d7da7f266a92"; // Haradcoded output of printed contract address
     let InvokeTransactionResult { transaction_hash } = account
         .execute(vec![Call {
             calldata: vec![FieldElement::from_str("0xa").unwrap()],
