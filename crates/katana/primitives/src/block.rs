7085921df46fdf22b3277242e94227545975e6ee
Kariy <ammr.arf@gmail.com>
2023-11-16 16:02:42 +0300

    Add more primitives types

--- crates/katana/primitives/src/block.rs
+++ crates/katana/primitives/src/block.rs
@@ -6,11 +6,18 @@ use crate::FieldElement;
 /// Block state update type.
 pub type StateUpdate = starknet::core::types::StateUpdate;
 
+#[derive(Debug, Copy, Clone, Serialize, Deserialize)]
+pub enum BlockHashOrNumber {
+    Hash(BlockHash),
+    Num(BlockNumber),
+}
+
 /// Block number type.
 pub type BlockNumber = u64;
 /// Block hash type.
 pub type BlockHash = FieldElement;
 
+/// Represents a block header.
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Header {
     pub parent_hash: BlockHash,
@@ -19,4 +26,16 @@ pub struct Header {
     pub timestamp: u64,
     pub state_root: FieldElement,
     pub sequencer_address: ContractAddress,
+}
+
+impl From<BlockNumber> for BlockHashOrNumber {
+    fn from(number: BlockNumber) -> Self {
+        Self::Num(number)
+    }
+}
+
+impl From<BlockHash> for BlockHashOrNumber {
+    fn from(hash: BlockHash) -> Self {
+        Self::Hash(hash)
+    }
 }
