d9a6e79bd6511980ef1a22d41c80cec73a18ca16
Kariy <ammr.arf@gmail.com>
2023-11-20 22:31:18 +0800

    Update get page size function

--- crates/katana/storage/db/src/utils.rs
+++ crates/katana/storage/db/src/utils.rs
@@ -5,15 +5,15 @@ use crate::codecs::{Decode, Decompress};
 use crate::error::DatabaseError;
 use crate::mdbx::tables::Table;
 
-/// Returns the default page size that can be used in this OS.
+// May lead to errors if it's reduced further because of the potential size of the data.
+const MIN_PAGE_SIZE: usize = 4096;
+// source: https://gitflic.ru/project/erthink/libmdbx/blob?file=mdbx.h#line-num-821
+const LIBMDBX_MAX_PAGE_SIZE: usize = 65536;
+
+/// Returns the default page size (in bytes) that can be used in this OS.
 pub(crate) fn default_page_size() -> usize {
     let os_page_size = page_size::get();
-    // source: https://gitflic.ru/project/erthink/libmdbx/blob?file=mdbx.h#line-num-821
-    let libmdbx_max_page_size = 0x10000;
-    // May lead to errors if it's reduced further because of the potential size of the
-    // data.
-    let min_page_size = 4096;
-    os_page_size.clamp(min_page_size, libmdbx_max_page_size)
+    os_page_size.clamp(MIN_PAGE_SIZE, LIBMDBX_MAX_PAGE_SIZE)
 }
 
 /// Check if a db is empty. It does not provide any information on the
