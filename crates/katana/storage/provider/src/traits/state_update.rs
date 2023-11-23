4bcb86edef2d4eb98a85c77fe00214cf08450130
Kariy <ammr.arf@gmail.com>
2023-11-23 12:27:19 +0800

    refactor(katana-provider): mark traits as send + sync

--- crates/katana/storage/provider/src/traits/state_update.rs
+++ crates/katana/storage/provider/src/traits/state_update.rs
@@ -1,7 +1,7 @@
 use anyhow::Result;
 use katana_primitives::block::{BlockHashOrNumber, StateUpdate};
 
-pub trait StateUpdateProvider {
+pub trait StateUpdateProvider: Send + Sync {
     /// Returns the state update for the given block.
     fn state_update(&self, block_id: BlockHashOrNumber) -> Result<Option<StateUpdate>>;
 }
