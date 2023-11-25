6d31fb24c4e1ad04c7577391fa157c3839c86638
Kariy <ammr.arf@gmail.com>
2023-11-26 05:50:44 +0800

    refactor(katana-provider): auto impl traits for smart pointer

--- crates/katana/storage/provider/src/traits/state_update.rs
+++ crates/katana/storage/provider/src/traits/state_update.rs
@@ -1,6 +1,7 @@
 use anyhow::Result;
 use katana_primitives::block::{BlockHashOrNumber, StateUpdate};
 
+#[auto_impl::auto_impl(&, Box, Arc)]
 pub trait StateUpdateProvider: Send + Sync {
     /// Returns the state update for the given block.
     fn state_update(&self, block_id: BlockHashOrNumber) -> Result<Option<StateUpdate>>;
