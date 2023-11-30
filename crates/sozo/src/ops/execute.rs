e000d27b3da9e8031b19852d8e3160473774aa00
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2023-11-30 11:11:59 +0100

    fixes and accept addr and name

--- crates/sozo/src/ops/execute.rs
+++ crates/sozo/src/ops/execute.rs
@@ -1,13 +1,11 @@
 use anyhow::{Context, Result};
 use dojo_world::metadata::Environment;
+use dojo_world::migration::strategy::generate_salt;
 use starknet::accounts::{Account, Call};
 use starknet::core::types::{BlockId, BlockTag, FieldElement, FunctionCall};
-use starknet::core::utils::{
-    cairo_short_string_to_felt, get_contract_address, get_selector_from_name,
-};
+use starknet::core::utils::{get_contract_address, get_selector_from_name};
 use starknet::macros::selector;
 use starknet::providers::Provider;
-use starknet_crypto::poseidon_hash_many;
 
 use crate::commands::execute::ExecuteArgs;
 
@@ -16,45 +14,33 @@ pub async fn execute(args: ExecuteArgs, env_metadata: Option<Environment>) -> Re
 
     let provider = starknet.provider(env_metadata.as_ref())?;
 
-    let world_address = match env_metadata.as_ref() {
-        Some(env) => match env.world_address {
-            Some(ref address) => Some(address.clone()),
-            None => None,
-        },
-        None => return Err(anyhow::anyhow!("No World Address found")),
-    };
-
-    let contract_address_str = world_address.as_ref().unwrap().as_str();
+    let world_address = env_metadata
+        .as_ref()
+        .and_then(|env| env.world_address.as_ref())
+        .map(|address| address.clone())
+        .ok_or_else(|| anyhow::anyhow!("No World Address found"))?;
 
     let contract_class_hash = provider
         .call(
             FunctionCall {
-                contract_address: FieldElement::from_hex_be(contract_address_str).unwrap(),
+                contract_address: FieldElement::from_hex_be(&world_address).unwrap(),
                 entry_point_selector: selector!("base"),
-                calldata: Vec::new(),
+                calldata: [].to_vec(),
             },
             &BlockId::Tag(BlockTag::Latest),
         )
         .await?;
 
-    let salt = poseidon_hash_many(
-        &contract_name
-            .chars()
-            .collect::<Vec<_>>()
-            .chunks(31)
-            .map(|chunk| {
-                let s: String = chunk.iter().collect();
-                cairo_short_string_to_felt(&s).unwrap()
-            })
-            .collect::<Vec<_>>(),
-    );
-
-    let contract_address = get_contract_address(
-        salt,
-        contract_class_hash[0],
-        &[],
-        FieldElement::from_hex_be(contract_address_str).unwrap(),
-    );
+    let contract_address = if contract_name.starts_with("0x") {
+        FieldElement::from_hex_be(&contract_name)?
+    } else {
+        get_contract_address(
+            generate_salt(&contract_name),
+            contract_class_hash[0],
+            &[],
+            FieldElement::from_hex_be(&world_address).unwrap(),
+        )
+    };
 
     let account = account.account(provider, env_metadata.as_ref()).await?;
 
