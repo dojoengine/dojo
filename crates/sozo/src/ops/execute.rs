8c33ed6cc93d8eea65acf5ce0584d80605475960
Loaf <90423308+ponderingdemocritus@users.noreply.github.com>
2023-11-30 16:09:27 +0100

    updates execute to accept name of contract not address (#1225)
    * updates execute to accept name of contract not address
    
    * remove print
    
    * fixes and accept addr and name
    
    * changed to contract from contract_name
    
    * clippy shutup
    
    * clippy
    
    * move network req

--- crates/sozo/src/ops/execute.rs
+++ crates/sozo/src/ops/execute.rs
@@ -1,7 +1,11 @@
 use anyhow::{Context, Result};
 use dojo_world::metadata::Environment;
+use dojo_world::migration::strategy::generate_salt;
 use starknet::accounts::{Account, Call};
-use starknet::core::utils::get_selector_from_name;
+use starknet::core::types::{BlockId, BlockTag, FieldElement, FunctionCall};
+use starknet::core::utils::{get_contract_address, get_selector_from_name};
+use starknet::macros::selector;
+use starknet::providers::Provider;
 
 use crate::commands::execute::ExecuteArgs;
 
@@ -10,12 +14,40 @@ pub async fn execute(args: ExecuteArgs, env_metadata: Option<Environment>) -> Re
 
     let provider = starknet.provider(env_metadata.as_ref())?;
 
+    let contract_address = if contract.starts_with("0x") {
+        FieldElement::from_hex_be(&contract)?
+    } else {
+        let world_address = env_metadata
+            .as_ref()
+            .and_then(|env| env.world_address.as_ref())
+            .cloned()
+            .ok_or_else(|| anyhow::anyhow!("No World Address found"))?;
+
+        let contract_class_hash = provider
+            .call(
+                FunctionCall {
+                    contract_address: FieldElement::from_hex_be(&world_address).unwrap(),
+                    entry_point_selector: selector!("base"),
+                    calldata: [].to_vec(),
+                },
+                BlockId::Tag(BlockTag::Latest),
+            )
+            .await?;
+
+        get_contract_address(
+            generate_salt(&contract),
+            contract_class_hash[0],
+            &[],
+            FieldElement::from_hex_be(&world_address).unwrap(),
+        )
+    };
+
     let account = account.account(provider, env_metadata.as_ref()).await?;
 
     let res = account
         .execute(vec![Call {
             calldata,
-            to: contract,
+            to: contract_address,
             selector: get_selector_from_name(&entrypoint).unwrap(),
         }])
         .send()
