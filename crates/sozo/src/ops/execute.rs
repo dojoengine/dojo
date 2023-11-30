4034c27240d4765299272e72b86c38fdf31bd570
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2023-11-30 13:32:54 +0100

    move network req

--- crates/sozo/src/ops/execute.rs
+++ crates/sozo/src/ops/execute.rs
@@ -14,26 +14,26 @@ pub async fn execute(args: ExecuteArgs, env_metadata: Option<Environment>) -> Re
 
     let provider = starknet.provider(env_metadata.as_ref())?;
 
-    let world_address = env_metadata
-        .as_ref()
-        .and_then(|env| env.world_address.as_ref())
-        .cloned()
-        .ok_or_else(|| anyhow::anyhow!("No World Address found"))?;
-
-    let contract_class_hash = provider
-        .call(
-            FunctionCall {
-                contract_address: FieldElement::from_hex_be(&world_address).unwrap(),
-                entry_point_selector: selector!("base"),
-                calldata: [].to_vec(),
-            },
-            BlockId::Tag(BlockTag::Latest),
-        )
-        .await?;
-
     let contract_address = if contract.starts_with("0x") {
         FieldElement::from_hex_be(&contract)?
     } else {
+        let world_address = env_metadata
+            .as_ref()
+            .and_then(|env| env.world_address.as_ref())
+            .cloned()
+            .ok_or_else(|| anyhow::anyhow!("No World Address found"))?;
+
+        let contract_class_hash = provider
+            .call(
+                FunctionCall {
+                    contract_address: FieldElement::from_hex_be(&world_address).unwrap(),
+                    entry_point_selector: selector!("base"),
+                    calldata: [].to_vec(),
+                },
+                BlockId::Tag(BlockTag::Latest),
+            )
+            .await?;
+
         get_contract_address(
             generate_salt(&contract),
             contract_class_hash[0],
