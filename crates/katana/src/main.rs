e7939bfcff839ca20c663857ae29e458185b1bd8
Tarrence van As <tarrencev@users.noreply.github.com>
2023-11-30 14:15:00 +0200

    Update main.rs

--- crates/katana/src/main.rs
+++ crates/katana/src/main.rs
@@ -105,7 +105,7 @@ PREDEPLOYED CONTRACTS
 | Address         | {}
 | Class Hash      | {}
 
-| Contract        | Universal Deployer UDC
+| Contract        | Universal Deployer
 | Address         | {}
 | Class Hash      | {}
 
