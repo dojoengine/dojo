d87eee20c6762d70d7e3755e9ddcd9f7a39d1e4e
Kariy <ammr.arf@gmail.com>
2023-12-04 22:01:56 +0800

    feat(katana-primitives): add more types + util functions

---
+++ crates/katana/primitives/src/receipt.rs
@@ -0,0 +1,104 @@
+use std::collections::HashMap;
+
+use starknet::core::types::{Event, MsgToL1};
+
+use crate::contract::ContractAddress;
+
+/// A mapping of execution resource to the amount used.
+pub type ExecutionResources = HashMap<String, usize>;
+
+/// Receipt for a `Invoke` transaction.
+#[derive(Debug, Clone)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub struct InvokeTxReceipt {
+    /// Actual fee paid for the transaction.
+    pub actual_fee: u128,
+    /// Events emitted by contracts.
+    pub events: Vec<Event>,
+    /// Messages sent to L1.
+    pub messages_sent: Vec<MsgToL1>,
+    /// Revert error message if the transaction execution failed.
+    pub revert_error: Option<String>,
+    /// The execution resources used by the transaction.
+    pub actual_resources: ExecutionResources,
+}
+
+/// Receipt for a `Declare` transaction.
+#[derive(Debug, Clone)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub struct DeclareTxReceipt {
+    /// Actual fee paid for the transaction.
+    pub actual_fee: u128,
+    /// Events emitted by contracts.
+    pub events: Vec<Event>,
+    /// Messages sent to L1.
+    pub messages_sent: Vec<MsgToL1>,
+    /// Revert error message if the transaction execution failed.
+    pub revert_error: Option<String>,
+    /// The execution resources used by the transaction.
+    pub actual_resources: ExecutionResources,
+}
+
+/// Receipt for a `L1Handler` transaction.
+#[derive(Debug, Clone)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub struct L1HandlerTxReceipt {
+    /// Actual fee paid for the transaction.
+    pub actual_fee: u128,
+    /// Events emitted by contracts.
+    pub events: Vec<Event>,
+    /// Messages sent to L1.
+    pub messages_sent: Vec<MsgToL1>,
+    /// Revert error message if the transaction execution failed.
+    pub revert_error: Option<String>,
+    /// The execution resources used by the transaction.
+    pub actual_resources: ExecutionResources,
+}
+
+/// Receipt for a `DeployAccount` transaction.
+#[derive(Debug, Clone)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub struct DeployAccountTxReceipt {
+    /// Actual fee paid for the transaction.
+    pub actual_fee: u128,
+    /// Events emitted by contracts.
+    pub events: Vec<Event>,
+    /// Messages sent to L1.
+    pub messages_sent: Vec<MsgToL1>,
+    /// Revert error message if the transaction execution failed.
+    pub revert_error: Option<String>,
+    /// The execution resources used by the transaction.
+    pub actual_resources: ExecutionResources,
+    /// Contract address of the deployed account contract.
+    pub contract_address: ContractAddress,
+}
+
+/// The receipt of a transaction containing the outputs of its execution.
+#[derive(Debug, Clone)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub enum Receipt {
+    Invoke(InvokeTxReceipt),
+    Declare(DeclareTxReceipt),
+    L1Handler(L1HandlerTxReceipt),
+    DeployAccount(DeployAccountTxReceipt),
+}
+
+impl Receipt {
+    pub fn messages_sent(&self) -> &[MsgToL1] {
+        match self {
+            Receipt::Invoke(rct) => &rct.messages_sent,
+            Receipt::Declare(rct) => &rct.messages_sent,
+            Receipt::L1Handler(rct) => &rct.messages_sent,
+            Receipt::DeployAccount(rct) => &rct.messages_sent,
+        }
+    }
+
+    pub fn events(&self) -> &[Event] {
+        match self {
+            Receipt::Invoke(rct) => &rct.events,
+            Receipt::Declare(rct) => &rct.events,
+            Receipt::L1Handler(rct) => &rct.events,
+            Receipt::DeployAccount(rct) => &rct.events,
+        }
+    }
+}
---
+++ crates/katana/primitives/src/state.rs
@@ -0,0 +1,33 @@
+use std::collections::HashMap;
+
+use crate::contract::{
+    ClassHash, CompiledClassHash, CompiledContractClass, ContractAddress, Nonce, SierraClass,
+    StorageKey, StorageValue,
+};
+
+/// State updates.
+///
+/// Represents all the state updates after performing some executions on a state.
+#[derive(Debug, Default, Clone)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub struct StateUpdates {
+    /// A mapping of contract addresses to their updated nonces.
+    pub nonce_updates: HashMap<ContractAddress, Nonce>,
+    /// A mapping of contract addresses to their updated storage entries.
+    pub storage_updates: HashMap<ContractAddress, HashMap<StorageKey, StorageValue>>,
+    /// A mapping of contract addresses to their updated class hashes.
+    pub contract_updates: HashMap<ContractAddress, ClassHash>,
+    /// A mapping of newly declared class hashes to their compiled class hashes.
+    pub declared_classes: HashMap<ClassHash, CompiledClassHash>,
+}
+
+/// State update with declared classes definition.
+#[derive(Debug, Default, Clone)]
+pub struct StateUpdatesWithDeclaredClasses {
+    /// State updates.
+    pub state_updates: StateUpdates,
+    /// A mapping of class hashes to their sierra classes definition.
+    pub declared_sierra_classes: HashMap<ClassHash, SierraClass>,
+    /// A mapping of class hashes to their compiled classes definition.
+    pub declared_compiled_classes: HashMap<ClassHash, CompiledContractClass>,
+}
