b0eda1833ce46d85d0aaa53f58b14eedd8427671
gianalarcon <gian.alarcon@unmsm.edu.pe>
2023-12-05 00:12:23 -0500

    rollback records contract

--- crates/torii/graphql/src/tests/mod.rs
+++ crates/torii/graphql/src/tests/mod.rs
@@ -269,7 +269,7 @@ pub async fn spinup_types_test() -> Result<SqlitePool> {
     execute_strategy(&ws, &migration, &account, None).await.unwrap();
 
     //  Execute `create` and insert 10 records into storage
-    let records_contract = "0x2753d30656b393ecea156189bf0acf5e1063f3ac978fb5c3cebe7a4570bbc78";
+    let records_contract = "0x2e6254aaf7e47502319f35de01376cece263f9b83afe6169a4b3a76ef47c8a3";
     let InvokeTransactionResult { transaction_hash } = account
         .execute(vec![Call {
             calldata: vec![FieldElement::from_str("0xa").unwrap()],
