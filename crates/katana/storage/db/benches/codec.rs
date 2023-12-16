90ff46eaad9e4060c49ff41dccc7705b01ced69b
Kariy <ammr.arf@gmail.com>
2023-12-16 08:58:46 +0800

    update

--- crates/katana/storage/db/benches/codec.rs
+++ crates/katana/storage/db/benches/codec.rs
@@ -1,6 +1,5 @@
 use blockifier::execution::contract_class::ContractClassV1;
 use cairo_lang_starknet::casm_contract_class::CasmContractClass;
-use cairo_lang_starknet::contract_class::ContractClass;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use katana_db::codecs::{Compress, Decompress};
 use katana_db::models::contract::StoredContractClass;
@@ -8,8 +7,7 @@ use katana_primitives::contract::CompiledContractClass;
 
 fn compress_contract(contract: CompiledContractClass) -> Vec<u8> {
     let class = StoredContractClass::from(contract);
-    let compressed = class.compress();
-    compressed
+    class.compress()
 }
 
 fn decompress_contract(compressed: &[u8]) -> CompiledContractClass {
@@ -18,10 +16,12 @@ fn decompress_contract(compressed: &[u8]) -> CompiledContractClass {
 }
 
 fn compress_contract_with_main_codec(c: &mut Criterion) {
-    let class: ContractClass =
-        serde_json::from_str(include_str!("artifacts/dojo::world::world.json")).unwrap();
-    let class = CasmContractClass::from_contract_class(class, true).unwrap();
-    let class = CompiledContractClass::V1(ContractClassV1::try_from(class).unwrap());
+    let class = {
+        let class =
+            serde_json::from_slice(include_bytes!("./artifacts/dojo_world_240.json")).unwrap();
+        let class = CasmContractClass::from_contract_class(class, true).unwrap();
+        CompiledContractClass::V1(ContractClassV1::try_from(class).unwrap())
+    };
 
     c.bench_function("compress world contract", |b| {
         b.iter_with_large_drop(|| compress_contract(black_box(class.clone())))
@@ -29,10 +29,13 @@ fn compress_contract_with_main_codec(c: &mut Criterion) {
 }
 
 fn decompress_contract_with_main_codec(c: &mut Criterion) {
-    let class: ContractClass =
-        serde_json::from_str(include_str!("artifacts/dojo::world::world.json")).unwrap();
-    let class = CasmContractClass::from_contract_class(class, true).unwrap();
-    let class = CompiledContractClass::V1(ContractClassV1::try_from(class).unwrap());
+    let class = {
+        let class =
+            serde_json::from_slice(include_bytes!("./artifacts/dojo_world_240.json")).unwrap();
+        let class = CasmContractClass::from_contract_class(class, true).unwrap();
+        CompiledContractClass::V1(ContractClassV1::try_from(class).unwrap())
+    };
+
     let compressed = compress_contract(class);
 
     c.bench_function("decompress world contract", |b| {
