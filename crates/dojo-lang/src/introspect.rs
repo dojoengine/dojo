c36342b72fcc44aa3af18eb49b64e62d0f5ddb5f
glihm <dev@glihm.net>
2023-12-21 17:48:30 -0600

    fix: remove condition that can be skipped

--- crates/dojo-lang/src/introspect.rs
+++ crates/dojo-lang/src/introspect.rs
@@ -275,17 +275,7 @@ fn handle_introspect_internal(
         }
     });
 
-    if size_precompute > 0 {
-        size.push(format!("{}", size_precompute));
-    }
-
-    if size.is_empty() {
-        // If the introspected node has only keys,
-        // the expected size in database is 0 as keys are
-        // not stored. Only models are affected by this,
-        // and `model.rs` already handle this situation.
-        size.push(format!("{}", 0));
-    }
+    size.push(format!("{}", size_precompute));
 
     RewriteNode::interpolate_patched(
         "
