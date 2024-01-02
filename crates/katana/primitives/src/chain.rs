8be8d70e40b4064e323cb59f40758bdded79e3f7
Kariy <ammr.arf@gmail.com>
2024-01-03 07:23:14 +0800

    update

--- crates/katana/primitives/src/chain.rs
+++ crates/katana/primitives/src/chain.rs
@@ -1,30 +1,6 @@
 use starknet::core::types::{FieldElement, FromStrError};
 use starknet::core::utils::{cairo_short_string_to_felt, CairoShortStringToFeltError};
 
-/// `SN_MAIN` in ASCII
-const SN_MAIN: FieldElement = FieldElement::from_mont([
-    0xf596341657d6d657,
-    0xffffffffffffffff,
-    0xffffffffffffffff,
-    0x6f9757bd5443bc6,
-]);
-
-/// `SN_GOERLI` in ASCII
-const SN_GOERLI: FieldElement = FieldElement::from_mont([
-    0x3417161755cc97b2,
-    0xfffffffffffff596,
-    0xffffffffffffffff,
-    0x588778cb29612d1,
-]);
-
-/// `SN_SEPOLIA` in ASCII
-const SN_SEPOLIA: FieldElement = FieldElement::from_mont([
-    0x159755f62c97a933,
-    0xfffffffffff59634,
-    0xffffffffffffffff,
-    0x70cb558f6123c62,
-]);
-
 /// Known chain ids that has been assigned a name.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, strum_macros::Display)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
@@ -35,14 +11,38 @@ pub enum NamedChainId {
 }
 
 impl NamedChainId {
+    /// `SN_MAIN` in ASCII
+    const SN_MAIN: FieldElement = FieldElement::from_mont([
+        0xf596341657d6d657,
+        0xffffffffffffffff,
+        0xffffffffffffffff,
+        0x6f9757bd5443bc6,
+    ]);
+
+    /// `SN_GOERLI` in ASCII
+    const SN_GOERLI: FieldElement = FieldElement::from_mont([
+        0x3417161755cc97b2,
+        0xfffffffffffff596,
+        0xffffffffffffffff,
+        0x588778cb29612d1,
+    ]);
+
+    /// `SN_SEPOLIA` in ASCII
+    const SN_SEPOLIA: FieldElement = FieldElement::from_mont([
+        0x159755f62c97a933,
+        0xfffffffffff59634,
+        0xffffffffffffffff,
+        0x70cb558f6123c62,
+    ]);
+
     /// Returns the id of the chain. It is the ASCII representation of a predefined string
     /// constants.
     #[inline]
     pub const fn id(&self) -> FieldElement {
         match self {
-            NamedChainId::Mainnet => SN_MAIN,
-            NamedChainId::Goerli => SN_GOERLI,
-            NamedChainId::Sepolia => SN_SEPOLIA,
+            NamedChainId::Mainnet => Self::SN_MAIN,
+            NamedChainId::Goerli => Self::SN_GOERLI,
+            NamedChainId::Sepolia => Self::SN_SEPOLIA,
         }
     }
 }
@@ -55,11 +55,11 @@ pub struct NamedChainTryFromError(FieldElement);
 impl TryFrom<FieldElement> for NamedChainId {
     type Error = NamedChainTryFromError;
     fn try_from(value: FieldElement) -> Result<Self, Self::Error> {
-        if value == SN_MAIN {
+        if value == Self::SN_MAIN {
             Ok(Self::Mainnet)
-        } else if value == SN_GOERLI {
+        } else if value == Self::SN_GOERLI {
             Ok(Self::Goerli)
-        } else if value == SN_SEPOLIA {
+        } else if value == Self::SN_SEPOLIA {
             Ok(Self::Sepolia)
         } else {
             Err(NamedChainTryFromError(value))
@@ -85,6 +85,10 @@ pub enum ParseChainIdError {
 }
 
 impl ChainId {
+    pub const MAINNET: Self = Self::Named(NamedChainId::Mainnet);
+    pub const GOERLI: Self = Self::Named(NamedChainId::Goerli);
+    pub const SEPOLIA: Self = Self::Named(NamedChainId::Sepolia);
+
     /// Parse a [`ChainId`] from a [`str`].
     ///
     /// If the `str` starts with `0x` it is parsed as a hex string, otherwise it is parsed as a
@@ -175,18 +179,18 @@ mod tests {
         let goerli_id = cairo_short_string_to_felt("SN_GOERLI").unwrap();
         let sepolia_id = cairo_short_string_to_felt("SN_SEPOLIA").unwrap();
 
-        assert_eq!(ChainId::Named(NamedChainId::Mainnet).id(), NamedChainId::Mainnet.id());
-        assert_eq!(ChainId::Named(NamedChainId::Goerli).id(), NamedChainId::Goerli.id());
-        assert_eq!(ChainId::Named(NamedChainId::Sepolia).id(), NamedChainId::Sepolia.id());
+        assert_eq!(ChainId::MAINNET.id(), NamedChainId::Mainnet.id());
+        assert_eq!(ChainId::GOERLI.id(), NamedChainId::Goerli.id());
+        assert_eq!(ChainId::SEPOLIA.id(), NamedChainId::Sepolia.id());
 
-        assert_eq!(ChainId::from(mainnet_id), ChainId::Named(NamedChainId::Mainnet));
-        assert_eq!(ChainId::from(goerli_id), ChainId::Named(NamedChainId::Goerli));
-        assert_eq!(ChainId::from(sepolia_id), ChainId::Named(NamedChainId::Sepolia));
+        assert_eq!(ChainId::from(mainnet_id), ChainId::MAINNET);
+        assert_eq!(ChainId::from(goerli_id), ChainId::GOERLI);
+        assert_eq!(ChainId::from(sepolia_id), ChainId::SEPOLIA);
         assert_eq!(ChainId::from(felt!("0x1337")), ChainId::Id(felt!("0x1337")));
 
-        assert_eq!(ChainId::Named(NamedChainId::Mainnet).to_string(), "Mainnet");
-        assert_eq!(ChainId::Named(NamedChainId::Goerli).to_string(), "Goerli");
-        assert_eq!(ChainId::Named(NamedChainId::Sepolia).to_string(), "Sepolia");
+        assert_eq!(ChainId::MAINNET.to_string(), "Mainnet");
+        assert_eq!(ChainId::GOERLI.to_string(), "Goerli");
+        assert_eq!(ChainId::SEPOLIA.to_string(), "Sepolia");
         assert_eq!(ChainId::Id(felt!("0x1337")).to_string(), "0x1337");
     }
 
@@ -195,11 +199,8 @@ mod tests {
         let mainnet_id = cairo_short_string_to_felt("SN_MAIN").unwrap();
         let custom_id = cairo_short_string_to_felt("KATANA").unwrap();
 
-        assert_eq!(ChainId::parse("SN_MAIN").unwrap(), ChainId::Named(NamedChainId::Mainnet));
+        assert_eq!(ChainId::parse("SN_MAIN").unwrap(), ChainId::MAINNET);
         assert_eq!(ChainId::parse("KATANA").unwrap(), ChainId::Id(custom_id));
-        assert_eq!(
-            ChainId::parse(&format!("{mainnet_id:#x}")).unwrap(),
-            ChainId::Named(NamedChainId::Mainnet)
-        );
+        assert_eq!(ChainId::parse(&format!("{mainnet_id:#x}")).unwrap(), ChainId::MAINNET);
     }
 }
