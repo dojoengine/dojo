5db79a5910a58833b24ec67ee4f649a70cc7b5e4
Nasr <larko236@outlook.com>
2024-01-02 12:10:45 -0500

    fix: parse error

--- crates/torii/client/src/client/mod.rs
+++ crates/torii/client/src/client/mod.rs
@@ -54,7 +54,10 @@ impl Client {
         models_keys: Option<Vec<KeysClause>>,
     ) -> Result<Self, Error> {
         let tls = ClientTlsConfig::default();
-        let torii_url = Channel::from_shared(torii_url).map_err(ParseError::Uri)?.tls_config(tls);
+        let torii_url = Channel::from_shared(torii_url)
+            .map_err(|_| ParseError::Url)?
+            .tls_config(tls)
+            .map_err(|_| ParseError::Url)?;
         let mut grpc_client = torii_grpc::client::WorldClient::new(torii_url, world).await?;
 
         let metadata = grpc_client.metadata().await?;
