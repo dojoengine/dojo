ba180bdbd17d64fd5a9a0ba89741188cb224fd99
lambda-0x <0xlambda@protonmail.com>
2024-01-09 23:59:00 +0530

    tests: add tests for options/account

--- crates/sozo/src/commands/options/account.rs
+++ crates/sozo/src/commands/options/account.rs
@@ -33,7 +33,6 @@ pub struct AccountOptions {
 
     #[arg(long = "password", env = DOJO_KEYSTORE_PASSWORD_ENV_VAR)]
     #[arg(value_name = "PASSWORD")]
-    #[arg(requires = "keystore_path")]
     #[arg(help_heading = "Signer options - KEYSTORE")]
     #[arg(help = "The keystore password. Used with --keystore.")]
     pub keystore_password: Option<String>,
@@ -64,6 +63,11 @@ impl AccountOptions {
     }
 
     fn signer(&self, env_metadata: Option<&Environment>) -> Result<LocalWallet> {
+        // INVARIANT: We assume that from commandline we can either specify `private_key` along with
+        // `account_address` or `keystore_path` along with `keystore_password`.
+        //
+        // This is enforced by clap.
+        // TODO: See if there is any way to test it.
         if let Some(private_key) =
             self.private_key.as_deref().or_else(|| env_metadata.and_then(|env| env.private_key()))
         {
@@ -72,7 +76,11 @@ impl AccountOptions {
             )));
         }
 
-        if let Some(path) = &self.keystore_path {
+        if let Some(path) = &self
+            .keystore_path
+            .as_deref()
+            .or_else(|| env_metadata.and_then(|env| env.keystore_path()))
+        {
             if let Some(password) = self
                 .keystore_password
                 .as_deref()
@@ -108,12 +116,16 @@ impl AccountOptions {
 
 #[cfg(test)]
 mod tests {
-    use super::AccountOptions;
-    use super::{
-        DOJO_ACCOUNT_ADDRESS_ENV_VAR, DOJO_KEYSTORE_PASSWORD_ENV_VAR, DOJO_PRIVATE_KEY_ENV_VAR,
-    };
+    use std::str::FromStr;
+
     use clap::Parser;
+    use starknet::signers::{LocalWallet, Signer, SigningKey};
     use starknet_crypto::FieldElement;
+
+    use super::{
+        AccountOptions, DOJO_ACCOUNT_ADDRESS_ENV_VAR, DOJO_KEYSTORE_PASSWORD_ENV_VAR,
+        DOJO_PRIVATE_KEY_ENV_VAR,
+    };
 
     #[derive(clap::Parser, Debug)]
     struct Command {
@@ -164,7 +176,6 @@ mod tests {
         };
 
         let cmd = Command::parse_from([""]);
-
         assert_eq!(
             cmd.account.account_address(Some(&env_metadata)).unwrap(),
             FieldElement::from_hex_be("0x0").unwrap()
@@ -179,7 +190,6 @@ mod tests {
         };
 
         let cmd = Command::parse_from(["sozo", "--account-address", "0x1"]);
-
         assert_eq!(
             cmd.account.account_address(Some(&env_metadata)).unwrap(),
             FieldElement::from_hex_be("0x1").unwrap()
@@ -191,7 +201,110 @@ mod tests {
         let env_metadata = dojo_world::metadata::Environment::default();
 
         let cmd = Command::parse_from([""]);
+        assert!(cmd.account.account_address(Some(&env_metadata)).is_err());
+    }
 
-        assert!(cmd.account.account_address(Some(&env_metadata)).is_err(),);
+    #[tokio::test]
+    async fn private_key_from_args() {
+        let env_metadata = dojo_world::metadata::Environment::default();
+        let private_key = "0x1";
+
+        let cmd =
+            Command::parse_from(["sozo", "--account-address", "0x0", "--private-key", private_key]);
+        let result_wallet = cmd.account.signer(Some(&env_metadata)).unwrap();
+        let expected_wallet = LocalWallet::from_signing_key(SigningKey::from_secret_scalar(
+            FieldElement::from_str(private_key).unwrap(),
+        ));
+
+        let result_public_key = result_wallet.get_public_key().await.unwrap();
+        let expected_public_key = expected_wallet.get_public_key().await.unwrap();
+        assert!(result_public_key.scalar() == expected_public_key.scalar());
+    }
+
+    #[tokio::test]
+    async fn private_key_from_env_metadata() {
+        let private_key = "0x1";
+        let env_metadata = dojo_world::metadata::Environment {
+            private_key: Some(private_key.to_owned()),
+            ..Default::default()
+        };
+
+        let cmd = Command::parse_from(["sozo", "--account-address", "0x0"]);
+        let result_wallet = cmd.account.signer(Some(&env_metadata)).unwrap();
+        let expected_wallet = LocalWallet::from_signing_key(SigningKey::from_secret_scalar(
+            FieldElement::from_str(private_key).unwrap(),
+        ));
+
+        let result_public_key = result_wallet.get_public_key().await.unwrap();
+        let expected_public_key = expected_wallet.get_public_key().await.unwrap();
+        assert!(result_public_key.scalar() == expected_public_key.scalar());
+    }
+
+    #[tokio::test]
+    async fn keystore_path_and_keystore_password_from_args() {
+        let keystore_path = "./tests/test_data/keystore/test.json";
+        let keystore_password = "dojoftw";
+        let private_key = "0x1";
+        let env_metadata = dojo_world::metadata::Environment::default();
+
+        let cmd = Command::parse_from([
+            "sozo",
+            "--keystore",
+            keystore_path,
+            "--password",
+            keystore_password,
+        ]);
+        let result_wallet = cmd.account.signer(Some(&env_metadata)).unwrap();
+        let expected_wallet = LocalWallet::from_signing_key(SigningKey::from_secret_scalar(
+            FieldElement::from_str(private_key).unwrap(),
+        ));
+
+        let result_public_key = result_wallet.get_public_key().await.unwrap();
+        let expected_public_key = expected_wallet.get_public_key().await.unwrap();
+        assert!(result_public_key.scalar() == expected_public_key.scalar());
+    }
+
+    #[tokio::test]
+    async fn keystore_path_from_env_metadata() {
+        let keystore_path = "./tests/test_data/keystore/test.json";
+        let keystore_password = "dojoftw";
+
+        let private_key = "0x1";
+        let env_metadata = dojo_world::metadata::Environment {
+            keystore_path: Some(keystore_path.to_owned()),
+            ..Default::default()
+        };
+
+        let cmd = Command::parse_from(["sozo", "--password", keystore_password]);
+        let result_wallet = cmd.account.signer(Some(&env_metadata)).unwrap();
+        let expected_wallet = LocalWallet::from_signing_key(SigningKey::from_secret_scalar(
+            FieldElement::from_str(private_key).unwrap(),
+        ));
+
+        let result_public_key = result_wallet.get_public_key().await.unwrap();
+        let expected_public_key = expected_wallet.get_public_key().await.unwrap();
+        assert!(result_public_key.scalar() == expected_public_key.scalar());
+    }
+
+    #[tokio::test]
+    async fn keystore_password_from_env_metadata() {
+        let keystore_path = "./tests/test_data/keystore/test.json";
+        let keystore_password = "dojoftw";
+        let private_key = "0x1";
+
+        let env_metadata = dojo_world::metadata::Environment {
+            keystore_password: Some(keystore_password.to_owned()),
+            ..Default::default()
+        };
+
+        let cmd = Command::parse_from(["sozo", "--keystore", keystore_path]);
+        let result_wallet = cmd.account.signer(Some(&env_metadata)).unwrap();
+        let expected_wallet = LocalWallet::from_signing_key(SigningKey::from_secret_scalar(
+            FieldElement::from_str(private_key).unwrap(),
+        ));
+
+        let result_public_key = result_wallet.get_public_key().await.unwrap();
+        let expected_public_key = expected_wallet.get_public_key().await.unwrap();
+        assert!(result_public_key.scalar() == expected_public_key.scalar());
     }
 }
