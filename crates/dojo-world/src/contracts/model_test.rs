72646199e31bddc96a35f1adcc3c9de8e4efd99a
glihm <dev@glihm.net>
2024-01-13 17:05:45 -0600

    feat: add `ensure_abi` method into model generated contract (#1433)
    feat: add ensure_abi method into model generated contract

--- crates/dojo-world/src/contracts/model_test.rs
+++ crates/dojo-world/src/contracts/model_test.rs
@@ -63,7 +63,7 @@ async fn test_model() {
     assert_eq!(
         position.class_hash(),
         FieldElement::from_hex_be(
-            "0x004cd20d231b04405a77b184c115dc60637e186504fad7f0929bd76cbd09c10b"
+            "0x02a7df852d6ef0af662dad741b97b423ba0fb34a1483599781ac9e7f6822bc25"
         )
         .unwrap()
     );
