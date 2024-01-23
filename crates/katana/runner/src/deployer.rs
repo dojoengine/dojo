6943ea2a9e8d9f4e9471fa9c77a22715ba3b5141
Mateusz Zając <matzayonc@gmail.com>
2024-01-23 18:41:00 +0000

    deployed using sozo depencency

--- crates/katana/runner/src/deployer.rs
+++ crates/katana/runner/src/deployer.rs
@@ -1,38 +1,84 @@
-use anyhow::{Context, Ok, Result};
+use std::env;
+
+use anyhow::{anyhow, Context, Ok, Result};
+use clap::Parser;
+use dojo_lang::compiler::DojoCompiler;
+use dojo_lang::plugin::CairoPluginRepository;
+use dojo_lang::scarb_internal::compile_workspace;
+use scarb::compiler::CompilerRepository;
+use scarb::core::{Config, TargetKind};
+use scarb::ops::CompileOpts;
+use sozo::args::{Commands, SozoArgs};
+use sozo::ops::migration;
 use starknet::core::types::FieldElement;
+use starknet::macros::felt;
 use tokio::process::Command;
 
 use crate::KatanaRunner;
+
+async fn prepare_migration_args(args: SozoArgs) -> Result<()> {
+    // Preparing config, as in https://github.com/neotheprogramist/dojo/blob/fec863cb0375a684edd9e7e76c8fdeb9ebf386e6/bin/sozo/src/cli_entry.rs#L29
+    let mut compilers = CompilerRepository::std();
+    let cairo_plugins = CairoPluginRepository::default();
+    compilers.add(Box::new(DojoCompiler)).unwrap();
+    let manifest_path = scarb::ops::find_manifest_path(args.manifest_path.as_deref())?;
+
+    let config = Config::builder(manifest_path)
+        .log_filter_directive(env::var_os("SCARB_LOG"))
+        .profile(args.profile_spec.determine()?)
+        .offline(args.offline)
+        .cairo_plugins(cairo_plugins.into())
+        .ui_verbosity(args.ui_verbosity())
+        .compilers(compilers)
+        .build()
+        .context("failed to build config")?;
+
+    // Extractiong migration command, as here https://github.com/neotheprogramist/dojo/blob/fec863cb0375a684edd9e7e76c8fdeb9ebf386e6/bin/sozo/src/commands/mod.rs#L24
+    let mut migrate = match args.command {
+        Commands::Migrate(migrate) => *migrate,
+        _ => return Err(anyhow!("failed to parse migrate args")),
+    };
+
+    // Preparing workspace, as in https://github.com/neotheprogramist/dojo/blob/fec863cb0375a684edd9e7e76c8fdeb9ebf386e6/bin/sozo/src/commands/migrate.rs#L40-L41
+    let ws = scarb::ops::read_workspace(config.manifest_path(), &config)?;
+    if migrate.name.is_none() {
+        if let Some(root_package) = ws.root_package() {
+            migrate.name = Some(root_package.id.name.to_string());
+        }
+    }
+
+    let target_dir = ws.target_dir().path_existent().unwrap();
+    let target_dir = target_dir.join(ws.config().profile().as_str());
+
+    if !target_dir.join("manifest.json").exists() {
+        compile_workspace(
+            &config,
+            CompileOpts { include_targets: vec![], exclude_targets: vec![TargetKind::TEST] },
+        )?;
+    }
+
+    migration::execute(&ws, migrate, target_dir).await?;
+    Ok(())
+}
 
 impl KatanaRunner {
     /// Known issue - rpc set in Scarb.toml overrides command line argument
     pub async fn deploy(&self, manifest: &str, script: &str) -> Result<FieldElement> {
         let rpc_url = &format!("http://localhost:{}", self.port);
 
-        let out = Command::new("sozo")
-            .arg("migrate")
-            .args(["--rpc-url", rpc_url])
-            .args(["--manifest-path", manifest])
-            .output()
-            .await
-            .context("failed to start subprocess")?;
+        let args = SozoArgs::parse_from(&[
+            "sozo",
+            "migrate",
+            "--rpc-url",
+            rpc_url,
+            "--manifest-path",
+            manifest,
+        ]);
 
-        if !out.status.success() {
-            return Err(anyhow::anyhow!("deploy failed {:?}", out));
-        }
+        prepare_migration_args(args).await?;
 
-        let constract_address = String::from_utf8_lossy(&out.stdout)
-            .lines()
-            .rev()
-            .find(|l| l.contains("> Contract address:"))
-            .expect("failed to find contract address")
-            .split_whitespace()
-            .last()
-            .expect("failed to get contract address")
-            .to_owned();
-
-        let constract_address = FieldElement::from_hex_be(&constract_address)
-            .expect("failed to parse contract address");
+        let constract_address =
+            felt!("0x38979e719956897617c83fcbc69de9bc56491fd10c093dd8492b92ee7326d98");
 
         let out = Command::new("bash")
             .arg(script)
