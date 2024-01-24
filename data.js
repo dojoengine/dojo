91bda670deb8ae99a9410ee2c563af470eb57b83
github-action-benchmark <github@users.noreply.github.com>
2024-01-24 16:37:45 +0000

    add Benchmark (cargo) benchmark result for 67305d132bcfca329d69331b0655e0a63a36584b

---
+++ data.js
@@ -0,0 +1,34 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1706114265632,
+  "repoUrl": "https://github.com/dojoengine/dojo",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "dojoengine",
+            "username": "dojoengine"
+          },
+          "committer": {
+            "name": "dojoengine",
+            "username": "dojoengine"
+          },
+          "id": "67305d132bcfca329d69331b0655e0a63a36584b",
+          "message": "Enable benches in ci",
+          "timestamp": "2024-01-21T20:35:22Z",
+          "url": "https://github.com/dojoengine/dojo/pull/1474/commits/67305d132bcfca329d69331b0655e0a63a36584b"
+        },
+        "date": 1706114264610,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "decompress world contract",
+            "value": 1034519,
+            "range": "± 37812",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
