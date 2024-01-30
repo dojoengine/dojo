32ff50b2647dc863cb2719c2ef12fa04223daf46
Nasr <larko236@outlook.com>
2024-01-30 01:55:39 -0500

    refactor: return arc cloen

--- crates/torii/client/src/client/mod.rs
+++ crates/torii/client/src/client/mod.rs
@@ -11,7 +11,7 @@ use dojo_types::schema::Ty;
 use dojo_types::WorldMetadata;
 use dojo_world::contracts::WorldContractReader;
 use futures::channel::mpsc::UnboundedReceiver;
-use futures_util::lock::MutexGuard;
+use futures_util::lock::Mutex;
 use parking_lot::{RwLock, RwLockReadGuard};
 use starknet::core::utils::cairo_short_string_to_felt;
 use starknet::providers::jsonrpc::HttpTransport;
@@ -127,13 +127,13 @@ impl Client {
 
     /// Returns the event loop of the relay client.
     /// Which can then be used to run the relay client
-    pub async fn relay_client_runner(&self) -> MutexGuard<'_, EventLoop> {
-        self.relay_client.event_loop.lock().await
+    pub fn relay_client_runner(&self) -> Arc<Mutex<EventLoop>> {
+        self.relay_client.event_loop.clone()
     }
 
     /// Returns the message receiver of the relay client.
-    pub async fn relay_client_stream(&self) -> MutexGuard<'_, UnboundedReceiver<Message>> {
-        self.relay_client.message_receiver.lock().await
+    pub fn relay_client_stream(&self) -> Arc<Mutex<UnboundedReceiver<Message>>> {
+        self.relay_client.message_receiver.clone()
     }
 
     /// Returns a read lock on the World metadata that the client is connected to.
