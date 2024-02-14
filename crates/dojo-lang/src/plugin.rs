c54c7c05a63a776c6ae5aba23d348492fb210710
glihm <dev@glihm.net>
2024-02-14 15:06:26 -0600

    fix: fmt

--- crates/dojo-lang/src/plugin.rs
+++ crates/dojo-lang/src/plugin.rs
@@ -430,7 +430,6 @@ impl MacroPlugin for BuiltinDojoPlugin {
             "dojo::contract".to_string(),
             "key".to_string(),
             "computed".to_string(),
-
             // Not adding capacity for now, this will automatically
             // makes Scarb emitting a diagnostic saying this attribute is not supported.
             // "capacity".to_string(),
