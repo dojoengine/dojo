ca3b018048ddbeaa3a78c8287143ba526e0f89a0
V.O.T <74396818+makluganteng@users.noreply.github.com>
2024-02-17 12:14:47 +0700

    refactor(katana-rpc): isolate dev JSON RPC API from `katana` namespace (#1544)
    * feat: isolate dev JSON RPC API from katana impl
    
    * feat: fix the changes
    
    * feat: remove the duplicate endpoint in katana

---
+++ crates/katana/rpc/rpc/src/dev.rs
@@ -0,0 +1,55 @@
+use std::sync::Arc;
+
+use jsonrpsee::core::{async_trait, Error};
+use katana_core::sequencer::KatanaSequencer;
+use katana_primitives::FieldElement;
+use katana_rpc_api::dev::DevApiServer;
+use katana_rpc_types::error::katana::KatanaApiError;
+
+pub struct DevApi {
+    sequencer: Arc<KatanaSequencer>,
+}
+
+impl DevApi {
+    pub fn new(sequencer: Arc<KatanaSequencer>) -> Self {
+        Self { sequencer }
+    }
+}
+
+#[async_trait]
+impl DevApiServer for DevApi {
+    async fn generate_block(&self) -> Result<(), Error> {
+        self.sequencer.block_producer().force_mine();
+        Ok(())
+    }
+
+    async fn next_block_timestamp(&self) -> Result<(), Error> {
+        // Ok(self.sequencer.backend().env.read().block.block_timestamp.0)
+        Ok(())
+    }
+
+    async fn set_next_block_timestamp(&self, timestamp: u64) -> Result<(), Error> {
+        self.sequencer
+            .set_next_block_timestamp(timestamp)
+            .map_err(|_| Error::from(KatanaApiError::FailedToChangeNextBlockTimestamp))
+    }
+
+    async fn increase_next_block_timestamp(&self, timestamp: u64) -> Result<(), Error> {
+        self.sequencer
+            .increase_next_block_timestamp(timestamp)
+            .map_err(|_| Error::from(KatanaApiError::FailedToChangeNextBlockTimestamp))
+    }
+
+    async fn set_storage_at(
+        &self,
+        _contract_address: FieldElement,
+        _key: FieldElement,
+        _value: FieldElement,
+    ) -> Result<(), Error> {
+        // self.sequencer
+        //     .set_storage_at(contract_address.into(), key, value)
+        //     .await
+        //     .map_err(|_| Error::from(KatanaApiError::FailedToUpdateStorage))
+        Ok(())
+    }
+}
