e22342e5ec31314fcb848447801a22b4b2b7e586
kwkr <kawka.maciej.93@gmail.com>
2024-02-24 11:08:51 +0100

    check existing class-hashes before registering model

--- bin/sozo/src/ops/register.rs
+++ bin/sozo/src/ops/register.rs
@@ -15,8 +15,39 @@ pub async fn execute(command: RegisterCommand, env_metadata: Option<Environment>
             let account = account.account(&provider, env_metadata.as_ref()).await?;
             let world = WorldContract::new(world_address, &account);
 
+            let world_reader = WorldContractReader::new(world_address, &provider)
+                .with_block(BlockId::Tag(BlockTag::Pending));
+            let remote_manifest = {
+                match Manifest::load_from_remote(&provider, world_address).await {
+                    Ok(manifest) => Some(manifest),
+                    Err(ManifestError::RemoteWorldNotFound) => None,
+                    Err(e) => {
+                        return Err(anyhow!("Failed to build remote World state: {e}"));
+                    }
+                }
+            };
+
+            let manifest = remote_manifest.unwrap();
+            let registered_models_names =
+                manifest.models.into_iter().map(|m| m.name).collect::<Vec<String>>();
+
+            let mut model_class_hashes = HashMap::new();
+            for model_name in registered_models_names.iter() {
+                let read_model = world_reader.model_reader(model_name).await?;
+                let class_hash = read_model.class_hash();
+                model_class_hashes.insert(class_hash, model_name.clone());
+            }
+
             let calls = models
                 .iter()
+                .filter(|m| {
+                    if model_class_hashes.contains_key(m) {
+                        let model_name = model_class_hashes.get(&m).unwrap();
+                        config.ui().print(format!("\"{model_name}\" model already registered with the given class hash"));
+                        return false;
+                    }
+                    true
+                })
                 .map(|c| world.register_model_getcall(&(*c).into()))
                 .collect::<Vec<_>>();
 
