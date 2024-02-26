e03d3eb46b20249368852967249cf4d782c6bdbc
Ammar Arif <evergreenkary@gmail.com>
2024-02-26 18:33:46 +0800

    fix clippy

--- scripts/clippy.sh
+++ scripts/clippy.sh
@@ -4,8 +4,8 @@ run_clippy() {
   cargo clippy --all-targets "$@" -- -D warnings -D future-incompatible -D nonstandard-style -D rust-2018-idioms -D unused
 }
 
-run_clippy --all-features --workspace --exclude katana,katana-executor
+run_clippy --all-features --workspace --exclude katana --exclude katana-executor
 
-run_clippy -p katana-executor --all-features
+run_clippy -p katana-executor --all-
 run_clippy -p katana
 run_clippy -p katana --no-default-features --features sir
