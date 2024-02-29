314361a9ea0f0c02d3740e159b6d09e827c210eb
Ammar Arif <evergreenkary@gmail.com>
2024-02-29 23:02:41 +0800

    feat(katana-executor): add starknet_in_rust executor (#1564)

--- crates/katana/executor/src/implementation/mod.rs
+++ crates/katana/executor/src/implementation/mod.rs
@@ -1,3 +1,7 @@
+#[cfg(feature = "blockifier")]
 pub mod blockifier;
+
+#[cfg(feature = "sir")]
+pub mod sir;
 
 pub mod noop;
--- scripts/docs.sh
+++ scripts/docs.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-RUSTDOCFLAGS="-Dwarnings" cargo doc --document-private-items --no-deps --all-features
+RUSTDOCFLAGS="-Dwarnings" cargo doc --document-private-items --no-deps --all-features --workspace --exclude katana
+RUSTDOCFLAGS="-Dwarnings" cargo doc --document-private-items --no-deps -p katana
