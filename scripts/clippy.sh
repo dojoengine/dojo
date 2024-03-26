61edb80ab140705b82365013701357822ae871e3
Thomas Coratger <thomas.coratger@gmail.com>
2024-03-27 00:30:14 +0100

    modify clippy script

--- scripts/clippy.sh
+++ scripts/clippy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Install Cargo Clippy for the specified toolchain
+rustup component add clippy --toolchain 1.76.0-x86_64-unknown-linux-gnu
+
 run_clippy() {
   cargo clippy --all-targets "$@" -- -D warnings -D future-incompatible -D nonstandard-style -D rust-2018-idioms -D unused
 }
