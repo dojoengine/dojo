a4ccc2ee1619c1dbb8a61749f19812eae545e49e
glihm <dev@glihm.net>
2024-03-26 16:00:57 -0600

    fix: add script to teardown tests and updated cargo lock (#1707)

---
+++ scripts/teardown_test_artifacts.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# When tests are run, the `build.rs` of `dojo-test-utils` is re-building the
+# cairo artifacts ONLY if they don't exist.
+# This script gives an easy way to remove those artifacts.
+
+rm -rf examples/spawn-and-move/target
+rm -rf crates/torii/types-test/target
