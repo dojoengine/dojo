afbfede93dcbe3169b8186a91b284d1c617caca4
Ammar Arif <evergreenkary@gmail.com>
2024-04-01 20:35:55 +0800

    fix(katana-core): dont use optional dep in non-feature specific code (#1746)
    before this, running cargo test -p katana-core would result in error bcs we're using ethers crate in non-feature code
    
    ref #1527

--- crates/katana/core/tests/sequencer.rs
+++ crates/katana/core/tests/sequencer.rs
@@ -1,4 +1,3 @@
-use ethers::types::U256;
 use katana_core::backend::config::{Environment, StarknetConfig};
 use katana_core::sequencer::{KatanaSequencer, SequencerConfig};
 use katana_executor::implementation::noop::NoopExecutorFactory;
@@ -7,6 +6,7 @@ use katana_primitives::genesis::constant::DEFAULT_PREFUNDED_ACCOUNT_BALANCE;
 use katana_primitives::genesis::Genesis;
 use katana_provider::traits::block::{BlockNumberProvider, BlockProvider};
 use katana_provider::traits::env::BlockEnvProvider;
+use primitive_types::U256;
 
 fn create_test_sequencer_config() -> (SequencerConfig, StarknetConfig) {
     let accounts = DevAllocationsGenerator::new(2)
