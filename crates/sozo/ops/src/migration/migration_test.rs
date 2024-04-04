789f4cbca2d16fc3565c164a2203e1bbb9289afc
glihm <dev@glihm.net>
2024-04-04 11:40:21 -0400

    fix: tests to be reworked

--- crates/sozo/ops/src/migration/migration_test.rs
+++ crates/sozo/ops/src/migration/migration_test.rs
@@ -171,29 +171,3 @@ async fn migration_from_remote() {
     assert_eq!(local_manifest.world.inner.class_hash, remote_manifest.world.inner.class_hash);
     assert_eq!(local_manifest.models.len(), remote_manifest.models.len());
 }
-
-#[tokio::test(flavor = "multi_thread")]
-async fn migrate_dry_run() {
-    let config = build_test_config("../../../examples/spawn-and-move/Scarb.toml").unwrap();
-    let ws = ops::read_workspace(config.manifest_path(), &config)
-        .unwrap_or_else(|op| panic!("Error building workspace: {op:?}"));
-
-    let sequencer =
-        TestSequencer::start(SequencerConfig::default(), get_default_test_starknet_config()).await;
-
-    let mut account = sequencer.account();
-    account.set_block_id(BlockId::Tag(BlockTag::Pending));
-
-    crate::migration::migrate(
-        &ws,
-        None,
-        "KATANA".to_string(),
-        &account,
-        Some("dojo_examples".to_string()),
-        true,
-    )
-    .await
-    .unwrap();
-
-    sequencer.stop().unwrap();
-}
