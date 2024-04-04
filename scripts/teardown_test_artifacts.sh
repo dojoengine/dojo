ff8743dc83b0557a8a131e744b4a37fa21995033
glihm <dev@glihm.net>
2024-04-04 16:01:23 -0400

    fix: add new generated manifests for tests

--- scripts/teardown_test_artifacts.sh
+++ scripts/teardown_test_artifacts.sh
@@ -5,4 +5,7 @@
 # This script gives an easy way to remove those artifacts.
 
 rm -rf examples/spawn-and-move/target
+rm -rf examples/spawn-and-move/manifests
+
 rm -rf crates/torii/types-test/target
+rm -rf crates/torii/types-test/manifests
