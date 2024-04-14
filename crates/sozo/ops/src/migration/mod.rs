fb2dde67547a28c14bd44a2e689133f5544a7891
Mateusz Zając <matzayonc@gmail.com>
2024-04-14 22:31:48 +0000

    serialization fixes

--- crates/sozo/ops/src/migration/mod.rs
+++ crates/sozo/ops/src/migration/mod.rs
@@ -1039,8 +1039,12 @@ where
 
     let calls = resources.iter().map(|r| world.set_metadata_getcall(r)).collect::<Vec<_>>();
 
-    let InvokeTransactionResult { transaction_hash } =
-        migrator.execute(calls).send_with_cfg(&txn_config).await.map_err(|e| {
+    let InvokeTransactionResult { transaction_hash } = migrator
+        .execute(calls)
+        .fee_estimate_multiplier(2.0) // TODO: Replace hardcoded value here
+        .send()
+        .await
+        .map_err(|e| {
             ui.verbose(format!("{e:?}"));
             anyhow!("Failed to register metadata into the resource registry: {e}")
         })?;
