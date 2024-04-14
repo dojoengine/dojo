beedae7fd389a3bd5029c399dbf128e6af586f9d
Mateusz Zając <matzayonc@gmail.com>
2024-04-14 20:30:02 +0000

    fetching transaction status

--- crates/saya/core/src/starknet_os/mod.rs
+++ crates/saya/core/src/starknet_os/mod.rs
@@ -7,15 +7,23 @@
 // pub mod input;
 // pub mod transaction;
 
+use std::time::Duration;
+
+use anyhow::bail;
 use itertools::chain;
+use starknet::accounts::ConnectedAccount;
 use starknet::accounts::{Account, Call, ExecutionEncoding, SingleOwnerAccount};
-use starknet::core::types::{BlockId, BlockTag, FieldElement};
+use starknet::core::types::{
+    BlockId, BlockTag, ExecuteInvocation, FieldElement, TransactionExecutionStatus,
+    TransactionStatus, TransactionTrace,
+};
 use starknet::core::utils::get_selector_from_name;
 use starknet::providers::jsonrpc::HttpTransport;
 use starknet::providers::JsonRpcClient;
+use starknet::providers::Provider;
 use starknet::signers::{LocalWallet, SigningKey};
+use tokio::time::sleep;
 use url::Url;
-
 // will need to be read from the environment for chains other than sepoia
 pub const STARKNET_URL: &str = "https://free-rpc.nethermind.io/sepolia-juno/v0_6";
 pub const CHAIN_ID: &str = "0x00000000000000000000000000000000000000000000534e5f5345504f4c4941";
@@ -52,6 +60,9 @@ pub async fn starknet_apply_diffs(
         program_output.into_iter()
     ]
     .collect();
+
+    println!("Calldata: {:?}", calldata);
+
     let tx = STARKNET_ACCOUNT
         .execute(vec![Call {
             to: FieldElement::from_hex_be(
@@ -64,6 +75,45 @@ pub async fn starknet_apply_diffs(
         .max_fee(starknet::macros::felt!("1000000000000000")) // sometimes failing without this line 
         .send()
         .await?;
+
+    let start_fetching = std::time::Instant::now();
+    let wait_for = Duration::from_secs(60);
+    let execution_status = loop {
+        if start_fetching.elapsed() > wait_for {
+            bail!("Transaction not mined in {} seconds.", wait_for.as_secs());
+        }
+
+        let status =
+            match STARKNET_ACCOUNT.provider().get_transaction_status(tx.transaction_hash).await {
+                Ok(status) => status,
+                Err(_e) => {
+                    sleep(Duration::from_secs(1)).await;
+                    continue;
+                }
+            };
+
+        break match status {
+            TransactionStatus::Received => {
+                println!("Transaction received.");
+                sleep(Duration::from_secs(1)).await;
+                continue;
+            }
+            TransactionStatus::Rejected => {
+                bail!("Transaction {:#x} rejected.", tx.transaction_hash);
+            }
+            TransactionStatus::AcceptedOnL2(execution_status) => execution_status,
+            TransactionStatus::AcceptedOnL1(execution_status) => execution_status,
+        };
+    };
+
+    match execution_status {
+        TransactionExecutionStatus::Succeeded => {
+            println!("Transaction accepted on L2.");
+        }
+        TransactionExecutionStatus::Reverted => {
+            bail!("Transaction failed with.");
+        }
+    }
 
     Ok(format!("{:#x}", tx.transaction_hash))
 }
