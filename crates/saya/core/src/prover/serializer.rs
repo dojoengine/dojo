62bf5a2a5c184d8fae995add9d61037638d1fcb9
Neo <neotheprogramist@proton.me>
2024-04-17 14:09:01 +0000

    improvments

--- crates/saya/core/src/prover/serializer.rs
+++ crates/saya/core/src/prover/serializer.rs
@@ -2,6 +2,6 @@ use anyhow::Ok;
 use cairo_proof_parser::parse;
 use starknet::core::types::FieldElement;
 
-pub fn parse_proof(proof: String) -> anyhow::Result<Vec<FieldElement>> {
-    Ok(parse(&proof)?.into())
+pub fn parse_proof(proof: &str) -> anyhow::Result<Vec<FieldElement>> {
+    Ok(parse(proof)?.into())
 }
