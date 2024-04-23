64a35e33606bfe3db0a12a0c6bad194d900489f2
Fabricio Robles <fabrobles92@gmail.com>
2024-04-22 22:12:30 -0600

    Katana instance with contracts deployed ready to send messages

--- crates/katana/rpc/rpc/tests/starknet.rs
+++ crates/katana/rpc/rpc/tests/starknet.rs
@@ -181,7 +181,7 @@ async fn test_send_declare_and_deploy_legacy_contract() {
 }
 
 #[tokio::test(flavor = "multi_thread")]
-async fn test_anvil_deploy_contract() {
+async fn test_messaging_L1_L2() {
     let root =
         PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("tests").join("test_data").join("solidity");
     let paths = ProjectPathsConfig::builder().root(&root).sources(&root).build().unwrap();
@@ -193,28 +193,96 @@ async fn test_anvil_deploy_contract() {
     let output = project.compile().context("Error compiling project").unwrap();
     output.assert_success();
 
-    let contract = output
-        .find_first("StarknetMessagingLocal")
-        .context("Could not find contract")
-        .unwrap()
-        .clone();
+    //Get both contracts
+    let contract_starknet_messaging_local =
+        output.find_first("StarknetMessagingLocal").expect("could not find contract").clone();
 
-    let (abi, bytecode, _) = contract.into_parts();
+    let (abi_cstrk, bytecode_cstrk, _) = contract_starknet_messaging_local.into_parts();
+
+    let contract_1 = output.find_first("Contract1").expect("could not find contract").clone();
+
+    let (abi_c1, bytecode_c1, _) = contract_1.into_parts();
 
     let anvil: TestAnvil = TestAnvil::start().await;
 
     let account = Arc::new(anvil.account());
 
-    let factory = ContractFactory::new(abi.clone().unwrap(), bytecode.unwrap(), account.clone());
+    // Factories to deploy the contracts
+    let factory_cstrk =
+        ContractFactory::new(abi_cstrk.clone().unwrap(), bytecode_cstrk.unwrap(), account.clone());
 
-    // Deploy contract
-    let contract =
-        factory.deploy(()).expect("Failing deploying").send().await.expect("Failing sending");
+    let factory_c1 =
+        ContractFactory::new(abi_c1.clone().unwrap(), bytecode_c1.unwrap(), account.clone());
+
+    //Deploy to local node (anvil)
+    let contract_strk =
+        factory_cstrk.deploy(()).expect("Failing deploying").send().await.expect("Failing sending");
 
     assert_eq!(
-        contract.address(),
+        contract_strk.address(),
         H160::from_str("0x5fbdb2315678afecb367f032d93f642f64180aa3").unwrap()
     );
 
+    let contract_c1 = factory_c1
+        .deploy(contract_strk.address())
+        .expect("Failing deploying")
+        .send()
+        .await
+        .expect("Failing sending");
+
+    assert_eq!(
+        contract_c1.address(),
+        H160::from_str("0xe7f1725e7734ce288f8367e1bb143e90bb3f0512").unwrap()
+    );
+
+    // -----------------------------------------------------------------------------------------------------------------------------
+
+    let sequencer =
+        TestSequencer::start(SequencerConfig::default(), get_default_test_starknet_config()).await;
+    let account = sequencer.account();
+
+    let path: PathBuf = PathBuf::from("tests/test_data/cairo_l1_msg_contract.json");
+    let (contract, compiled_class_hash) =
+        common::prepare_contract_declaration_params(&path).unwrap();
+
+    let class_hash = contract.class_hash();
+    let res = account.declare(Arc::new(contract), compiled_class_hash).send().await.unwrap();
+
+    // wait for the tx to be mined
+    tokio::time::sleep(Duration::from_millis(WAIT_TX_DELAY_MILLIS)).await;
+
+    let receipt = account.provider().get_transaction_receipt(res.transaction_hash).await.unwrap();
+
+    match receipt {
+        MaybePendingTransactionReceipt::Receipt(TransactionReceipt::Declare(
+            DeclareTransactionReceipt { finality_status, .. },
+        )) => {
+            assert_eq!(finality_status, TransactionFinalityStatus::AcceptedOnL2);
+        }
+        _ => panic!("invalid tx receipt"),
+    }
+
+    assert!(account.provider().get_class(BlockId::Tag(BlockTag::Latest), class_hash).await.is_ok());
+
+    let constructor_calldata = vec![FieldElement::from(1_u32), FieldElement::from(2_u32)];
+
+    let contract_address = get_contract_address(
+        FieldElement::ZERO,
+        res.class_hash,
+        &constructor_calldata,
+        FieldElement::ZERO,
+    );
+
+    assert_eq!(
+        contract_address,
+        FieldElement::from_str(
+            "0x0024f0deadb642bd4792c19da937758eb6bf4747d7d93a13ecc527bba82eb1f1"
+        )
+        .unwrap()
+    );
+
+    //TODO Messaging between Layers
+
+    sequencer.stop().expect("failed to stop sequencer");
     anvil.stop();
 }
