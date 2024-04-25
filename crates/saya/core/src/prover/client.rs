f8146b5588c1c799f4d34a90ddb14d5e95b70ed6
Neo <128649481+neotheprogramist@users.noreply.github.com>
2024-04-25 15:21:22 +0200

    Update crates/saya/core/src/prover/client.rs
    Co-authored-by: glihm <dev@glihm.net>

--- crates/saya/core/src/prover/client.rs
+++ crates/saya/core/src/prover/client.rs
@@ -1,6 +1,6 @@
 use url::Url;
 
-pub async fn http_prove(prover_rpc: Url, input: String) -> anyhow::Result<String> {
+pub async fn http_prove(prover_url: Url, input: String) -> anyhow::Result<String> {
     let client = reqwest::Client::new();
     let resp = client.post(prover_rpc).body(input).send().await?;
     let result = resp.text().await?;
