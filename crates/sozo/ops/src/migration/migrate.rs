e2c4b5ebe5cd53881c5ccd016461bd98c013a31b
lambda-0x <0xlambda@protonmail.com>
2024-04-25 17:54:20 +0530

    fix lints

--- crates/sozo/ops/src/migration/migrate.rs
+++ crates/sozo/ops/src/migration/migrate.rs
@@ -33,12 +33,11 @@ use starknet::signers::Signer;
 use starknet_crypto::FieldElement;
 use tokio::fs;
 
-use crate::auth::{grant_writer, ModelContract};
-
 use super::ui::{bold_message, italic_message, MigrationUi};
 use super::{
     ContractDeploymentOutput, ContractMigrationOutput, ContractUpgradeOutput, MigrationOutput,
 };
+use crate::auth::{grant_writer, ModelContract};
 
 pub fn prepare_migration(
     target_dir: &Utf8PathBuf,
@@ -884,7 +883,7 @@ where
     ui.print_step(7, "🌐", "Authorizing Models to Systems (based on overlay)...");
     ui.print(" ");
     let models_contracts = compute_models_contracts(&ui, local_manifest, migration_output)?;
-    grant_writer(world, models_contracts, txn_config.clone()).await
+    grant_writer(world, models_contracts, *txn_config).await
 }
 
 fn compute_models_contracts(
@@ -895,28 +894,25 @@ fn compute_models_contracts(
     let mut res = vec![];
     let local_contracts = &local_manifest.contracts;
 
-    // from all the contracts that where suppossed to be migrated
-    for contract in &migration_output.contracts {
-        // if they where migrated successfully
-        if let Some(ref migrated_contract) = contract {
-            // find that contract from local_manifest based on its name
-            let contract = local_contracts
-                .iter()
-                .find(|c| &migrated_contract.name == c.name)
-                .expect("we know this contract exists");
+    // from all the contracts that where migrated successfully
+    for migrated_contract in migration_output.contracts.iter().flatten() {
+        // find that contract from local_manifest based on its name
+        let contract = local_contracts
+            .iter()
+            .find(|c| migrated_contract.name == c.name)
+            .expect("we know this contract exists");
 
-            ui.print_sub(format!(
-                "Authorizing {} for Models: {:?}",
-                contract.name, contract.inner.writes
-            ));
+        ui.print_sub(format!(
+            "Authorizing {} for Models: {:?}",
+            contract.name, contract.inner.writes
+        ));
 
-            // read all the models that its supposed to write to and
-            for model in &contract.inner.writes {
-                let model = cairo_utils::str_to_felt(&model)?;
-                let contract_addr_str = format!("{:#x}", migrated_contract.contract_address);
+        // read all the models that its supposed to write to and
+        for model in &contract.inner.writes {
+            let model = cairo_utils::str_to_felt(model)?;
+            let contract_addr_str = format!("{:#x}", migrated_contract.contract_address);
 
-                res.push(ModelContract { model, contract: contract_addr_str });
-            }
+            res.push(ModelContract { model, contract: contract_addr_str });
         }
     }
 
