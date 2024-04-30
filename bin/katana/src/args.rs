7ff9da648765e21365b7bbccd4e9b213f08b6b0a
Ammar Arif <evergreenkary@gmail.com>
2024-04-30 17:23:43 +0800

    refactor forked backend apis

--- bin/katana/src/args.rs
+++ bin/katana/src/args.rs
@@ -203,7 +203,7 @@ pub struct EnvironmentOptions {
 
 impl KatanaArgs {
     pub fn init_logging(&self) -> Result<(), Box<dyn std::error::Error>> {
-        const DEFAULT_LOG_FILTER: &str = "info,executor=trace,forked_backend=trace,server=debug,\
+        const DEFAULT_LOG_FILTER: &str = "info,executor=trace,forking::backend=trace,server=debug,\
                                           katana_core=trace,blockifier=off,jsonrpsee_server=off,\
                                           hyper=off,messaging=debug,node=error";
 
