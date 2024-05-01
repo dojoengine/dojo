373320fcd2c41a6bb46eb6c7c81fa930978c04df
glihm <dev@glihm.net>
2024-04-30 21:19:49 -0600

    fix: remove test expansion test seems not detected by codecov

--- crates/dojo-lang/src/plugin_test.rs
+++ crates/dojo-lang/src/plugin_test.rs
@@ -1,7 +1,7 @@
 use std::sync::Arc;
 
 use cairo_lang_defs::db::{DefsDatabase, DefsGroup};
-use cairo_lang_defs::ids::{ModuleId, ModuleItemId};
+use cairo_lang_defs::ids::ModuleId;
 use cairo_lang_defs::plugin::MacroPlugin;
 use cairo_lang_filesystem::cfg::CfgSet;
 use cairo_lang_filesystem::db::{
@@ -11,7 +11,6 @@ use cairo_lang_filesystem::ids::{CrateLongId, Directory, FileLongId};
 use cairo_lang_parser::db::ParserDatabase;
 use cairo_lang_plugins::get_base_plugins;
 use cairo_lang_plugins::test_utils::expand_module_text;
-use cairo_lang_syntax::attribute::structured::AttributeListStructurize;
 use cairo_lang_syntax::node::db::{SyntaxDatabase, SyntaxGroup};
 use cairo_lang_test_utils::parse_test_file::TestRunnerResult;
 use cairo_lang_test_utils::verify_diagnostics_expectation;
@@ -115,80 +114,4 @@ pub fn test_expand_plugin_inner(
         ]),
         error,
     }
-}
-
-#[test]
-pub fn test_extract_allow_ref_self_ok() {
-    let cairo_code = r#"
-    #[dojo::contract(allow_ref_self)]
-    mod dojo_contract {}
-    "#;
-
-    let db = init_db_for_code(cairo_code);
-    let crate_id = db.intern_crate(CrateLongId::Real("test".into()));
-    let crate_root = ModuleId::CrateRoot(crate_id);
-    assert!(verify_extract_allow_ref_self(&db, crate_root));
-}
-
-#[test]
-pub fn test_extract_allow_ref_self_ok_no_attr() {
-    let cairo_code = r#"
-    #[dojo::contract()]
-    mod dojo_contract {}
-    "#;
-
-    let db = init_db_for_code(cairo_code);
-    let crate_id = db.intern_crate(CrateLongId::Real("test".into()));
-    let crate_root = ModuleId::CrateRoot(crate_id);
-    assert!(!verify_extract_allow_ref_self(&db, crate_root));
-}
-
-#[test]
-pub fn test_extract_allow_ref_self_none() {
-    let cairo_code = r#"
-    mod other_contract {}
-    "#;
-
-    let db = init_db_for_code(cairo_code);
-    let crate_id = db.intern_crate(CrateLongId::Real("test".into()));
-    let crate_root = ModuleId::CrateRoot(crate_id);
-    assert!(!verify_extract_allow_ref_self(&db, crate_root));
-}
-
-pub fn init_db_for_code(cairo_code: &str) -> DatabaseForTesting {
-    let mut db = DatabaseForTesting::default();
-
-    let crate_id = db.intern_crate(CrateLongId::Real("test".into()));
-    let root = Directory::Real("test_src".into());
-
-    db.set_crate_config(crate_id, Some(CrateConfiguration::default_for_root(root)));
-
-    // Main module file.
-    let file_id = db.intern_file(FileLongId::OnDisk("test_src/lib.cairo".into()));
-    db.as_files_group_mut()
-        .override_file_content(file_id, Some(Arc::new(format!("{cairo_code}\n"))));
-
-    db
-}
-
-pub fn verify_extract_allow_ref_self(db: &dyn DefsGroup, module_id: ModuleId) -> bool {
-    let syntax_db = db.upcast();
-
-    for item_id in db.module_items(module_id).unwrap().iter() {
-        if let ModuleItemId::Submodule(item) = item_id {
-            let module_ast = item.stable_ptr(db).lookup(syntax_db);
-            let attrs = module_ast.attributes(syntax_db).structurize(syntax_db);
-            let dojo_contract_attr =
-                attrs.iter().find(|attr| attr.id.as_str() == crate::plugin::DOJO_CONTRACT_ATTR);
-            let do_allow_ref_self =
-                crate::contract::extract_allow_ref_self(dojo_contract_attr, syntax_db)
-                    .unwrap_or_default();
-
-            if do_allow_ref_self {
-                return true;
-            }
-        }
-    }
-
-    false
 }
