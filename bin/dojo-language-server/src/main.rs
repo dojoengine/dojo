edf92094fd1bfc734fb992a5e9b93957ecd1aad5
glihm <dev@glihm.net>
2024-05-17 00:46:04 +0200

    fix: remove async for language server

--- bin/dojo-language-server/src/main.rs
+++ bin/dojo-language-server/src/main.rs
@@ -1,8 +1,7 @@
 use dojo_lang::plugin::dojo_plugin_suite;
 use cairo_lang_language_server::Tricks;
 
-#[tokio::main]
-async fn main() {
+fn main() {
     let mut tricks = Tricks::default();
     tricks.extra_plugin_suites = Some(&|| vec![dojo_plugin_suite()]);
     cairo_lang_language_server::start_with_tricks(tricks);
