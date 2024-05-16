18323f550768a525b9fbf727941c90c77cb6bd19
github-action-benchmark <github@users.noreply.github.com>
2024-05-16 20:09:20 +0000

    add Benchmark (cargo) benchmark result for c9714ef797b91aa9c4a102e8aed8f56686867f60

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1715889977451,
+  "lastUpdate": 1715890160981,
   "repoUrl": "https://github.com/dojoengine/dojo",
   "entries": {
     "Benchmark": [
@@ -8035,6 +8035,36 @@ window.BENCHMARK_DATA = {
             "name": "decompress world contract",
             "value": 17330590,
             "range": "± 225481",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "evergreenkary@gmail.com",
+            "name": "Ammar Arif",
+            "username": "kariy"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c9714ef797b91aa9c4a102e8aed8f56686867f60",
+          "message": "Add simple benchmark for the `transact` function (#1969)\n\n# Description\r\n\r\na simple benchmark for the smallest unit of execution that. we use the `pprof` profiler to output a flamegraph.\r\n\r\nwe can later extend this to `sir` once we have it re-enabled it.\r\n\r\n## Related issue\r\n\r\n<!--\r\nPlease link related issues: Fixes #<issue_number>\r\nMore info: https://docs.github.com/en/free-pro-team@latest/github/managing-your-work-on-github/linking-a-pull-request-to-an-issue#linking-a-pull-request-to-an-issue-using-a-keyword\r\n-->\r\n\r\n## Tests\r\n\r\n<!--\r\nPlease refer to the CONTRIBUTING.md file to know more about the testing process. Ensure you've tested at least the package you're modifying if running all the tests consumes too much memory on your system.\r\n-->\r\n\r\n- [ ] Yes\r\n- [x] No, because they aren't needed\r\n- [ ] No, because I need help\r\n\r\n## Added to documentation?\r\n\r\n<!--\r\nIf the changes are small, code comments are enough, otherwise, the documentation is needed. It\r\nmay be a README.md file added to your module/package, a DojoBook PR or both.\r\n-->\r\n\r\n- [ ] README.md\r\n- [ ] [Dojo Book](https://github.com/dojoengine/book)\r\n- [x] No documentation needed\r\n\r\n## Checklist\r\n\r\n- [x] I've formatted my code (`scripts/prettier.sh`, `scripts/rust_fmt.sh`, `scripts/cairo_fmt.sh`)\r\n- [x] I've linted my code (`scripts/clippy.sh`, `scripts/docs.sh`)\r\n- [x] I've commented my code\r\n- [ ] I've requested a review after addressing the comments",
+          "timestamp": "2024-05-17T04:00:54+08:00",
+          "tree_id": "308d3ef70fb0e6ecf0f3274fd1cf64d5826739d8",
+          "url": "https://github.com/dojoengine/dojo/commit/c9714ef797b91aa9c4a102e8aed8f56686867f60"
+        },
+        "date": 1715890159881,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "decompress world contract",
+            "value": 16876835,
+            "range": "± 215919",
             "unit": "ns/iter"
           }
         ]
