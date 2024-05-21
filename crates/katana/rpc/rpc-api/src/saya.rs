65c63c68abd30c221c0b9241fdc3f5421702c850
Ammar Arif <evergreenkary@gmail.com>
2024-05-21 11:26:59 -0400

    remove cursor based api

--- crates/katana/rpc/rpc-api/src/saya.rs
+++ crates/katana/rpc/rpc-api/src/saya.rs
@@ -2,24 +2,10 @@ use jsonrpsee::core::RpcResult;
 use jsonrpsee::proc_macros::rpc;
 use katana_primitives::block::BlockIdOrTag;
 use katana_rpc_types::trace::TxExecutionInfo;
-use katana_rpc_types::transaction::{TransactionsExecutionsPage, TransactionsPageCursor};
 
 #[cfg_attr(not(feature = "client"), rpc(server, namespace = "saya"))]
 #[cfg_attr(feature = "client", rpc(client, server, namespace = "saya"))]
 pub trait SayaApi {
-    /// Fetches the transaction execution info for all the transactions in the
-    /// given block.
-    ///
-    /// # Arguments
-    ///
-    /// * `block_number` - The block number to get executions from.
-    /// * `chunk_size` - The maximum number of transaction execution that should be returned.
-    #[method(name = "getTransactionsExecutions")]
-    async fn get_transactions_executions(
-        &self,
-        cursor: TransactionsPageCursor,
-    ) -> RpcResult<TransactionsExecutionsPage>;
-
     /// Retrieves a list of transaction execution informations of a given block.
     #[method(name = "getTransactionExecutionsByBlock")]
     async fn transaction_executions_by_block(
