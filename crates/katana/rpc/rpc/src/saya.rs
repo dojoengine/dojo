5fe059bb4e1a73abbcaaba724a660f0ed46d133b
Ammar Arif <evergreenkary@gmail.com>
2024-05-23 09:18:30 -0400

    Refactor `saya` to fetch the traces using the new API (#1987)
    # Description
    
    basically refactor the tx traces fetching logic to using the new API introduced in #1986.
    
    ref: https://github.com/dojoengine/dojo/pull/1986#discussion_r1607193514 
    
    to simplify the process of extracting the l1 -> l2 messages bcs currently we have to compute the message hash in order to find the corresponding tx as seen here:
    
    https://github.com/dojoengine/dojo/blob/855da3112c87faea87646db5a406ac77b4daf149/crates/saya/core/src/prover/program_input.rs#L54-L74
    
    these changes (including #1986) make the process more straightforward as we can just filter based on the tx hash which is more straightforward.
    
    ## Related issue
    
    <!--
    Please link related issues: Fixes #<issue_number>
    More info: https://docs.github.com/en/free-pro-team@latest/github/managing-your-work-on-github/linking-a-pull-request-to-an-issue#linking-a-pull-request-to-an-issue-using-a-keyword
    -->
    
    ## Tests
    
    <!--
    Please refer to the CONTRIBUTING.md file to know more about the testing process. Ensure you've tested at least the package you're modifying if running all the tests consumes too much memory on your system.
    -->
    
    - [ ] Yes
    - [x] No, because they aren't needed
    - [ ] No, because I need help
    
    ## Added to documentation?
    
    <!--
    If the changes are small, code comments are enough, otherwise, the documentation is needed. It
    may be a README.md file added to your module/package, a DojoBook PR or both.
    -->
    
    - [ ] README.md
    - [ ] [Dojo Book](https://github.com/dojoengine/book)
    - [x] No documentation needed
    
    ## Checklist
    
    - [x] I've formatted my code (`scripts/prettier.sh`, `scripts/rust_fmt.sh`, `scripts/cairo_fmt.sh`)
    - [x] I've linted my code (`scripts/clippy.sh`, `scripts/docs.sh`)
    - [x] I've commented my code
    - [ ] I've requested a review after addressing the comments

--- crates/katana/rpc/rpc/src/saya.rs
+++ crates/katana/rpc/rpc/src/saya.rs
@@ -3,14 +3,13 @@ use std::sync::Arc;
 use jsonrpsee::core::{async_trait, RpcResult};
 use katana_core::sequencer::KatanaSequencer;
 use katana_executor::ExecutorFactory;
-use katana_primitives::block::{BlockHashOrNumber, BlockIdOrTag, BlockTag};
+use katana_primitives::block::{BlockIdOrTag, BlockTag};
 use katana_provider::error::ProviderError;
 use katana_provider::traits::block::{BlockIdReader, BlockProvider};
 use katana_provider::traits::transaction::{TransactionTraceProvider, TransactionsProviderExt};
 use katana_rpc_api::saya::SayaApiServer;
 use katana_rpc_types::error::saya::SayaApiError;
 use katana_rpc_types::trace::TxExecutionInfo;
-use katana_rpc_types::transaction::{TransactionsExecutionsPage, TransactionsPageCursor};
 use katana_tasks::TokioTaskSpawner;
 
 pub struct SayaApi<EF: ExecutorFactory> {
@@ -40,43 +39,6 @@ impl<EF: ExecutorFactory> SayaApi<EF> {
 
 #[async_trait]
 impl<EF: ExecutorFactory> SayaApiServer for SayaApi<EF> {
-    async fn get_transactions_executions(
-        &self,
-        cursor: TransactionsPageCursor,
-    ) -> RpcResult<TransactionsExecutionsPage> {
-        self.on_io_blocking_task(move |this| {
-            let provider = this.sequencer.backend.blockchain.provider();
-            let mut next_cursor = cursor;
-
-            let transactions_executions = provider
-                .transaction_executions_by_block(BlockHashOrNumber::Num(cursor.block_number))
-                .map_err(SayaApiError::from)?
-                .ok_or(SayaApiError::BlockNotFound)?;
-
-            let total_execs = transactions_executions.len() as u64;
-
-            let transactions_executions = transactions_executions
-                .into_iter()
-                .skip(cursor.transaction_index as usize)
-                .take(cursor.chunk_size as usize)
-                .collect::<Vec<_>>();
-
-            if cursor.transaction_index + cursor.chunk_size >= total_execs {
-                // All transactions of the block pointed by the cursor were fetched.
-                // Indicate to the client this situation by setting the block number
-                // to the next block and transaction index to 0.
-                next_cursor.block_number = cursor.block_number + 1;
-                next_cursor.transaction_index = 0;
-            } else {
-                next_cursor.transaction_index +=
-                    cursor.transaction_index + transactions_executions.len() as u64;
-            }
-
-            Ok(TransactionsExecutionsPage { transactions_executions, cursor: next_cursor })
-        })
-        .await
-    }
-
     async fn transaction_executions_by_block(
         &self,
         block_id: BlockIdOrTag,
