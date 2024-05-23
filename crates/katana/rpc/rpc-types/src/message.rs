3b712254e5fa606089ec58b320bc6b4331986779
Ammar Arif <evergreenkary@gmail.com>
2024-05-23 09:23:41 -0400

    fix(katana): l1/l2 messaging hash computations (#1981)
    # Description
    
    l1 -> l2 messaging should be using a different hash computation, but instead we were using the hash computation for l2 -> l1 message.
    
    this pr adds a new `compute_l1_to_l2_message_hash` function for computing the hash of a l1 -> l2 message.
    
    ref : https://docs.starknet.io/documentation/architecture_and_concepts/Network_Architecture/messaging-mechanism/#l1-l2-messages
    
    ## Related issue
    
    <!--
    Please link related issues: Fixes #<issue_number>
    More info: https://docs.github.com/en/free-pro-team@latest/github/managing-your-work-on-github/linking-a-pull-request-to-an-issue#linking-a-pull-request-to-an-issue-using-a-keyword
    -->
    
    ## Tests
    
    <!--
    Please refer to the CONTRIBUTING.md file to know more about the testing process. Ensure you've tested at least the package you're modifying if running all the tests consumes too much memory on your system.
    -->
    
    - [ ] Yes
    - [ ] No, because they aren't needed
    - [ ] No, because I need help
    
    ## Added to documentation?
    
    <!--
    If the changes are small, code comments are enough, otherwise, the documentation is needed. It
    may be a README.md file added to your module/package, a DojoBook PR or both.
    -->
    
    - [ ] README.md
    - [ ] [Dojo Book](https://github.com/dojoengine/book)
    - [x] No documentation needed
    
    ## Checklist
    
    - [x] I've formatted my code (`scripts/prettier.sh`, `scripts/rust_fmt.sh`, `scripts/cairo_fmt.sh`)
    - [x] I've linted my code (`scripts/clippy.sh`, `scripts/docs.sh`)
    - [x] I've commented my code
    - [ ] I've requested a review after addressing the comments

--- crates/katana/rpc/rpc-types/src/message.rs
+++ crates/katana/rpc/rpc-types/src/message.rs
@@ -1,6 +1,6 @@
 use katana_primitives::chain::ChainId;
 use katana_primitives::transaction::L1HandlerTx;
-use katana_primitives::utils::transaction::compute_l1_message_hash;
+use katana_primitives::utils::transaction::compute_l2_to_l1_message_hash;
 use katana_primitives::FieldElement;
 use serde::{Deserialize, Serialize};
 
@@ -9,7 +9,11 @@ pub struct MsgFromL1(starknet::core::types::MsgFromL1);
 
 impl MsgFromL1 {
     pub fn into_tx_with_chain_id(self, chain_id: ChainId) -> L1HandlerTx {
-        let message_hash = compute_l1_message_hash(
+        // Set the L1 to L2 message nonce to 0, because this is just used
+        // for the `estimateMessageFee` RPC.
+        let nonce = FieldElement::ZERO;
+
+        let message_hash = compute_l2_to_l1_message_hash(
             // This conversion will never fail bcs `from_address` is 20 bytes and the it will only
             // fail if the slice is > 32 bytes
             FieldElement::from_byte_slice_be(self.0.from_address.as_bytes()).unwrap(),
@@ -18,10 +22,10 @@ impl MsgFromL1 {
         );
 
         L1HandlerTx {
+            nonce,
             chain_id,
             message_hash,
             calldata: self.0.payload,
-            nonce: Default::default(),
             version: FieldElement::ZERO,
             paid_fee_on_l1: Default::default(),
             contract_address: self.0.to_address.into(),
