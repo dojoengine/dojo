465ab840add60678d62a060b76c3a1da94b366d4
Nasr <larko236@outlook.com>
2024-05-23 18:23:17 -0400

    temp-fix: arrays

--- crates/torii/graphql/src/object/model_data.rs
+++ crates/torii/graphql/src/object/model_data.rs
@@ -1,3 +1,5 @@
+use std::ops::Deref;
+
 use async_graphql::dynamic::{Enum, Field, FieldFuture, InputObject, Object, TypeRef};
 use async_graphql::Value;
 use chrono::{DateTime, Utc};
@@ -65,7 +67,8 @@ impl BasicObject for ModelDataObject {
 
     fn objects(&self) -> Vec<Object> {
         let mut objects = data_objects_recursion(
-            &TypeData::Nested((TypeRef::named(self.type_name()), self.type_mapping.clone())),
+            self.type_name(),
+            self.type_mapping(),
             vec![self.type_name().to_string()],
         );
 
@@ -139,31 +142,71 @@ impl ResolvableObject for ModelDataObject {
     }
 }
 
-fn data_objects_recursion(type_data: &TypeData, path_array: Vec<String>) -> Vec<Object> {
-    let mut objects: Vec<Object> = vec![];
-    match type_data {
-        TypeData::Nested((nested_type, nested_mapping)) => {
-            let mut nested_path = path_array.clone();
-            nested_path.push(nested_type.to_string());
-            let nested_objects = nested_mapping.iter().flat_map(|(field_name, type_data)| {
-                let mut nested_path = nested_path.clone();
+// fn data_objects_recursion(type_data: &TypeData, path_array: Vec<String>) -> Vec<Object> {
+//     let mut objects: Vec<Object> = vec![];
+//     match type_data {
+//         TypeData::Nested((nested_type, nested_mapping)) => {
+//             let mut nested_path = path_array.clone();
+//             nested_path.push(nested_type.to_string());
+//             let nested_objects = nested_mapping.iter().flat_map(|(field_name, type_data)| {
+//                 let mut nested_path = nested_path.clone();
+//                 nested_path.push(field_name.to_string());
+//                 data_objects_recursion(type_data, nested_path)
+//             });
+
+//             objects.extend(nested_objects);
+//             objects.push(object(&nested_type.to_string(), nested_mapping, path_array));
+//         }
+//         TypeData::List(inner) => {
+//             let mut nested_path = path_array.clone();
+//             nested_path.push(inner.type_ref().to_string());
+//             let nested_objects = data_objects_recursion(inner, nested_path);
+
+//             objects.extend(nested_objects);
+//         }
+//         _ => {}
+//     }
+
+//     objects
+// }
+
+fn data_objects_recursion(
+    type_name: &str,
+    type_mapping: &TypeMapping,
+    path_array: Vec<String>,
+) -> Vec<Object> {
+    let mut objects: Vec<Object> = type_mapping
+        .iter()
+        .filter_map(|(field_name, type_data)| {
+            if let TypeData::Nested((nested_type, nested_mapping)) = type_data {
+                let mut nested_path = path_array.clone();
                 nested_path.push(field_name.to_string());
-                data_objects_recursion(type_data, nested_path)
-            });
+                let nested_objects =
+                    data_objects_recursion(&nested_type.to_string(), nested_mapping, nested_path);
 
-            objects.extend(nested_objects);
-            objects.push(object(&nested_type.to_string(), nested_mapping, path_array));
-        }
-        TypeData::List(inner) => {
-            let mut nested_path = path_array.clone();
-            nested_path.push(inner.type_ref().to_string());
-            let nested_objects = data_objects_recursion(inner, nested_path);
+                Some(nested_objects)
+            } else if let TypeData::List(inner) = type_data {
+                if let TypeData::Nested((nested_type, nested_mapping)) = inner.deref() {
+                    let mut nested_path = path_array.clone();
+                    nested_path.push(inner.type_ref().to_string());
+                    let nested_objects = data_objects_recursion(
+                        &nested_type.to_string(),
+                        nested_mapping,
+                        nested_path,
+                    );
 
-            objects.extend(nested_objects);
-        }
-        _ => {}
-    }
+                    return Some(nested_objects);
+                }
 
+                None
+            } else {
+                None
+            }
+        })
+        .flatten()
+        .collect();
+
+    objects.push(object(type_name, type_mapping, path_array));
     objects
 }
 
