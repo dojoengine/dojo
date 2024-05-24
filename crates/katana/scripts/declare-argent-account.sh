35ca97454678ab5e91aa31b53f372ddc914b88de
Matthias <matthias.monnier@gmail.com>
2024-05-24 17:50:52 +0200

    Katana and ArgentX wallet (#1958)
    * fix: path to Argent account compiled contracts in declare script
    
    * fix: allow some headers in CORS layer of Katana to work with ArgentX wallet
    
    * refactor: clean forgotten logs in declare-argent-account.sh script and remove unnecessary var
    
    * fix: remove vec in favor of array
    
    ---------
    
    Co-authored-by: glihm <dev@glihm.net>

--- crates/katana/scripts/declare-argent-account.sh
+++ crates/katana/scripts/declare-argent-account.sh
@@ -1,15 +1,14 @@
 #! /bin/bash
 
 STD_ARGENT_ACCOUNT_0_3_0_CLASS_HASH="0x01a736d6ed154502257f02b1ccdf4d9d1089f80811cd6acad48e6b6a9d1f2003"
-STD_ARGENT_ACCOUNT_0_3_0_COMPILED_FILE="../primitives/contracts/compiled/argent_ArgentAccount_0.3.0.json"
+STD_ARGENT_ACCOUNT_0_3_0_COMPILED_FILE="../contracts/compiled/argent_ArgentAccount_0.3.0.json"
 
 STD_ARGENT_ACCOUNT_0_3_1_CLASS_HASH="0x29927c8af6bccf3f6fda035981e765a7bdbf18a2dc0d630494f8758aa908e2b"
-STD_ARGENT_ACCOUNT_0_3_1_COMPILED_FILE="../primitives/contracts/compiled/argent_ArgentAccount_0.3.1.json"
+STD_ARGENT_ACCOUNT_0_3_1_COMPILED_FILE="../contracts/compiled/argent_ArgentAccount_0.3.1.json"
 
 TEMP_ACCOUNT_FILE="temp-account.json"
-TEMP_KEYSTORE_FILE="temp-keystore.json"
 
-trap "rm -f $TEMP_ACCOUNT_FILE $TEMP_KEYSTORE_FILE" EXIT
+trap "rm -f $TEMP_ACCOUNT_FILE" EXIT
 
 # This is helper script is for declaring the standard Argent account to Katana as it doesn't come as a default.
 # Argent account contract source code: https://github.com/argentlabs/argent-contracts-starknet/blob/53d01c0d6dce4fd30db955c3d698f658cdda1796/contracts/account/src/argent_account.cairo
@@ -56,7 +55,6 @@ else
 	echo "Only supported class hashes are: $STD_ARGENT_ACCOUNT_0_3_0_CLASS_HASH (0.3.0), $STD_ARGENT_ACCOUNT_0_3_1_CLASS_HASH (0.3.1)"
 	exit 1
 fi
-
 
 starkli account fetch $ACCOUNT_ADDRESS --rpc $RPC_URL --output $TEMP_ACCOUNT_FILE &> /dev/null
 
