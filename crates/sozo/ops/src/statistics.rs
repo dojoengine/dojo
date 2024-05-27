8aa23a48a6e87d8995a2f6b4757f9cd8df8a1c0f
lambda-0x <0xlambda@protonmail.com>
2024-05-27 12:08:40 +0530

    consider file size of minified json not pretty printed json

--- crates/sozo/ops/src/statistics.rs
+++ crates/sozo/ops/src/statistics.rs
@@ -1,5 +1,5 @@
 use std::fs::{self, File};
-use std::io::{self, BufReader};
+use std::io::BufReader;
 use std::path::PathBuf;
 
 use anyhow::{Context, Result};
@@ -35,18 +35,17 @@ fn get_casm_byte_code_size(contract_artifact: CasmContractClass) -> u64 {
     contract_artifact.bytecode.len() as u64
 }
 
-fn get_file_size(file: &File) -> Result<u64, io::Error> {
-    file.metadata().map(|metadata| metadata.len())
-}
-
 fn get_contract_statistics_for_file(
     contract_name: String,
-    sierra_json_file: File,
     sierra_class: ContractClass,
     casm_class: CasmContractClass,
 ) -> Result<ContractStatistics> {
-    let sierra_contract_class_size =
-        get_file_size(&sierra_json_file).context("Error getting file size")?;
+    let sierra_contract_class_size = serde_json::to_string(&sierra_class)
+        .context("should be valid json")
+        .unwrap()
+        .len()
+        .try_into()
+        .unwrap();
     let sierra_bytecode_size = get_sierra_byte_code_size(sierra_class);
 
     let casm_contract_class_size = serde_json::to_string(&casm_class)
@@ -104,7 +103,6 @@ pub fn get_contract_statistics_for_dir(
 
         contract_statistics.push(get_contract_statistics_for_file(
             contract_name,
-            sierra_json_file,
             sierra_class,
             casm_class,
         )?);
@@ -114,14 +112,15 @@ pub fn get_contract_statistics_for_dir(
 
 #[cfg(test)]
 mod tests {
-    use std::fs::File;
     use std::path::Path;
+    use std::{fs::File, io::BufReader};
 
+    use cairo_lang_starknet_classes::contract_class::ContractClass;
     use camino::Utf8PathBuf;
     use scarb_ui::Ui;
 
     use super::{
-        get_contract_statistics_for_dir, get_contract_statistics_for_file, get_file_size,
+        get_contract_statistics_for_dir, get_contract_statistics_for_file,
         get_sierra_byte_code_size, read_sierra_json_program, ContractStatistics,
     };
     use crate::statistics::get_casm_byte_code_size;
@@ -175,19 +174,15 @@ mod tests {
         let expected_contract_statistics: ContractStatistics = ContractStatistics {
             contract_name: String::from("test_contract"),
             sierra_bytecode_size: 2175,
-            sierra_contract_class_size: 114925,
+            sierra_contract_class_size: 106559,
 
             casm_bytecode_size: 4412,
             casm_contract_class_size: 95806,
         };
 
-        let statistics = get_contract_statistics_for_file(
-            filename.clone(),
-            sierra_json_file,
-            sierra_class,
-            casm_class,
-        )
-        .expect("Error getting contract statistics for file");
+        let statistics =
+            get_contract_statistics_for_file(filename.clone(), sierra_class, casm_class)
+                .expect("Error getting contract statistics for file");
 
         assert_eq!(statistics, expected_contract_statistics);
     }
@@ -207,10 +202,13 @@ mod tests {
     fn get_file_size_returns_correct_size() {
         let sierra_json_file = File::open(TEST_SIERRA_JSON_CONTRACT)
             .unwrap_or_else(|err| panic!("Failed to open test file: {}", err));
-        const EXPECTED_SIZE: u64 = 114925;
+        // file size of minified json
+        const EXPECTED_SIZE: u64 = 106559;
 
-        let file_size = get_file_size(&sierra_json_file)
-            .unwrap_or_else(|_| panic!("Error getting file size for test file"));
+        let sierra_contract_class: ContractClass =
+            serde_json::from_reader(BufReader::new(sierra_json_file)).unwrap();
+        let file_size: u64 =
+            serde_json::to_string(&sierra_contract_class).unwrap().len().try_into().unwrap();
 
         assert_eq!(file_size, EXPECTED_SIZE, "File size mismatch");
     }
