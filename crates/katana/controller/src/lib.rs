d8aad619824ef628d2d1b8e729d5a2c12fa76a76
Ammar Arif <evergreenkary@gmail.com>
2024-06-24 17:09:09 -0400

    add tests

--- crates/katana/controller/src/lib.rs
+++ crates/katana/controller/src/lib.rs
@@ -167,7 +167,49 @@ fn read_compiled_class_artifact(artifact: &str) -> Result<SierraCompiledClass> {
 
 #[cfg(test)]
 mod tests {
+    use starknet::macros::felt;
+
+    use super::*;
 
     #[test]
-    fn test_add_controller_account() {}
+    fn test_add_controller_account() {
+        let mut genesis = Genesis::default();
+        let controller_class_hash = add_controller_class(&mut Genesis::default()).unwrap();
+        add_controller_account(&mut genesis).unwrap();
+
+        let user = Credentials::load().unwrap();
+        let address = ContractAddress::from(user.account.contract_address);
+        let allocation = genesis.allocations.get(&address).unwrap();
+
+        assert!(genesis.allocations.contains_key(&address));
+        assert_eq!(allocation.class_hash(), Some(controller_class_hash));
+        assert_eq!(allocation.balance(), Some(U256::from(0xfffffffffffffffu128)));
+    }
+
+    #[test]
+    fn test_get_contract_storage() {
+        // Test data for Controller with WebAuthn Signer.
+        //
+        // Username: johnsmith
+        // Controller address: 0x0260ab0352da372054ed9dc586f024f6a259b9ea64a8e09b16147201220f88d2
+        // <https://sepolia.starkscan.co/contract/0x0260ab0352da372054ed9dc586f024f6a259b9ea64a8e09b16147201220f88d2#overview>
+
+        let expected_storage_key =
+            felt!("0x058c7ee1e9bb09b0d728314f36629772ef7a3c6773a823064d5a7e5651bcb890");
+        let expected_storage_value =
+            felt!("0x5d7709b0a485e64a549ada9bd14d30419364127dfd351e01f38871c82500cd7");
+
+        let webauthn_credential_id = "ja0NkHny-dlfPnClYECdmce0xTCuGT0xFjeuStaVqCI";
+        let webauthn_public_key = "pQECAyYgASFYIBLHWNmpxCtO47cfOXw9nFCGftMq57xhvQC98aY_zQchIlggIgGHmWwQe1_FGi9GYqcYYpoPC9mkkf0f1rVD5UoGPEA";
+
+        let credential_id = webauthn::credential::from_base64(webauthn_credential_id).unwrap();
+        let public_key = webauthn::cose_key::from_base64(webauthn_public_key).unwrap();
+
+        let storage =
+            get_contract_storage(credential_id.clone(), public_key.clone(), SignerType::Webauthn)
+                .unwrap();
+
+        assert_eq!(storage.len(), 1);
+        assert_eq!(storage.get(&expected_storage_key), Some(&expected_storage_value));
+    }
 }
