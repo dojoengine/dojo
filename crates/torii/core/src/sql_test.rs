fc9de163f0958d61060c4f33cb318380015ef728
Nasr <larko236@outlook.com>
2024-06-26 22:07:27 -0400

    chore: update test for new model

--- crates/torii/core/src/sql_test.rs
+++ crates/torii/core/src/sql_test.rs
@@ -123,7 +123,7 @@ async fn test_load_from_remote() {
 
     let _block_timestamp = 1710754478_u64;
     let models = sqlx::query("SELECT * FROM models").fetch_all(&pool).await.unwrap();
-    assert_eq!(models.len(), 7);
+    assert_eq!(models.len(), 8);
 
     let (id, name, packed_size, unpacked_size): (String, String, u8, u8) = sqlx::query_as(
         "SELECT id, name, packed_size, unpacked_size FROM models WHERE name = 'Position'",
