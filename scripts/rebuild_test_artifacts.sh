0cc8cace884c454ae6a7064a621f0d1f0451d608
lambda-0x <0xlambda@protonmail.com>
2024-06-30 15:53:44 +0530

    update example and script

--- scripts/rebuild_test_artifacts.sh
+++ scripts/rebuild_test_artifacts.sh
@@ -14,6 +14,11 @@ rm -rf examples/spawn-and-move/manifests/dev/base
 rm -rf examples/spawn-and-move/manifests/dev/manifest.json
 rm -rf examples/spawn-and-move/manifests/dev/manifest.toml
 
+rm -rf examples/spawn-and-move/manifests/release/abis
+rm -rf examples/spawn-and-move/manifests/release/base
+rm -rf examples/spawn-and-move/manifests/release/manifest.json
+rm -rf examples/spawn-and-move/manifests/release/manifest.toml
+
 rm -rf crates/torii/types-test/target
 rm -rf crates/torii/types-test/manifests
 
@@ -25,6 +30,7 @@ CAIRO_FIX_TESTS=1 cargo test --package dojo-lang semantics
 
 # Re-run the minimal tests, this will re-build the projects + generate the build artifacts.
 cargo run -r --bin sozo -- build --manifest-path examples/spawn-and-move/Scarb.toml
+cargo run -r --bin sozo -- build --manifest-path examples/spawn-and-move/Scarb.toml -P release
 cargo run -r --bin sozo -- build --manifest-path crates/torii/types-test/Scarb.toml
 
 # Finally, to include all the examples manifest, you should re-deploy the examples.
