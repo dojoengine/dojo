98801d3dc313b9cb5fb29a8fa99fb6f9ceb33929
Nasr <larko236@outlook.com>
2024-07-17 13:50:37 +0200

    feat(torii-core): store update member

--- crates/torii/core/src/processors/mod.rs
+++ crates/torii/core/src/processors/mod.rs
@@ -13,6 +13,7 @@ pub mod store_del_record;
 pub mod store_set_record;
 pub mod store_transaction;
 pub mod store_update_record;
+pub mod store_update_member;
 
 const MODEL_INDEX: usize = 0;
 const NUM_KEYS_INDEX: usize = 1;
