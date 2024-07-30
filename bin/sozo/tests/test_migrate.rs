fe1c0f922bbacac07ce91a1831f6224dc4ed9e62
glihm <dev@glihm.net>
2024-07-30 14:35:17 -0600

    fix: add test for upgrade during migration

--- bin/sozo/tests/test_migrate.rs
+++ bin/sozo/tests/test_migrate.rs
@@ -1,4 +1,4 @@
-mod utils;
+use std::fs;
 
 use dojo_test_utils::compiler::CompilerTestSetup;
 use katana_runner::KatanaRunner;
@@ -6,6 +6,8 @@ use scarb::compiler::Profile;
 use starknet::accounts::Account;
 use starknet::core::types::{BlockId, BlockTag};
 use utils::snapbox::get_snapbox;
+
+mod utils;
 
 #[tokio::test(flavor = "multi_thread")]
 async fn migrate_dry_run() {
@@ -45,4 +47,53 @@ async fn migrate_dry_run() {
     assert!(output.contains("# Models (10)"));
     assert!(output.contains("# World"));
     assert!(output.contains("# Contracts (4)"));
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn test_migrate_then_upgrade() {
+    let setup = CompilerTestSetup::from_examples("../../crates/dojo-core", "../../examples/");
+    let config = setup.build_test_config("spawn-and-move", Profile::DEV);
+    let tmp_dir = config.manifest_path().parent().unwrap();
+
+    let sequencer = KatanaRunner::new().expect("Failed to start runner.");
+
+    let mut account = sequencer.account(0);
+    account.set_block_id(BlockId::Tag(BlockTag::Pending));
+
+    let account_address = &format!("0x{:x}", account.address());
+    let private_key =
+        &format!("0x{:x}", sequencer.account_data(0).private_key.as_ref().unwrap().secret_scalar());
+    let rpc_url = &sequencer.url().to_string();
+
+    let args_vec = [
+        "migrate",
+        "apply",
+        "--account-address",
+        account_address,
+        "--rpc-url",
+        rpc_url,
+        "--private-key",
+        private_key,
+        "--manifest-path",
+        config.manifest_path().as_ref(),
+    ];
+
+    get_snapbox().args(args_vec.iter()).assert().success();
+
+    println!("tmp_dir: {:?}", tmp_dir);
+
+    // Modify the actions contracts to have a new class hash.
+    let actions_path = tmp_dir.join("src/actions.cairo");
+    let mut actions_content = fs::read_to_string(&actions_path).unwrap();
+    actions_content = actions_content.replace("quantity: 100", "quantity: 200");
+    fs::write(&actions_path, actions_content).unwrap();
+
+    let build_vec = ["build", "--manifest-path", config.manifest_path().as_ref()];
+
+    get_snapbox().args(build_vec.iter()).assert().success();
+
+    let assert = get_snapbox().args(args_vec.iter()).assert().success();
+    let output = format!("{:#?}", assert.get_output());
+
+    assert!(output.contains("Contracts (1)"));
 }
