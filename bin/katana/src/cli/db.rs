02e2625aed152d664ab09249b0dd9c19753a0f39
Ammar Arif <evergreenkary@gmail.com>
2024-07-31 14:20:05 -0400

    add freelist stats

--- bin/katana/src/cli/db.rs
+++ bin/katana/src/cli/db.rs
@@ -9,13 +9,13 @@ use katana_db::abstraction::Database;
 use katana_db::mdbx::{DbEnv, DbEnvKind};
 use katana_db::tables::NUM_TABLES;
 
-/// Create a human-readable byte unit string (eg. 1.23 MB)
+/// Create a human-readable byte unit string (eg. 16.00 KiB)
 macro_rules! byte_unit {
     ($size:expr) => {
         format!(
             "{:.2}",
             byte_unit::Byte::from_u64($size as u64)
-                .get_appropriate_unit(byte_unit::UnitType::Decimal)
+                .get_appropriate_unit(byte_unit::UnitType::Binary)
         )
     };
 }
@@ -45,11 +45,12 @@ impl DbArgs {
                 let db = open_db_ro(&self.path)?;
                 let stats = db.stats()?;
 
-                let mut table = Table::new();
+                let mut table = table();
                 let mut rows = Vec::with_capacity(NUM_TABLES);
+                // total size of all tables (incl. freelist)
                 let mut total_size = 0;
 
-                table.load_preset(UTF8_FULL).apply_modifier(UTF8_ROUND_CORNERS).set_header(vec![
+                table.set_header(vec![
                     "Table",
                     "Entries",
                     "Depth",
@@ -58,6 +59,10 @@ impl DbArgs {
                     "Overflow Pages",
                     "Size",
                 ]);
+
+                // page size is equal across all tables, so we can just get it from the first table
+                // and use it to calculate for the freelist table.
+                let mut pagesize: usize = 0;
 
                 for (name, stat) in stats.table_stats().iter() {
                     let entries = stat.entries();
@@ -79,20 +84,38 @@ impl DbArgs {
 
                     // increment the size of all tables
                     total_size += size;
+
+                    if pagesize == 0 {
+                        pagesize = stat.page_size() as usize;
+                    }
                 }
 
                 // sort the rows by the table name
                 rows.sort_by(|a, b| a[0].cmp(&b[0]));
                 table.add_rows(rows);
 
+                // add special row for the freelist table
+                let freelist_size = stats.freelist() * pagesize;
+                total_size += freelist_size;
+
+                table.add_row(vec![
+                    "Freelist".to_string(),
+                    stats.freelist().to_string(),
+                    "-".to_string(),
+                    "-".to_string(),
+                    "-".to_string(),
+                    "-".to_string(),
+                    byte_unit!(freelist_size),
+                ]);
+
                 // add the last row for the total size
                 table.add_row(vec![
                     "Total Size".to_string(),
-                    "".to_string(),
-                    "".to_string(),
-                    "".to_string(),
-                    "".to_string(),
-                    "".to_string(),
+                    "-".to_string(),
+                    "-".to_string(),
+                    "-".to_string(),
+                    "-".to_string(),
+                    "-".to_string(),
                     byte_unit!(total_size),
                 ]);
 
@@ -113,4 +136,11 @@ fn open_db_ro(path: &str) -> Result<DbEnv> {
     DbEnv::open(&path, DbEnvKind::RO).with_context(|| {
         format!("Opening database file in read-only mode at path {}", path.display())
     })
+}
+
+/// Create a table with the default UTF-8 full border and rounded corners.
+fn table() -> Table {
+    let mut table = Table::new();
+    table.load_preset(UTF8_FULL).apply_modifier(UTF8_ROUND_CORNERS);
+    table
 }
