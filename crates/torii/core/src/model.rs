70f9fd74ba215f23adb063acc54fe4f9b1fb8825
Nasr <larko236@outlook.com>
2024-08-07 09:22:22 -0400

    fix: tests

--- crates/torii/core/src/model.rs
+++ crates/torii/core/src/model.rs
@@ -1027,7 +1027,7 @@ mod tests {
              entities.id = [Test-Position$vec].entity_id  JOIN [Test-Position] ON entities.id = \
              [Test-Position].entity_id  JOIN [Test-PlayerConfig$favorite_item] ON entities.id = \
              [Test-PlayerConfig$favorite_item].entity_id  JOIN [Test-PlayerConfig] ON entities.id \
-             = [Test-PlayerConfig].entity_id";
+             = [Test-PlayerConfig].entity_id ORDER BY entities.event_id DESC";
         // todo: completely tests arrays
         assert_eq!(query.0, expected_query);
     }
