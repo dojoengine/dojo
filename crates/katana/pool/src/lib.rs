1794d97fe8da1e2067545f34f2293a85662096e5
Ammar Arif <evergreenkary@gmail.com>
2024-08-07 13:07:51 -0400

    init skeleton

--- crates/katana/pool/src/lib.rs
+++ crates/katana/pool/src/lib.rs
@@ -1,11 +1,16 @@
 // - txs of the same sender must be ordered by nonce (so needs some form of tx ordering mechanism)
 // - gets notification something happen to a transaction (new, removed, executed, etc).
-// - being able to send transactions (that are valid) but with incremented nonce. allowing for sending bunch of txs at once without waiting
+// - being able to send transactions (that are valid) but with incremented nonce. allowing for
+//   sending bunch of txs at once without waiting
 // for the previous one to be executed first (to prevent nonce collision).
 // - subscribe to a particular tx and gets notified when something happened to it (optional).
 
-// - stateful validator running on top of the tx pool that validate incoming tx and puts in the pool if valid. (Adding a pre-validation stage would mean we'd be running the validation stage twice)
-// - valid txs must be valid against all the txs in the pool as well, not just the one in the pending block
+// - stateful validator running on top of the tx pool that validate incoming tx and puts in the pool
+//   if valid. (Adding a pre-validation stage would mean we'd be running the validation stage twice)
+// - valid txs must be valid against all the txs in the pool as well, not just the one in the
+//   pending block
+
+// - use a time-based cache for a rejected txs pool (txs that gets rejected by the TxValidator trait)
 
 // ### State Changes
 //
@@ -14,70 +19,89 @@
 //   - remove mined transactions
 //
 
-use std::marker::PhantomData;
+pub mod tx;
+pub mod ordering;
+pub mod validation;
 
-use katana_primitives::transaction::{ExecutableTxWithHash, TxHash};
+use std::{collections::{BTreeMap, BinaryHeap}, sync::Arc};
 
-pub trait PoolTransaction {
-    fn hash(&self) -> &TxHash;
+use tracing::error;
+use katana_primitives::transaction::TxHash;
+use ordering::PoolOrd;
+use parking_lot::RwLock;
+use tx::{PoolTransaction, TxId};
+use validation::{ValidationOutcome, Validator};
 
-    fn tx(&self) -> &ExecutableTxWithHash;
-
-    /// returns the hash of the pool tx that this tx depends on.
-    fn dependent(&self) -> Option<&TxHash> {
-        None
-    }
+#[derive(Debug, Clone)]
+pub struct TxPool<T, V, O> {
+    inner: Arc<Inner<T, V, O>>,
 }
 
-pub enum PoolTx {
-    Tx,
-    Pending { dependent: TxHash },
+#[derive(Debug)]
+struct Inner<T, V, O> {
+    /// list of all valid txs in the pool
+    valid_txs: RwLock<BTreeMap<TxId, T>>,
+    /// List of independent txs that can be included.
+    ///
+    /// The order of the txs in the btree is determined by the priority values.
+    pending_txs: RwLock<BinaryHeap<T>>,
+    /// list of all invalid (aka rejected) txs in the pool
+    rejected_txs: RwLock<BTreeMap<TxId, T>>,
+    /// the tx validator
+    validator: V,
+    /// the ordering mechanism used to order the txs in the pool
+    ordering: O,
 }
 
-impl PoolTransaction for PoolTx {
-    fn hash(&self) -> &TxHash {
-        todo!()
-    }
+impl<T, V, O> TxPool<T, V, O>
+where
+    T: PoolTransaction,
+    V: Validator<Tx = T>,
+    O: PoolOrd<Tx = T>,
+{
+    /// add tx to the pool
+    ///
+    /// steps that must be taken before putting tx into the pool:
+    /// 1. validate using the pool's [TxValidator](crate::validation::TxValidator)
+    /// 2. if valid, assign a priority value to the tx using the ordering implementation. then insert to the all/pending tx.
+    /// 3. f not valid, insert to the rejected pool
+    /// 
+    // TODO: the API should accept raw tx type, the PoolTransaction should only be used for in the 
+    // pool scope.
+    pub fn add_transaction(&self, tx: T) {
+        match self.inner.validator.validate(tx) {
+            Ok(outcome) => {
+                match outcome {
+                    ValidationOutcome::Valid { tx, .. } => {
+                        let priority = self.inner.ordering.priority(&tx);
 
-    fn tx(&self) -> &ExecutableTxWithHash {
-        todo!()
-    }
+                        // TODO: convert the base tx into a pool tx with the priority value attached
 
-    fn dependent(&self) -> Option<&TxHash> {
-        match self {
-            PoolTx::Pending { dependent: parent } => Some(parent),
-            _ => None,
+                        // self.all_txs.write().insert(tx.id().clone(), tx.clone());
+                        // self.pending_txs.write().push(tx);
+                    },
+                    ValidationOutcome::Invalid { tx, .. } => {
+                        todo!("insert into the rejected pool");
+                    },
+                }
+
+                // TODO: notify listeners
+            },
+
+            Err(e) => {
+                error!(e);
+            }
         }
     }
-}
-
-pub trait Ordering<Tx: PoolTransaction> {
-    // order the given list of transactions. the ordering must be deterministic.
-    //
-    // the ordering implementation should be aware of the dependencies between the txs in the pool.
-    fn order(&self, txs: &mut Vec<Tx>);
-}
-
-pub struct TxPool<T, O> {
-    /// List of txs. the list must be ordered by whatever ordering mechanism is used.
-    pending_txs: Vec<T>,
-    /// the ordering mechanism used to order the txs in the pool
-    _ordering: PhantomData<O>,
-}
-
-impl<T, O> TxPool<T, O>
-where
-    O: Ordering<T>,
-    T: PoolTransaction,
-{
-    // add tx to the pool
-    pub fn add_transaction(&self) {}
 
     // returns transactions that are ready and valid to be included in the next block.
     //
     // best transactions must be ordered by the ordering mechanism used.
-    pub fn best_transactions(&self) -> impl Iterator<Item = &T> {
-        self.pending_txs.iter()
+    pub fn best_transactions(&self) -> impl Iterator<Item = T> {
+        BestTxs {
+            all: self.inner.valid_txs.read().clone(),
+            pending: self.inner.pending_txs.read().clone(),
+        }
     }
 
     // check if a tx is in the pool
@@ -85,40 +109,49 @@ where
         self.find(hash).is_some()
     }
 
-    pub fn find(&self, hash: TxHash) -> Option<&PoolTx> {
+    pub fn find(&self, hash: TxHash) -> Option<T> {
         todo!()
     }
 
-    // remove tx from the pool
+    // to be used for removing transactions that have been included in a block, and no longer
+    // needs to be kept around in the pool.
     //
-    // this should also remove the tx from the dependencies map along
-    // with all the txs that depend on it.
-    //
-    // removing an element must preserve the ordering of the other txs in the pool.
-    pub fn remove_transaction(&mut self, hash: TxHash) {
+    // should remove from all the pools.
+    pub fn remove_transactions(&mut self, hashes: &[TxHash]) {
         todo!()
     }
 }
 
-trait TxValidator: Send + Sync {
-    type Tx;
+/// an iterator that yields transactions from the pool that can be included in a block, sorted by
+/// by its priority.
+#[derive(Debug)]
+struct BestTxs<T> {
+    all: BTreeMap<TxId, T>,
+    pending: BinaryHeap<T>,
+}
 
-    fn validate(
-        &self,
-        tx: &Self::Tx,
-    ) -> Result<TxValidationOutcome<Self::Tx>, Box<dyn std::error::Error>>;
+impl<T> Iterator for BestTxs<T>
+where
+    T: PoolTransaction + Clone,
+{
+    type Item = T;
 
-    fn validate_all(
-        &self,
-        txs: Vec<Self::Tx>,
-    ) -> Vec<Result<TxValidationOutcome<Self::Tx>, Box<dyn std::error::Error>>> {
-        txs.into_iter().map(|tx| self.validate(&tx)).collect()
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(tx) = self.pending.pop() {
+            // check if there's a dependent tx that gets unlocked by this tx
+            if let Some(depedent) = self.all.get(&tx.id().descendent()) {
+                self.pending.push(depedent.clone());
+            }
+            Some(tx)
+        } else {
+            None
+        }
     }
 }
 
-enum TxValidationOutcome<T> {
-    Valid { tx: T }, // valid and can be picked up by the ordering mechanism
-    Invalid { tx: T, error: Box<dyn std::error::Error> }, // aka rejected in starknet terms
-}
+#[cfg(test)]
+mod tests {
 
-// impl TxValidator for StatefulValidator {}
+    #[test]
+    fn add_dependent_txs_in_parallel() {}
+}
---
+++ crates/katana/pool/src/ordering.rs
@@ -0,0 +1,74 @@
+use std::marker::PhantomData;
+use std::sync::atomic::{AtomicU64, Ordering as AtomicOrdering};
+
+use crate::PoolTransaction;
+
+// evaluates the priority of a transaction which would be used to determine how txs are ordered in
+// the pool.
+pub trait PoolOrd {
+    type Tx: PoolTransaction;
+
+    type PriorityValue: Ord;
+
+    // returns the priority value of a transaction
+    fn priority(&self, tx: &Self::Tx) -> Self::PriorityValue;
+}
+
+/// First-come-first-serve ordering implementation.
+///
+/// This ordering implementation can be generic over any transaction type as it doesn't require any
+/// context on the tx data itself.
+#[derive(Debug)]
+pub struct Fcfs<T> {
+    nonce: AtomicU64,
+    _tx: PhantomData<T>,
+}
+
+impl<T: PoolTransaction> Fcfs<T> {
+    pub fn new() -> Self {
+        Self { nonce: AtomicU64::new(0), _tx: PhantomData }
+    }
+}
+
+impl<T: PoolTransaction> PoolOrd for Fcfs<T> {
+    type Tx = T;
+    type PriorityValue = SubmissionNonce;
+
+    fn priority(&self, _: &Self::Tx) -> Self::PriorityValue {
+        SubmissionNonce(self.nonce.fetch_add(1, AtomicOrdering::Relaxed))
+    }
+}
+
+#[derive(Debug, Default, Clone, Copy)]
+pub struct SubmissionNonce(u64);
+
+impl SubmissionNonce {
+    fn new() -> Self {
+        SubmissionNonce::default()
+    }
+
+    fn next(&self) -> Self {
+        SubmissionNonce(self.0 + 1)
+    }
+}
+
+impl Ord for SubmissionNonce {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        // Reverse the ordering so lower values have higher priority
+        other.0.cmp(&self.0)
+    }
+}
+
+impl Eq for SubmissionNonce {}
+
+impl PartialOrd for SubmissionNonce {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialEq for SubmissionNonce {
+    fn eq(&self, other: &Self) -> bool {
+        self.0 == other.0
+    }
+}
