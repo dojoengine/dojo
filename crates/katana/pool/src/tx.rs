f014896e6e983c835897702193b99ed1a6edfeee
Ammar Arif <evergreenkary@gmail.com>
2024-08-07 16:12:11 -0400

    wip

--- crates/katana/pool/src/tx.rs
+++ crates/katana/pool/src/tx.rs
@@ -1,5 +1,6 @@
 use std::sync::Arc;
 
+use katana_executor::ExecutionError;
 use katana_primitives::contract::{ContractAddress, Nonce};
 use katana_primitives::transaction::TxHash;
 
@@ -12,6 +13,10 @@ pub trait PoolTransaction: Ord + Clone {
     fn id(&self) -> &TxId;
     /// return the tx hash.
     fn hash(&self) -> &TxHash;
+    /// return the tx nonce.
+    fn nonce(&self) -> &Nonce;
+    /// return the tx sender.
+    fn sender(&self) -> &ContractAddress;
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
@@ -39,13 +44,19 @@ impl TxId {
 }
 
 #[derive(Debug)]
-pub struct PoolTx<T, O: PoolOrd> {
+pub struct ValidPoolTx<T, O: PoolOrd> {
     pub id: TxId,
     pub tx: Arc<T>,
     pub priority: O::PriorityValue,
 }
 
-impl<T, O> Clone for PoolTx<T, O>
+impl<T, O: PoolOrd> ValidPoolTx<T, O> {
+    pub fn new(id: TxId, tx: T, priority: O::PriorityValue) -> Self {
+        Self { id, tx: Arc::new(tx), priority }
+    }
+}
+
+impl<T, O> Clone for ValidPoolTx<T, O>
 where
     O: PoolOrd,
 {
@@ -54,28 +65,33 @@ where
     }
 }
 
-impl<T, O: PoolOrd> PoolTx<T, O> {
-    pub fn new(id: TxId, tx: T, priority: O::PriorityValue) -> Self {
-        Self { id, tx: Arc::new(tx), priority }
-    }
-}
-
-impl<T, O: PoolOrd> PartialEq for PoolTx<T, O> {
+impl<T, O: PoolOrd> PartialEq for ValidPoolTx<T, O> {
     fn eq(&self, other: &Self) -> bool {
         self.priority == other.priority
     }
 }
 
-impl<T, O: PoolOrd> Eq for PoolTx<T, O> {}
+impl<T, O: PoolOrd> Eq for ValidPoolTx<T, O> {}
 
-impl<T, O: PoolOrd> PartialOrd for PoolTx<T, O> {
+impl<T, O: PoolOrd> PartialOrd for ValidPoolTx<T, O> {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
         Some(self.cmp(other))
     }
 }
 
-impl<T, O: PoolOrd> Ord for PoolTx<T, O> {
+impl<T, O: PoolOrd> Ord for ValidPoolTx<T, O> {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
         self.priority.cmp(&other.priority)
+    }
+}
+
+pub struct InvalidPoolTx<T> {
+    pub tx: Arc<T>,
+    pub error: ExecutionError,
+}
+
+impl<T> InvalidPoolTx<T> {
+    pub fn new(tx: T, error: ExecutionError) -> Self {
+        Self { tx: Arc::new(tx), error }
     }
 }
--- crates/katana/pool/src/validation/stateful.rs
+++ crates/katana/pool/src/validation/stateful.rs
@@ -2,25 +2,26 @@ use blockifier::blockifier::stateful_validator;
 use katana_executor::StateProviderDb;
 use parking_lot::Mutex;
 
-use super::{ValidationOutcome, Validator};
+use super::{ValidationResult, Validator};
 
 // Validates the incoming tx before adding them in the pool.
 // Runs the validation logic of the tx and keep track of the tx dependencies.
 //
-// the executor must wrap the pending state so that it can perform validation against the pending block state.
-// this validator only validate the tx validation and doesn't take into consideration of its nonce.
+// the executor must wrap the pending state so that it can perform validation against the pending
+// block state. this validator only validate the tx validation and doesn't take into consideration
+// of its nonce.
 pub struct StatefulValidator {
     inner: Mutex<stateful_validator::StatefulValidator<StateProviderDb<'static>>>,
 }
 
-impl Validator for StatefulValidator {
-    type Tx = ();
+// impl Validator for StatefulValidator {
+//     type Tx = todo!();
 
-    fn validate(
-        &self,
-        tx: Self::Tx,
-    ) -> Result<ValidationOutcome<Self::Tx>, Box<dyn std::error::Error>> {
-        let result = self.inner.lock().perform_validations(todo!(), None);
-        Ok(ValidationOutcome::Valid { tx })
-    }
-}
+//     fn validate(
+//         &self,
+//         tx: Self::Tx,
+//     ) -> Result<ValidationResult<Self::Tx>, Box<dyn std::error::Error>> {
+//         let result = self.inner.lock().perform_validations(todo!(), None);
+//         Ok(ValidationResult::Valid { tx })
+//     }
+// }
