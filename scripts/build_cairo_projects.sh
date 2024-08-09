1dfd0bbe005cdcbbe455c545a15e30904c945728
glihm <dev@glihm.net>
2024-08-09 18:39:50 -0400

    fix: adjustement for dev pipeline

--- scripts/build_cairo_projects.sh
+++ scripts/build_cairo_projects.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# usage: bash scripts/build_cairo_projects.sh <sozo_path>
 sozo_path=$1
 
 # Re-run the minimal tests, this will re-build the projects + generate the build artifacts.
 $sozo_path build --manifest-path examples/spawn-and-move/Scarb.toml
 $sozo_path build --manifest-path examples/spawn-and-move/Scarb.toml -P release
+$sozo_path build --manifest-path crates/torii/types-test/Scarb.toml
