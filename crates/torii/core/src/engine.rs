84f88fc9f6ab927b331562c4d5951927e1f34a5e
Larko <59736843+Larkooo@users.noreply.github.com>
2024-08-15 10:55:57 -0400

    fix(torii-core): engine update cursor when world event (#2300)
    * fix(torii-core): engine update cursor when world event
    
    * chore: clean

--- crates/torii/core/src/engine.rs
+++ crates/torii/core/src/engine.rs
@@ -18,6 +18,7 @@ use tracing::{error, info, trace, warn};
 
 use crate::processors::{BlockProcessor, EventProcessor, TransactionProcessor};
 use crate::sql::Sql;
+
 #[allow(missing_debug_implementations)]
 pub struct Processors<P: Provider + Sync> {
     pub block: Vec<Box<dyn BlockProcessor<P>>>,
@@ -196,12 +197,14 @@ impl<P: Provider + Sync> Engine<P> {
                         }
                     }
                 }
+                Ok(true) => {
+                    pending_block_tx = Some(*transaction.transaction_hash());
+                    info!(target: LOG_TARGET, transaction_hash = %format!("{:#x}", transaction.transaction_hash()), "Processed pending world transaction.");
+                }
                 Ok(_) => {
                     info!(target: LOG_TARGET, transaction_hash = %format!("{:#x}", transaction.transaction_hash()), "Processed pending transaction.")
                 }
             }
-
-            pending_block_tx = Some(*transaction.transaction_hash());
         }
 
         // Set the head to the last processed pending transaction
@@ -311,17 +314,8 @@ impl<P: Provider + Sync> Engine<P> {
             }
         }
 
-        // Process blocks
-        for (block_number, block_timestamp) in blocks.iter() {
-            if let Some(ref block_tx) = self.block_tx {
-                block_tx.send(*block_number).await?;
-            }
-
-            self.process_block(*block_number, *block_timestamp).await?;
-            info!(target: LOG_TARGET, block_number = %block_number, "Processed block.");
-        }
-
         // Process all transactions
+        let mut last_block = 0;
         for (block_number, transaction_hash) in transactions {
             // Process transaction
             let transaction = self.provider.get_transaction_by_hash(transaction_hash).await?;
@@ -333,6 +327,16 @@ impl<P: Provider + Sync> Engine<P> {
                 blocks[&block_number],
             )
             .await?;
+
+            // Process block
+            if block_number > last_block {
+                if let Some(ref block_tx) = self.block_tx {
+                    block_tx.send(block_number).await?;
+                }
+
+                self.process_block(block_number, blocks[&block_number]).await?;
+                last_block = block_number;
+            }
         }
 
         // We return None for the pending_block_tx because our sync_range
@@ -355,13 +359,15 @@ impl<P: Provider + Sync> Engine<P> {
         }
     }
 
+    // Process a transaction and its receipt.
+    // Returns whether the transaction has a world event.
     async fn process_transaction_and_receipt(
         &mut self,
         transaction_hash: Felt,
         transaction: &Transaction,
         block_number: u64,
         block_timestamp: u64,
-    ) -> Result<()> {
+    ) -> Result<bool> {
         let receipt = self.provider.get_transaction_receipt(transaction_hash).await?;
         let events = match &receipt.receipt {
             TransactionReceipt::Invoke(receipt) => Some(&receipt.events),
@@ -369,8 +375,8 @@ impl<P: Provider + Sync> Engine<P> {
             _ => None,
         };
 
+        let mut world_event = false;
         if let Some(events) = events {
-            let mut world_event = false;
             for (event_idx, event) in events.iter().enumerate() {
                 if event.from_address != self.world.address {
                     continue;
@@ -404,7 +410,7 @@ impl<P: Provider + Sync> Engine<P> {
             }
         }
 
-        Ok(())
+        Ok(world_event)
     }
 
     async fn process_block(&mut self, block_number: u64, block_timestamp: u64) -> Result<()> {
@@ -413,6 +419,8 @@ impl<P: Provider + Sync> Engine<P> {
                 .process(&mut self.db, self.provider.as_ref(), block_number, block_timestamp)
                 .await?
         }
+
+        info!(target: LOG_TARGET, block_number = %block_number, "Processed block.");
         Ok(())
     }
 
