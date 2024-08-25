194438922a028dac6bf40533ce139dd742809902
ADR!AN <111903096+adrianvrj@users.noreply.github.com>
2024-08-24 20:33:24 -0600

    fix(katana): ensure messages are ordered (#2326)
    * [fix] converted hash map to ordered vec in fetch events func
    
    * [fix] Orthographic error in comment
    
    * [fix] run fmt
    
    * [fix] returning a single Vec<EmittedEvent>
    
    * [fix] returning a Vec<Log> for ethereum.rs  fetch_logs
    
    * [fix] debugger
    
    * [fix] using compiler inference for Vecs
    
    * Update crates/katana/core/src/service/messaging/starknet.rs
    
    Co-authored-by: glihm <dev@glihm.net>
    
    ---------
    
    Co-authored-by: glihm <dev@glihm.net>

--- crates/katana/core/src/service/messaging/ethereum.rs
+++ crates/katana/core/src/service/messaging/ethereum.rs
@@ -1,6 +1,5 @@
 #![allow(dead_code)]
 
-use std::collections::HashMap;
 use std::str::FromStr;
 use std::sync::Arc;
 
@@ -72,14 +71,10 @@ impl EthereumMessaging {
     ///
     /// * `from_block` - The first block of which logs must be fetched.
     /// * `to_block` - The last block of which logs must be fetched.
-    pub async fn fetch_logs(
-        &self,
-        from_block: u64,
-        to_block: u64,
-    ) -> MessengerResult<HashMap<u64, Vec<Log>>> {
+    pub async fn fetch_logs(&self, from_block: u64, to_block: u64) -> MessengerResult<Vec<Log>> {
         trace!(target: LOG_TARGET, from_block = ?from_block, to_block = ?to_block, "Fetching logs.");
 
-        let mut block_to_logs: HashMap<u64, Vec<Log>> = HashMap::new();
+        let mut logs = vec![];
 
         let filters = Filter {
             block_option: FilterBlockOption::Range {
@@ -107,15 +102,11 @@ impl EthereumMessaging {
             .await?
             .into_iter()
             .filter(|log| log.block_number.is_some())
-            .map(|log| (log.block_number.unwrap(), log))
-            .for_each(|(block_num, log)| {
-                block_to_logs
-                    .entry(block_num)
-                    .and_modify(|v| v.push(log.clone()))
-                    .or_insert(vec![log]);
+            .for_each(|log| {
+                logs.push(log);
             });
 
-        Ok(block_to_logs)
+        Ok(logs)
     }
 }
 
@@ -143,22 +134,17 @@ impl Messenger for EthereumMessaging {
         let mut l1_handler_txs = vec![];
 
         trace!(target: LOG_TARGET, from_block, to_block, "Fetching logs from {from_block} to {to_block}.");
-        self.fetch_logs(from_block, to_block).await?.into_iter().for_each(
-            |(block_number, block_logs)| {
-                debug!(
-                    target: LOG_TARGET,
-                    block_number = %block_number,
-                    logs_found = %block_logs.len(),
-                    "Converting logs into L1HandlerTx.",
-                );
+        self.fetch_logs(from_block, to_block).await?.iter().for_each(|l| {
+            debug!(
+                target: LOG_TARGET,
+                log = ?l,
+                "Converting log into L1HandlerTx.",
+            );
 
-                block_logs.into_iter().for_each(|log| {
-                    if let Ok(tx) = l1_handler_tx_from_log(log, chain_id) {
-                        l1_handler_txs.push(tx)
-                    }
-                })
-            },
-        );
+            if let Ok(tx) = l1_handler_tx_from_log(l.clone(), chain_id) {
+                l1_handler_txs.push(tx)
+            }
+        });
 
         Ok((to_block, l1_handler_txs))
     }
