74952008ee935383c8713ba871e071db07654f89
Ammar Arif <evergreenkary@gmail.com>
2024-08-28 11:28:53 -0400

    fix

--- crates/katana/pool/src/validation/stateful.rs
+++ crates/katana/pool/src/validation/stateful.rs
@@ -93,7 +93,7 @@ impl StatefulValidatorAdapter {
         cfg_env: &CfgEnv,
     ) -> Self {
         let block_context = block_context_from_envs(block_env, cfg_env);
-        let inner = StatefulValidator::create(state, block_context);
+        let inner = StatefulValidator::create(state, block_context, Default::default());
         Self { inner }
     }
 
@@ -119,7 +119,17 @@ impl StatefulValidatorAdapter {
                     });
                 }
 
-                match self.inner.perform_validations(blockifier_tx, skip_validate, skip_fee_check) {
+                match self.inner.perform_validations(
+                    blockifier_tx,
+                    {
+                        if skip_validate {
+                            Some(Default::default())
+                        } else {
+                            None
+                        }
+                    },
+                    skip_fee_check,
+                ) {
                     Ok(()) => Ok(ValidationOutcome::Valid(tx)),
                     Err(e) => match map_invalid_tx_err(e) {
                         Ok(error) => Ok(ValidationOutcome::Invalid { tx, error }),
