842c2569c1ad29415ea85b2d32e30412b8106644
Ammar Arif <evergreenkary@gmail.com>
2024-08-29 23:19:45 -0400

    refactor(katana-pool): enable ordering using `BTreeSet` (#2370)

--- crates/katana/pool/src/ordering.rs
+++ crates/katana/pool/src/ordering.rs
@@ -53,7 +53,7 @@ pub struct TxSubmissionNonce(u64);
 impl Ord for TxSubmissionNonce {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
         // Reverse the ordering so lower values have higher priority
-        other.0.cmp(&self.0)
+        self.0.cmp(&other.0)
     }
 }
 
@@ -76,25 +76,136 @@ impl PartialEq for TxSubmissionNonce {
 /// This ordering implementation uses the transaction's tip as the priority value. We don't have a
 /// use case for this ordering implementation yet, but it's mostly used for testing.
 #[derive(Debug)]
-pub struct Tip<T>(PhantomData<T>);
+pub struct TipOrdering<T>(PhantomData<T>);
 
-impl<T> Tip<T> {
+impl<T> TipOrdering<T> {
     pub fn new() -> Self {
         Self(PhantomData)
     }
 }
 
-impl<T: PoolTransaction> PoolOrd for Tip<T> {
-    type Transaction = T;
-    type PriorityValue = u64;
+#[derive(Debug, Clone)]
+pub struct Tip(u64);
 
-    fn priority(&self, tx: &Self::Transaction) -> Self::PriorityValue {
-        tx.tip()
+impl Ord for Tip {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        other.0.cmp(&self.0)
     }
 }
 
-impl<T> Default for Tip<T> {
+impl PartialOrd for Tip {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialEq for Tip {
+    fn eq(&self, other: &Self) -> bool {
+        self.0 == other.0
+    }
+}
+
+impl Eq for Tip {}
+
+impl<T: PoolTransaction> PoolOrd for TipOrdering<T> {
+    type Transaction = T;
+    type PriorityValue = Tip;
+
+    fn priority(&self, tx: &Self::Transaction) -> Self::PriorityValue {
+        Tip(tx.tip())
+    }
+}
+
+impl<T> Default for TipOrdering<T> {
     fn default() -> Self {
         Self::new()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use crate::ordering::{self, FiFo};
+    use crate::pool::test_utils::*;
+    use crate::tx::PoolTransaction;
+    use crate::validation::NoopValidator;
+    use crate::{Pool, TransactionPool};
+
+    #[test]
+    fn fifo_ordering() {
+        // Create mock transactions
+        let txs = [PoolTx::new(), PoolTx::new(), PoolTx::new(), PoolTx::new(), PoolTx::new()];
+
+        // Create a pool with FiFo ordering
+        let pool = Pool::new(NoopValidator::new(), FiFo::new());
+
+        // Add transactions to the pool
+        txs.iter().for_each(|tx| {
+            let _ = pool.add_transaction(tx.clone());
+        });
+
+        // Get pending transactions
+        let pendings = pool.take_transactions().collect::<Vec<_>>();
+
+        // Assert that the transactions are in the order they were added (first to last)
+        pendings.iter().zip(txs).for_each(|(pending, tx)| {
+            assert_eq!(pending.tx.as_ref(), &tx);
+        });
+    }
+
+    #[test]
+    fn tip_based_ordering() {
+        // Create mock transactions with different tips and in random order
+        let txs = [
+            PoolTx::new().with_tip(2),
+            PoolTx::new().with_tip(1),
+            PoolTx::new().with_tip(6),
+            PoolTx::new().with_tip(3),
+            PoolTx::new().with_tip(2),
+            PoolTx::new().with_tip(2),
+            PoolTx::new().with_tip(5),
+            PoolTx::new().with_tip(4),
+            PoolTx::new().with_tip(7),
+        ];
+
+        // Create a pool with tip-based ordering
+        let pool = Pool::new(NoopValidator::new(), ordering::TipOrdering::new());
+
+        // Add transactions to the pool
+        txs.iter().for_each(|tx| {
+            let _ = pool.add_transaction(tx.clone());
+        });
+
+        // Get pending transactions
+        let pending = pool.take_transactions().collect::<Vec<_>>();
+        assert_eq!(pending.len(), txs.len());
+
+        // Assert that the transactions are ordered by tip (highest to lowest)
+        assert_eq!(pending[0].tx.tip(), 7);
+        assert_eq!(pending[0].tx.hash(), txs[8].hash());
+
+        assert_eq!(pending[1].tx.tip(), 6);
+        assert_eq!(pending[1].tx.hash(), txs[2].hash());
+
+        assert_eq!(pending[2].tx.tip(), 5);
+        assert_eq!(pending[2].tx.hash(), txs[6].hash());
+
+        assert_eq!(pending[3].tx.tip(), 4);
+        assert_eq!(pending[3].tx.hash(), txs[7].hash());
+
+        assert_eq!(pending[4].tx.tip(), 3);
+        assert_eq!(pending[4].tx.hash(), txs[3].hash());
+
+        assert_eq!(pending[5].tx.tip(), 2);
+        assert_eq!(pending[5].tx.hash(), txs[0].hash());
+
+        assert_eq!(pending[6].tx.tip(), 2);
+        assert_eq!(pending[6].tx.hash(), txs[4].hash());
+
+        assert_eq!(pending[7].tx.tip(), 2);
+        assert_eq!(pending[7].tx.hash(), txs[5].hash());
+
+        assert_eq!(pending[8].tx.tip(), 1);
+        assert_eq!(pending[8].tx.hash(), txs[1].hash());
     }
 }
