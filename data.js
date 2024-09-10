409052130d16ad8282e2f26fcfc99ff3eac9e93e
github-action-benchmark <github@users.noreply.github.com>
2024-09-10 17:37:31 +0000

    add Benchmark (cargo) benchmark result for 1ef88f846a84b674c41ddd13a85b212b6744625f

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1725989817123,
+  "lastUpdate": 1725989851389,
   "repoUrl": "https://github.com/dojoengine/dojo",
   "entries": {
     "Benchmark": [
@@ -19441,6 +19441,54 @@ window.BENCHMARK_DATA = {
             "name": "Invoke.ERC20.transfer/Blockifier.Cold",
             "value": 3960470,
             "range": "± 158880",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "evergreenkary@gmail.com",
+            "name": "Ammar Arif",
+            "username": "kariy"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1ef88f846a84b674c41ddd13a85b212b6744625f",
+          "message": "feat(katana): add node handle (#2408)",
+          "timestamp": "2024-09-11T01:19:11+08:00",
+          "tree_id": "70b050cbfb0bd4cac4b5c82ebfaa9825276b8b04",
+          "url": "https://github.com/dojoengine/dojo/commit/1ef88f846a84b674c41ddd13a85b212b6744625f"
+        },
+        "date": 1725989850266,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "decompress world contract",
+            "value": 17643057,
+            "range": "± 386033",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Concurrent.Simulate/Blockifier.1",
+            "value": 3904800,
+            "range": "± 112378",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Concurrent.Simulate/Blockifier.1000",
+            "value": 3529445792,
+            "range": "± 857963829",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Invoke.ERC20.transfer/Blockifier.Cold",
+            "value": 3923452,
+            "range": "± 19517",
             "unit": "ns/iter"
           }
         ]
