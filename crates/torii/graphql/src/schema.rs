9db7b9b4cd7bb211430aca6666a601ee9fbf1165
lambda-0x <0xlambda@protonmail.com>
2024-09-19 16:38:15 +0530

    feat(torii): implement graphql for erc
    commit-id:10465a00

--- crates/torii/graphql/src/schema.rs
+++ crates/torii/graphql/src/schema.rs
@@ -10,6 +10,9 @@ use super::object::model_data::ModelDataObject;
 use super::types::ScalarType;
 use super::utils;
 use crate::constants::{QUERY_TYPE_NAME, SUBSCRIPTION_TYPE_NAME};
+use crate::object::erc::erc_balance::ErcBalanceObject;
+use crate::object::erc::erc_token::ErcTokenObject;
+use crate::object::erc::erc_transfer::ErcTransferObject;
 use crate::object::event_message::EventMessageObject;
 use crate::object::metadata::content::ContentObject;
 use crate::object::metadata::social::SocialObject;
@@ -28,6 +31,7 @@ pub async fn build_schema(pool: &SqlitePool) -> Result<Schema> {
     let (objects, unions) = build_objects(pool).await?;
 
     let mut schema_builder = Schema::build(QUERY_TYPE_NAME, None, Some(SUBSCRIPTION_TYPE_NAME));
+    //? why we need to provide QUERY_TYPE_NAME object here when its already passed to Schema?
     let mut query_root = Object::new(QUERY_TYPE_NAME);
     let mut subscription_root = Subscription::new(SUBSCRIPTION_TYPE_NAME);
 
@@ -112,9 +116,12 @@ async fn build_objects(pool: &SqlitePool) -> Result<(Vec<ObjectVariant>, Vec<Uni
         ObjectVariant::Resolvable(Box::new(MetadataObject)),
         ObjectVariant::Resolvable(Box::new(ModelObject)),
         ObjectVariant::Resolvable(Box::new(TransactionObject)),
+        ObjectVariant::Resolvable(Box::new(ErcBalanceObject)),
+        ObjectVariant::Resolvable(Box::new(ErcTransferObject)),
         ObjectVariant::Basic(Box::new(SocialObject)),
         ObjectVariant::Basic(Box::new(ContentObject)),
         ObjectVariant::Basic(Box::new(PageInfoObject)),
+        ObjectVariant::Basic(Box::new(ErcTokenObject)),
     ];
 
     // model union object
