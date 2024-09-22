c0163931c9f133af304a67f30bb31cb2b55df920
Ammar Arif <evergreenkary@gmail.com>
2024-09-22 15:24:54 -0400

    clippy

--- crates/katana/runner/macro/src/lib.rs
+++ crates/katana/runner/macro/src/lib.rs
@@ -1,3 +1,5 @@
+#![cfg_attr(not(test), warn(unused_crate_dependencies))]
+
 //! Implementation of the proc macro in this module is highly adapted from `tokio-macros` crate.
 //! `tokio-macro`: https://docs.rs/tokio-macros/2.4.0/tokio_macros/
 
