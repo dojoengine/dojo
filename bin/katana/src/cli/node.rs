7f5bbe7cb4f98dafd6847794857cb60ec76f9993
glihm <dev@glihm.net>
2024-09-24 10:11:03 -0400

    fix: ensure log crate is taken in account by tracing

--- bin/katana/src/cli/node.rs
+++ bin/katana/src/cli/node.rs
@@ -35,6 +35,7 @@ use katana_primitives::genesis::Genesis;
 use katana_rpc::config::ServerConfig;
 use katana_rpc_api::ApiKind;
 use tracing::{info, Subscriber};
+use tracing_log::LogTracer;
 use tracing_subscriber::{fmt, EnvFilter};
 use url::Url;
 
@@ -249,6 +250,8 @@ impl NodeArgs {
         const DEFAULT_LOG_FILTER: &str = "info,executor=trace,forking::backend=trace,server=debug,\
                                           katana_core=trace,blockifier=off,jsonrpsee_server=off,\
                                           hyper=off,messaging=debug,node=error";
+
+        LogTracer::init()?;
 
         let builder = fmt::Subscriber::builder().with_env_filter(
             EnvFilter::try_from_default_env().or(EnvFilter::try_new(DEFAULT_LOG_FILTER))?,
