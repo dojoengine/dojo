3ebf2d4db6165b397405719b5de3570249f8fc75
github-action-benchmark <github@users.noreply.github.com>
2024-09-30 23:25:15 +0000

    add Benchmark (cargo) benchmark result for 5eb722ac28080d656599de5f681942e0e5762f6d

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1727738613907,
+  "lastUpdate": 1727738715840,
   "repoUrl": "https://github.com/dojoengine/dojo",
   "entries": {
     "Benchmark": [
@@ -22951,6 +22951,54 @@ window.BENCHMARK_DATA = {
             "name": "build/Sozo.Cold",
             "value": 11203914678,
             "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "evergreenkary@gmail.com",
+            "name": "Ammar Arif",
+            "username": "kariy"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5eb722ac28080d656599de5f681942e0e5762f6d",
+          "message": "chore: replace `reth-metrics-derive` with the new standalone version (#2487)\n\nthe reth team has made the previously named reth-metrics-derive into a standalone crate, metrics-derive",
+          "timestamp": "2024-09-30T19:08:11-04:00",
+          "tree_id": "5cc0b26957934ee1c97768089b60fe44747485f4",
+          "url": "https://github.com/dojoengine/dojo/commit/5eb722ac28080d656599de5f681942e0e5762f6d"
+        },
+        "date": 1727738714955,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "decompress world contract",
+            "value": 15745374,
+            "range": "± 304161",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Concurrent.Simulate/Blockifier.1",
+            "value": 3824425,
+            "range": "± 73050",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Concurrent.Simulate/Blockifier.1000",
+            "value": 2476345971,
+            "range": "± 264076035",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Invoke.ERC20.transfer/Blockifier.Cold",
+            "value": 3918328,
+            "range": "± 46996",
             "unit": "ns/iter"
           }
         ]
