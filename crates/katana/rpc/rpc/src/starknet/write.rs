c9e014726fffd434818cca084537a8b94ea80d39
Ammar Arif <evergreenkary@gmail.com>
2024-10-12 01:01:29 -0400

    wip

--- crates/katana/rpc/rpc/src/starknet/write.rs
+++ crates/katana/rpc/rpc/src/starknet/write.rs
@@ -23,8 +23,7 @@ impl<EF: ExecutorFactory> StarknetApi<EF> {
 
             let tx = tx.into_tx_with_chain_id(this.inner.backend.chain_spec.id);
             let tx = ExecutableTxWithHash::new(ExecutableTx::Invoke(tx));
-            let hash =
-                this.inner.pool.add_transaction(tx).inspect_err(|e| println!("Error: {:?}", e))?;
+            let hash = this.inner.pool.add_transaction(tx)?;
 
             Ok(hash.into())
         })
