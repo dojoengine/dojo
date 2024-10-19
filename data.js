d317aefb9dc0a1a79e96facd18fc2213882ab03b
github-action-benchmark <github@users.noreply.github.com>
2024-10-19 17:25:39 +0000

    add Benchmark (cargo) benchmark result for a20b53c5b5e09f556f4c0ffac2bd1b0c9104946c

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1729358546040,
+  "lastUpdate": 1729358739799,
   "repoUrl": "https://github.com/dojoengine/dojo",
   "entries": {
     "Benchmark": [
@@ -27163,6 +27163,54 @@ window.BENCHMARK_DATA = {
             "name": "build/Sozo.Cold",
             "value": 11989564941,
             "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "evergreenkary@gmail.com",
+            "name": "Ammar Arif",
+            "username": "kariy"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a20b53c5b5e09f556f4c0ffac2bd1b0c9104946c",
+          "message": "feat(katana): commitment fields in block header (#2560)\n\nAdd new fields for block commitments; `state_diff_commitment`, `transactions_commitment`, `receipts_commitment`, `events_commitment`, `transaction_count`, `events_count`. as described in the starknet [docs](https://docs.starknet.io/architecture-and-concepts/network-architecture/block-structure/).\r\n\r\nAlso removed `SealedHeader` and flatten the hash computation logic into `SealedBlock` directly.",
+          "timestamp": "2024-10-20T01:06:13+08:00",
+          "tree_id": "c1786e948b15a005524f659ae2d106c3697625ef",
+          "url": "https://github.com/dojoengine/dojo/commit/a20b53c5b5e09f556f4c0ffac2bd1b0c9104946c"
+        },
+        "date": 1729358737642,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "decompress world contract",
+            "value": 16359124,
+            "range": "± 228848",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Concurrent.Simulate/Blockifier.1",
+            "value": 4529289,
+            "range": "± 548805",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Concurrent.Simulate/Blockifier.1000",
+            "value": 3816143897,
+            "range": "± 784588670",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Invoke.ERC20.transfer/Blockifier.Cold",
+            "value": 4650884,
+            "range": "± 488008",
             "unit": "ns/iter"
           }
         ]
