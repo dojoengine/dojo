4ad6d2c3ded2986bdf46a710212e92208fad6843
Ammar Arif <evergreenkary@gmail.com>
2024-10-22 13:02:23 -0400

    feat(katana-db): ephemeral database environment (#2567)
    this will eventually be used to as the storage backend for in-memory provider, so as to simplify the logic. having to maintain completely different logic for multiple providers is a lot of work. eventually we should just use the database api abstraction for provider to storage interaction. 
    
    therefore, the ephemeral database is initialized with the lowest durability configuration using [`MDBX_UTTERLY_NOSYNC`](https://erthink.github.io/libmdbx/group__c__opening.html#gga9138119a904355d245777c4119534061aec1ed5ccbf0626391088f787ffc4962c) to trade off durability for better write performance.

--- crates/katana/storage/db/src/mdbx/mod.rs
+++ crates/katana/storage/db/src/mdbx/mod.rs
@@ -7,7 +7,8 @@ pub mod stats;
 pub mod tx;
 
 use std::collections::HashMap;
-use std::path::Path;
+use std::path::{Path, PathBuf};
+use std::sync::Arc;
 
 use dojo_metrics::metrics::gauge;
 pub use libmdbx;
@@ -39,7 +40,20 @@ pub enum DbEnvKind {
 
 /// Wrapper for `libmdbx-sys` environment.
 #[derive(Debug, Clone)]
-pub struct DbEnv(libmdbx::Environment);
+pub struct DbEnv {
+    inner: Arc<DbEnvInner>,
+}
+
+#[derive(Debug)]
+struct DbEnvInner {
+    /// The handle to the MDBX environment.
+    env: libmdbx::Environment,
+    /// The path where the database environemnt is stored at.
+    dir: PathBuf,
+    /// A flag inidicating whether the database is ephemeral or not. If `true`, the database will
+    /// be deleted when the environment is dropped.
+    ephemeral: bool,
+}
 
 impl DbEnv {
     /// Opens the database at the specified path with the given `EnvKind`.
@@ -73,12 +87,48 @@ impl DbEnv {
             })
             .set_max_readers(DEFAULT_MAX_READERS);
 
-        Ok(DbEnv(builder.open(path.as_ref()).map_err(DatabaseError::OpenEnv)?).with_metrics())
+        let env = builder.open(path.as_ref()).map_err(DatabaseError::OpenEnv)?;
+        let dir = path.as_ref().to_path_buf();
+        let inner = DbEnvInner { env, dir, ephemeral: false };
+
+        Ok(Self { inner: Arc::new(inner) }.with_metrics())
+    }
+
+    /// Opens an ephemeral database. Temporary database environment whose underlying directory will
+    /// be deleted when the returned [`DbEnv`] is dropped.
+    pub fn open_ephemeral() -> Result<Self, DatabaseError> {
+        let dir =
+            tempfile::Builder::new().keep(true).tempdir().expect("failed to create a temp dir");
+        let path = dir.path();
+
+        let mut builder = libmdbx::Environment::builder();
+        builder
+            .set_max_dbs(Tables::ALL.len())
+            .set_geometry(Geometry {
+                size: Some(0..(GIGABYTE * 10)),             // 10gb
+                growth_step: Some((GIGABYTE / 2) as isize), // 512mb
+                shrink_threshold: None,
+                page_size: Some(PageSize::Set(utils::default_page_size())),
+            })
+            .set_flags(EnvironmentFlags {
+                // we dont care about durability  here
+                mode: Mode::ReadWrite { sync_mode: SyncMode::UtterlyNoSync },
+                no_rdahead: true,
+                coalesce: true,
+                ..Default::default()
+            })
+            .set_max_readers(DEFAULT_MAX_READERS);
+
+        let env = builder.open(path).map_err(DatabaseError::OpenEnv)?;
+        let dir = path.to_path_buf();
+        let inner = DbEnvInner { env, dir, ephemeral: true };
+
+        Ok(Self { inner: Arc::new(inner) }.with_metrics())
     }
 
     /// Creates all the defined tables in [`Tables`], if necessary.
     pub fn create_tables(&self) -> Result<(), DatabaseError> {
-        let tx = self.0.begin_rw_txn().map_err(DatabaseError::CreateRWTx)?;
+        let tx = self.inner.env.begin_rw_txn().map_err(DatabaseError::CreateRWTx)?;
 
         for table in Tables::ALL {
             let flags = match table.table_type() {
@@ -92,6 +142,11 @@ impl DbEnv {
         tx.commit().map_err(DatabaseError::Commit)?;
 
         Ok(())
+    }
+
+    /// Returns the path to the database environment directory.
+    pub fn path(&self) -> &Path {
+        &self.inner.dir
     }
 
     fn with_metrics(self) -> Self {
@@ -109,11 +164,11 @@ impl Database for DbEnv {
     type Stats = stats::Stats;
 
     fn tx(&self) -> Result<Self::Tx, DatabaseError> {
-        Ok(Tx::new(self.0.begin_ro_txn().map_err(DatabaseError::CreateROTx)?))
+        Ok(Tx::new(self.inner.env.begin_ro_txn().map_err(DatabaseError::CreateROTx)?))
     }
 
     fn tx_mut(&self) -> Result<Self::TxMut, DatabaseError> {
-        Ok(Tx::new(self.0.begin_rw_txn().map_err(DatabaseError::CreateRWTx)?))
+        Ok(Tx::new(self.inner.env.begin_rw_txn().map_err(DatabaseError::CreateRWTx)?))
     }
 
     fn stats(&self) -> Result<Self::Stats, DatabaseError> {
@@ -126,8 +181,8 @@ impl Database for DbEnv {
                 table_stats.insert(table.name(), TableStat::new(stat));
             }
 
-            let info = self.0.info().map_err(DatabaseError::Stat)?;
-            let freelist = self.0.freelist().map_err(DatabaseError::Stat)?;
+            let info = self.inner.env.info().map_err(DatabaseError::Stat)?;
+            let freelist = self.inner.env.freelist().map_err(DatabaseError::Stat)?;
             Ok(Stats { table_stats, info, freelist })
         })?
     }
@@ -200,6 +255,21 @@ pub mod test_utils {
         let env = DbEnv::open(path, kind).expect(ERROR_DB_CREATION);
         env.create_tables().expect("Failed to create tables.");
         env
+    }
+}
+
+impl Drop for DbEnv {
+    fn drop(&mut self) {
+        // Try to get a mutable reference, this will return Some if there's only a single reference
+        // left.
+        if let Some(inner) = Arc::get_mut(&mut self.inner) {
+            // And if it is ephemeral, remove the directory.
+            if inner.ephemeral {
+                if let Err(e) = std::fs::remove_dir_all(&inner.dir) {
+                    eprintln!("Failed to remove temporary directory: {e}");
+                }
+            }
+        }
     }
 }
 
