f6b1423486f9a82c2a7dccbef3d629ed8e4febcb
Nasr <larko236@outlook.com>
2024-10-25 10:51:13 -0400

    fix: tests

--- crates/torii/core/src/model.rs
+++ crates/torii/core/src/model.rs
@@ -1075,16 +1075,15 @@ mod tests {
         .unwrap();
 
         let expected_query =
-            "SELECT entities.id, entities.keys, [Test-Position].external_player AS \
-             \"Test-Position.player\", [Test-Position$vec].external_x AS \"Test-Position$vec.x\", \
-             [Test-Position$vec].external_y AS \"Test-Position$vec.y\", \
-             [Test-PlayerConfig$favorite_item].external_Some AS \
-             \"Test-PlayerConfig$favorite_item.Some\", [Test-PlayerConfig].external_favorite_item \
-             AS \"Test-PlayerConfig.favorite_item\" FROM entities JOIN [Test-Position$vec] ON \
-             entities.id = [Test-Position$vec].entity_id  JOIN [Test-Position] ON entities.id = \
-             [Test-Position].entity_id  JOIN [Test-PlayerConfig$favorite_item] ON entities.id = \
-             [Test-PlayerConfig$favorite_item].entity_id  JOIN [Test-PlayerConfig] ON entities.id \
-             = [Test-PlayerConfig].entity_id ORDER BY entities.event_id DESC";
+            "SELECT entities.id, entities.keys, [Test-Position].external_player AS \"Test-Position.player\", \
+            [Test-Position$vec].external_x AS \"Test-Position$vec.x\", [Test-Position$vec].external_y AS \"Test-Position$vec.y\", \
+            [Test-PlayerConfig$favorite_item].external_Some AS \"Test-PlayerConfig$favorite_item.Some\", \
+            [Test-PlayerConfig].external_favorite_item AS \"Test-PlayerConfig.favorite_item\" \
+            FROM entities JOIN [Test-Position] ON entities.id = [Test-Position].entity_id  \
+            JOIN [Test-PlayerConfig] ON entities.id = [Test-PlayerConfig].entity_id  \
+            JOIN [Test-Position$vec] ON [Test-Position$vec].full_array_id = [Test-Position].full_array_id  \
+            LEFT JOIN [Test-PlayerConfig$favorite_item] ON [Test-PlayerConfig$favorite_item].full_array_id = [Test-PlayerConfig].full_array_id \
+            ORDER BY entities.event_id DESC";
         // todo: completely tests arrays
         assert_eq!(query.0, expected_query);
     }
