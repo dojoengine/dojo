7f7d486ea90323de2df9d63caec2fd3c807a7240
Valentin Dosimont <valentin@pupucecorp.com>
2024-10-25 11:25:38 +0200

    fix: bindgen nested types init generation

--- crates/dojo-world/src/contracts/abi/world.rs
+++ crates/dojo-world/src/contracts/abi/world.rs
@@ -1237,5 +1237,6 @@ abigen!(
 ]"#,
 type_aliases {
 dojo::world::config::Config::Event as DojoConfigEvent;
-},derives(Debug, serde::Serialize, serde::Deserialize, PartialEq, Eq, Clone)
+},derives(Debug, serde::Serialize, serde::Deserialize, PartialEq, Eq, Clone),
+contract_derives(Debug, serde::Serialize, serde::Deserialize, PartialEq, Eq, Clone)
 );
