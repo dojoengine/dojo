da038e26244dd95d1be8bf4f4a3d78b2e7ffe847
github-action-benchmark <github@users.noreply.github.com>
2024-11-08 05:13:56 +0000

    add Benchmark (cargo) benchmark result for ed1275626ad435c47062b47a4d47f836da378506

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1731040593077,
+  "lastUpdate": 1731042836036,
   "repoUrl": "https://github.com/dojoengine/dojo",
   "entries": {
     "Benchmark": [
@@ -31927,6 +31927,36 @@ window.BENCHMARK_DATA = {
             "name": "Invoke.ERC20.transfer/Blockifier.Cold",
             "value": 8323064,
             "range": "± 326908",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "dev@glihm.net",
+            "name": "glihm",
+            "username": "glihm"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ed1275626ad435c47062b47a4d47f836da378506",
+          "message": "feat(sozo): add sozo auth back (#2659)\n\n* feat: add back sozo auth + new sozo auth list\n\n* feat: add permission cloning",
+          "timestamp": "2024-11-07T22:57:47-06:00",
+          "tree_id": "0de745fede2103c95d020c0c33cdda48c3a1e605",
+          "url": "https://github.com/dojoengine/dojo/commit/ed1275626ad435c47062b47a4d47f836da378506"
+        },
+        "date": 1731042834665,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "build/Sozo.Cold",
+            "value": 10204835128,
+            "range": "± 0",
             "unit": "ns/iter"
           }
         ]
