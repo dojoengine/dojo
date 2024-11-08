bb879581659e3c6b60b232d2bcd2bcc823d5673b
Ammar Arif <evergreenkary@gmail.com>
2024-11-07 23:16:34 -0500

    refactor(katana): decrease default fixed gas prices values (#2658)

--- crates/katana/core/src/constants.rs
+++ crates/katana/core/src/constants.rs
@@ -3,12 +3,12 @@ use lazy_static::lazy_static;
 use starknet::macros::felt;
 
 // Default gas prices
-pub const DEFAULT_ETH_L1_GAS_PRICE: u128 = 100 * u128::pow(10, 9); // Given in units of Wei.
-pub const DEFAULT_STRK_L1_GAS_PRICE: u128 = 100 * u128::pow(10, 9); // Given in units of STRK.
+pub const DEFAULT_ETH_L1_GAS_PRICE: u128 = 20 * u128::pow(10, 9); // Given in units of Wei.
+pub const DEFAULT_STRK_L1_GAS_PRICE: u128 = 20 * u128::pow(10, 9); // Given in units of STRK.
 
 // Default data gas prices
 pub const DEFAULT_ETH_L1_DATA_GAS_PRICE: u128 = u128::pow(10, 6); // Given in units of Wei.
-pub const DEFAULT_STRK_L1_DATA_GAS_PRICE: u128 = u128::pow(10, 9); // Given in units of STRK.
+pub const DEFAULT_STRK_L1_DATA_GAS_PRICE: u128 = u128::pow(10, 6); // Given in units of STRK.
 
 lazy_static! {
 
