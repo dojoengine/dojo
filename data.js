0934e13779f33fbb3eaaf4f37aa57862a479b70d
github-action-benchmark <github@users.noreply.github.com>
2024-11-09 15:25:05 +0000

    add Benchmark (cargo) benchmark result for aa44e0c6ab7b4b76200e526e2afc800ba4ff38a7

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1731165880220,
+  "lastUpdate": 1731165905714,
   "repoUrl": "https://github.com/dojoengine/dojo",
   "entries": {
     "Benchmark": [
@@ -32425,6 +32425,54 @@ window.BENCHMARK_DATA = {
             "name": "Invoke.ERC20.transfer/Blockifier.Cold",
             "value": 8326165,
             "range": "± 62698",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "dev@glihm.net",
+            "name": "glihm",
+            "username": "glihm"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "aa44e0c6ab7b4b76200e526e2afc800ba4ff38a7",
+          "message": "fix: ensure wide block range supported mainnet + refactor auth (#2665)\n\n* fix: ensure tests and other commands are using the config world block\n\n* refactor: refacto auth including external resources\n\n* fix: add world as valid keyword for grant/revoke",
+          "timestamp": "2024-11-09T09:05:09-06:00",
+          "tree_id": "c4d76508c8b261441d0e90183939372a9aae493a",
+          "url": "https://github.com/dojoengine/dojo/commit/aa44e0c6ab7b4b76200e526e2afc800ba4ff38a7"
+        },
+        "date": 1731165904493,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "decompress world contract",
+            "value": 16426723,
+            "range": "± 262623",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Concurrent.Simulate/Blockifier.1",
+            "value": 8466148,
+            "range": "± 129857",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Concurrent.Simulate/Blockifier.1000",
+            "value": 3572571995,
+            "range": "± 797504303",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Invoke.ERC20.transfer/Blockifier.Cold",
+            "value": 8348904,
+            "range": "± 99135",
             "unit": "ns/iter"
           }
         ]
