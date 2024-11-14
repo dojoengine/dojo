3f2be30b1b2832b5b99b2447818f1b4321bc25d3
github-action-benchmark <github@users.noreply.github.com>
2024-11-14 21:24:58 +0000

    add Benchmark (cargo) benchmark result for f6659db7cf081ac4071b2c2e9555fad0eed12210

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1731619281017,
+  "lastUpdate": 1731619498629,
   "repoUrl": "https://github.com/dojoengine/dojo",
   "entries": {
     "Benchmark": [
@@ -34765,6 +34765,54 @@ window.BENCHMARK_DATA = {
             "name": "build/Sozo.Cold",
             "value": 10879049746,
             "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "tarrencev@users.noreply.github.com",
+            "name": "Tarrence van As",
+            "username": "tarrencev"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f6659db7cf081ac4071b2c2e9555fad0eed12210",
+          "message": "Update devcontainer image: v1.0.1 (#2694)\n\nCo-authored-by: glihm <glihm@users.noreply.github.com>",
+          "timestamp": "2024-11-14T15:04:43-06:00",
+          "tree_id": "72cd6ae4ccb410151285539fa7a5182e203818cb",
+          "url": "https://github.com/dojoengine/dojo/commit/f6659db7cf081ac4071b2c2e9555fad0eed12210"
+        },
+        "date": 1731619496463,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "decompress world contract",
+            "value": 15273123,
+            "range": "± 197089",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Concurrent.Simulate/Blockifier.1",
+            "value": 9740386,
+            "range": "± 446753",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Concurrent.Simulate/Blockifier.1000",
+            "value": 3513211506,
+            "range": "± 789394513",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Invoke.ERC20.transfer/Blockifier.Cold",
+            "value": 8578014,
+            "range": "± 94749",
             "unit": "ns/iter"
           }
         ]
