ea63e39c8d92928f39b452a8a3af2082685db5f6
glihm <dev@glihm.net>
2024-11-20 20:01:27 -0600

    fix: merge external url logic for subscription and endpoint

--- crates/torii/graphql/src/server.rs
+++ crates/torii/graphql/src/server.rs
@@ -48,35 +48,25 @@ fn graphql_filter(
         },
     );
 
-    let subscription_endpoint = if let Some(external_url) = external_url.clone() {
-        let mut websocket_url = external_url.clone();
-        websocket_url.set_path("graphql/ws");
-
-        let websocket_scheme = match websocket_url.scheme() {
-            "http" => "ws",
-            "https" => "wss",
-            _ => panic!("Invalid URL scheme"), // URL validated on input so this never hits
-        };
-
-        let _ = websocket_url.set_scheme(websocket_scheme);
-        websocket_url.to_string()
-    } else {
-        "graphql/ws".to_string()
-    };
-
-    let graphql_endpoint = if let Some(external_url) = external_url {
+    let (graphql_endpoint, subscription_endpoint) = if let Some(external_url) = external_url {
         let mut graphql_url = external_url.clone();
-        graphql_url.set_path(&format!("{}/graphql", graphql_url.path().trim_end_matches('/')));
-        graphql_url.path().to_string()
+        graphql_url.set_path("graphql");
+
+        let mut websocket_url = external_url;
+        websocket_url.set_path("graphql/ws");
+        let _ =
+            websocket_url.set_scheme(if websocket_url.scheme() == "https" { "wss" } else { "ws" });
+
+        (graphql_url.path().to_string(), websocket_url.to_string())
     } else {
-        "graphql".to_string()
+        ("graphql".to_string(), "graphql/ws".to_string())
     };
 
     let playground_filter = warp::path("graphql").map(move || {
         warp::reply::html(
             GraphiQLSource::build()
                 .endpoint(&graphql_endpoint)
-                .subscription_endpoint(subscription_endpoint.as_str())
+                .subscription_endpoint(&subscription_endpoint)
                 .finish(),
         )
     });
