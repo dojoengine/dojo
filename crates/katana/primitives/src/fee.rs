c5c6760ce2062ddd3c6ebd1d296b1c79fb7c19b7
Ammar Arif <evergreenkary@gmail.com>
2024-12-02 14:37:13 -0500

    fix

--- crates/katana/primitives/src/fee.rs
+++ crates/katana/primitives/src/fee.rs
@@ -12,12 +12,14 @@ pub struct ResourceBounds {
     pub max_price_per_unit: u128,
 }
 
+// Renamed to match the feeder gateway API
 #[derive(Debug, Clone, PartialEq, Eq)]
 #[cfg_attr(feature = "arbitrary", derive(::arbitrary::Arbitrary))]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-#[cfg_attr(feature = "serde", serde(rename_all = "SCREAMING_SNAKE_CASE"))]
 pub struct ResourceBoundsMapping {
+    #[serde(alias = "L1_GAS")]
     pub l1_gas: ResourceBounds,
+    #[serde(alias = "L2_GAS")]
     pub l2_gas: ResourceBounds,
 }
 
