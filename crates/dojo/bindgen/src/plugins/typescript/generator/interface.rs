575040c701404579cc581d7010b6eecdbb5d306d
Brother MartianGreed <valentin@pupucecorp.com>
2024-12-06 01:03:17 +0100

    feat: Improve bindgen types (#2773)
    * bump: cainome-0.4.8
    
    * feat: add CairoOption and CairoCustomEnum type handling
    
    * feat: merge schema in models.gen.ts
    
    * feat: add Input type to omit fieldOrder field
    
    * feat: recursive types in model definition

--- crates/dojo/bindgen/src/plugins/typescript/generator/interface.rs
+++ crates/dojo/bindgen/src/plugins/typescript/generator/interface.rs
@@ -1,22 +1,53 @@
-use cainome::parser::tokens::{Composite, CompositeType};
+use cainome::parser::tokens::{Composite, CompositeType, Token};
 
-use super::JsType;
+use super::constants::{
+    BIGNUMNERISH_IMPORT, CAIRO_OPTION_IMPORT, REMOVE_FIELD_ORDER_TYPE_DEF, SN_IMPORT_SEARCH,
+};
+use super::{token_is_option, JsType};
 use crate::error::BindgenResult;
+use crate::plugins::typescript::generator::constants::CAIRO_OPTION_TOKEN;
 use crate::plugins::{BindgenModelGenerator, Buffer};
 
-const BIGNUMNERISH_IMPORT: &str = "import type { BigNumberish } from 'starknet';";
-
 pub(crate) struct TsInterfaceGenerator;
+impl TsInterfaceGenerator {
+    fn check_import(&self, token: &Composite, buffer: &mut Buffer) {
+        // only search for end part of the import, as we append the other imports afterward
+        if !buffer.has("BigNumberish } from 'starknet';") {
+            buffer.push(BIGNUMNERISH_IMPORT.to_owned());
+        }
+
+        // type is Option, need to import CairoOption
+        if token_is_option(token) {
+            // we directly add import if 'starknet' import is not present
+            if !buffer.has(SN_IMPORT_SEARCH) {
+                buffer.push(CAIRO_OPTION_IMPORT.to_owned());
+            } else if !buffer.has(CAIRO_OPTION_TOKEN) {
+                // If 'starknet' import is present, we add CairoOption to the imported types
+                buffer.insert_after(format!(" {CAIRO_OPTION_TOKEN}"), SN_IMPORT_SEARCH, "{", 1);
+            }
+        }
+    }
+
+    fn add_input_type(&self, buffer: &mut Buffer) {
+        if !buffer.has(REMOVE_FIELD_ORDER_TYPE_DEF) {
+            buffer.push(REMOVE_FIELD_ORDER_TYPE_DEF.to_owned());
+        }
+    }
+}
 
 impl BindgenModelGenerator for TsInterfaceGenerator {
     fn generate(&self, token: &Composite, buffer: &mut Buffer) -> BindgenResult<String> {
         if token.r#type != CompositeType::Struct || token.inners.is_empty() {
             return Ok(String::new());
         }
-
-        if !buffer.has(BIGNUMNERISH_IMPORT) {
-            buffer.push(BIGNUMNERISH_IMPORT.to_owned());
+        if buffer
+            .has(format!("// Type definition for `{path}` struct", path = token.type_path).as_str())
+        {
+            return Ok(String::new());
         }
+
+        self.check_import(token, buffer);
+        self.add_input_type(buffer);
 
         Ok(format!(
             "// Type definition for `{path}` struct
@@ -24,13 +55,22 @@ export interface {name} {{
 \tfieldOrder: string[];
 {fields}
 }}
+export type Input{name} = RemoveFieldOrder<{name}>;
 ",
             path = token.type_path,
             name = token.type_name(),
             fields = token
                 .inners
                 .iter()
-                .map(|inner| { format!("\t{}: {};", inner.name, JsType::from(&inner.token)) })
+                .map(|inner| {
+                    if let Token::Composite(composite) = &inner.token {
+                        if token_is_option(composite) {
+                            self.check_import(composite, buffer);
+                        }
+                    }
+
+                    format!("\t{}: {};", inner.name, JsType::from(&inner.token))
+                })
                 .collect::<Vec<String>>()
                 .join("\n")
         ))
@@ -89,8 +129,24 @@ mod tests {
             result,
             "// Type definition for `core::test::TestStruct` struct\nexport interface TestStruct \
              {\n\tfieldOrder: string[];\n\tfield1: BigNumberish;\n\tfield2: \
-             BigNumberish;\n\tfield3: BigNumberish;\n}\n"
+             BigNumberish;\n\tfield3: BigNumberish;\n}\nexport type InputTestStruct = \
+             RemoveFieldOrder<TestStruct>;\n"
         );
+    }
+
+    #[test]
+    fn test_check_import() {
+        let mut buff = Buffer::new();
+        let writer = TsInterfaceGenerator;
+        let token = create_test_struct_token();
+        writer.check_import(&token, &mut buff);
+        assert_eq!(1, buff.len());
+        let option = create_option_token();
+        writer.check_import(&option, &mut buff);
+        assert_eq!(1, buff.len());
+        let custom_enum = create_custom_enum_token();
+        writer.check_import(&custom_enum, &mut buff);
+        assert_eq!(1, buff.len());
     }
 
     fn create_test_struct_token() -> Composite {
@@ -118,6 +174,59 @@ mod tests {
             ],
             generic_args: vec![],
             r#type: CompositeType::Struct,
+            is_event: false,
+            alias: None,
+        }
+    }
+
+    fn create_option_token() -> Composite {
+        Composite {
+            type_path: "core::option::Option<core::felt252>".to_owned(),
+            inners: vec![CompositeInner {
+                index: 0,
+                name: "value".to_owned(),
+                kind: CompositeInnerKind::Key,
+                token: Token::CoreBasic(CoreBasic { type_path: "core::felt252".to_owned() }),
+            }],
+            generic_args: vec![],
+            r#type: CompositeType::Struct,
+            is_event: false,
+            alias: None,
+        }
+    }
+    fn create_custom_enum_token() -> Composite {
+        Composite {
+            type_path: "core::test::CustomEnum".to_owned(),
+            inners: vec![
+                CompositeInner {
+                    index: 0,
+                    name: "Variant1".to_owned(),
+                    kind: CompositeInnerKind::NotUsed,
+                    token: Token::CoreBasic(CoreBasic { type_path: "core::felt252".to_owned() }),
+                },
+                CompositeInner {
+                    index: 1,
+                    name: "Variant2".to_owned(),
+                    kind: CompositeInnerKind::NotUsed,
+                    token: Token::Composite(Composite {
+                        type_path: "core::test::NestedStruct".to_owned(),
+                        inners: vec![CompositeInner {
+                            index: 0,
+                            name: "nested_field".to_owned(),
+                            kind: CompositeInnerKind::Key,
+                            token: Token::CoreBasic(CoreBasic {
+                                type_path: "core::felt252".to_owned(),
+                            }),
+                        }],
+                        generic_args: vec![],
+                        r#type: CompositeType::Struct,
+                        is_event: false,
+                        alias: None,
+                    }),
+                },
+            ],
+            generic_args: vec![],
+            r#type: CompositeType::Enum,
             is_event: false,
             alias: None,
         }
