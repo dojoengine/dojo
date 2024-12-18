d847df94fcf82e2a7a34d3bc0a3a79746cd11ce5
Ammar Arif <evergreenkary@gmail.com>
2024-12-18 10:21:11 -0500

    wip

--- bin/katana/src/cli/init/mod.rs
+++ bin/katana/src/cli/init/mod.rs
@@ -1,57 +1,176 @@
-use anyhow::{anyhow, Result};
+use std::fs;
+use std::path::PathBuf;
+use std::rc::Rc;
+use std::str::FromStr;
+use std::sync::Arc;
+
+use anyhow::{anyhow, Context, Result};
 use clap::Args;
 use dojo_utils::TransactionWaiter;
+use inquire::parser::CustomTypeParser;
+use inquire::validator::{CustomTypeValidator, StringValidator, Validation};
+use inquire::{CustomType, CustomUserError, Text};
 use katana_cairo::lang::starknet_classes::casm_contract_class::CasmContractClass;
 use katana_cairo::lang::starknet_classes::contract_class::ContractClass;
-use katana_primitives::{felt, ContractAddress, Felt};
+use katana_primitives::{ContractAddress, Felt};
+use serde::{Deserialize, Serialize};
 use starknet::accounts::{Account, ConnectedAccount, ExecutionEncoding, SingleOwnerAccount};
 use starknet::contract::ContractFactory;
 use starknet::core::types::contract::{CompiledClass, SierraClass};
-use starknet::core::types::FlattenedSierraClass;
+use starknet::core::types::{BlockId, BlockTag, FlattenedSierraClass};
+use starknet::core::utils::{cairo_short_string_to_felt, parse_cairo_short_string};
 use starknet::providers::jsonrpc::HttpTransport;
-use starknet::providers::{JsonRpcClient, Url};
+use starknet::providers::{JsonRpcClient, Provider, Url};
 use starknet::signers::{LocalWallet, SigningKey};
 
-struct InitOutcome {
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "kebab-case")]
+pub struct L1 {
+    // The id of the settlement chain.
+    pub id: String,
+
     // - The token that will be used to pay for tx fee in the appchain.
     // - For now, this must be the native token that is used to pay for tx fee in the settlement chain.
-    fee_token: ContractAddress,
+    pub fee_token: ContractAddress,
 
-    // - The bridge contract for bridging the fee token to the appchain.
+    // - The bridge contract for bridging the fee token from L1 to the appchain
     // - This will be part of the initialization process.
-    bridge_contract: ContractAddress,
+    pub bridge_contract: ContractAddress,
 
-    settlement_contract: ContractAddress,
+    // - The core appchain contract used to settlement
+    // - This is deployed on the L1
+    pub settlement_contract: ContractAddress,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "kebab-case")]
+pub struct InitOutcome {
+    // the initialized chain id
+    pub id: String,
+
+    // the fee token contract
+    //
+    // this corresponds to the l1 token contract
+    pub fee_token: ContractAddress,
+
+    pub l1: L1,
 }
 
 #[derive(Args)]
 pub struct InitArgs {
-    // wallet
+    // #[arg(long = "account", value_name = "ADDRESS")]
+    // pub sender_address: ContractAddress,
+
+    // pub private_key: Felt,
+
+    // #[arg(long = "l1.provider", value_name = "URL")]
+    // pub l1_provider_url: Url,
+
+    // /// The id of the chain to be initialized.
+    // #[arg(long = "id", value_name = "ID")]
+    // pub chain_id: String,
+
+    // pub l1_fee_token: ContractAddress,
+
+    // /// If not specified, will be deployed on-demand.
+    // pub settlement_contract: Option<ContractAddress>,
+    #[arg(value_name = "PATH")]
+    pub output_path: Option<PathBuf>,
 }
 
 impl InitArgs {
     pub(crate) fn execute(self) -> Result<()> {
-        let address = felt!("0x123");
-        let pk = SigningKey::from_secret_scalar(felt!("0xdeadbeef"));
+        tokio::runtime::Builder::new_multi_thread().enable_all().build()?.block_on(async move {
+            // let account = self.account();
+            // let l1_chain_id = account.provider().chain_id().await?;
 
-        let provider = JsonRpcClient::new(HttpTransport::new(Url::parse("http://localhost:5050")?));
-        let account = SingleOwnerAccount::new(
-            provider,
-            LocalWallet::from_signing_key(pk),
-            address,
-            Felt::ONE,
-            ExecutionEncoding::New,
-        );
+            // let core_contract = init_core_contract(&account).await?;
 
-        let _contract_address = deploy_core_contracts(&account);
+            self.prompt()?;
 
-        // TODO: deploy bridge contract
+            todo!();
+
+            // let output_path =
+            //     if let Some(path) = self.output_path { path } else { config_path(&self.chain_id)? };
+
+            // TODO:
+            // - deploy bridge contract
+            // - generate the genesis
+
+            // let l1 = L1 {
+            //     id: parse_cairo_short_string(&l1_chain_id)?,
+            //     settlement_contract: ContractAddress::default(),
+            //     bridge_contract: ContractAddress::default(),
+            //     fee_token: ContractAddress::default(),
+            // };
+
+            // let output =
+            //     InitOutcome { l1, id: self.chain_id, fee_token: ContractAddress::default() };
+
+            // let content = toml::to_string_pretty(&output)?;
+            // std::fs::write(dbg!(output_path), content)?;
+
+            Result::<(), anyhow::Error>::Ok(())
+        })?;
 
         Ok(())
     }
+
+    fn prompt(&self) -> Result<()> {
+        let chain_id = Text::new("Chain id").prompt()?;
+
+        let url = CustomType::<Url>::new("L1 RPC URL")
+            .with_error_message("Please enter a valid URL")
+            .prompt()?;
+
+        let l1_provider = JsonRpcClient::new(HttpTransport::new(url));
+        let l1_provider = Arc::new(l1_provider);
+
+        let fee_token = CustomType::<ContractAddress>::new("Fee token")
+            .with_parser(fee_token_parser(l1_provider.clone()))
+            .with_error_message("Please enter a valid fee token")
+            // .with_validator(fee_token_parser(l1_provider.clone()))
+            .prompt()?;
+
+        // If skipped, we deploy on demand.
+        let settlement_contract = Text::new("Settlement contract").prompt_skippable()?;
+
+        Ok(())
+    }
+
+    // fn account(&self) -> SingleOwnerAccount<JsonRpcClient<HttpTransport>, LocalWallet> {
+    //     let provider = JsonRpcClient::new(HttpTransport::new(self.l1_provider_url.clone()));
+    //     let private_key = SigningKey::from_secret_scalar(self.private_key);
+
+    //     SingleOwnerAccount::new(
+    //         provider,
+    //         LocalWallet::from_signing_key(private_key),
+    //         self.sender_address.into(),
+    //         Felt::ONE,
+    //         ExecutionEncoding::New,
+    //     )
+    // }
 }
 
-async fn deploy_core_contracts(
+#[derive(Debug, thiserror::Error)]
+#[error("Fee token doesn't exist")]
+pub struct FeeTokenNotExist;
+
+// pub type CustomTypeParser<'a, T> = &'a dyn Fn(&str) -> Result<T, ()>;
+
+fn fee_token_parser(provider: impl Provider + Clone) -> impl CustomTypeParser<'a, T> {
+    move |input: &str| {
+        let block_id = BlockId::Tag(BlockTag::Pending);
+        let result = futures::executor::block_on(provider.get_class_hash_at(block_id, input));
+
+        match result {
+            Ok(..) => Ok(Validation::Valid),
+            Err(..) => Err(Box::new(FeeTokenNotExist) as CustomUserError),
+        }
+    }
+}
+
+async fn init_core_contract(
     account: &SingleOwnerAccount<JsonRpcClient<HttpTransport>, LocalWallet>,
 ) -> Result<ContractAddress> {
     let class =
@@ -79,15 +198,15 @@ async fn deploy_core_contracts(
     let res = request.send().await?;
     let _ = TransactionWaiter::new(res.transaction_hash, account.provider()).await?;
 
+    // TODO: initialize the core contract with the right program info
+
     Ok(request.deployed_address().into())
 }
 
-pub fn prepare_contract_declaration_params(artifact: &str) -> Result<(FlattenedSierraClass, Felt)> {
-    let flattened_class = get_flattened_class(artifact)
-        .map_err(|e| anyhow!("error flattening the contract class: {e}"))?;
-    let compiled_class_hash = get_compiled_class_hash(artifact)
-        .map_err(|e| anyhow!("error computing compiled class hash: {e}"))?;
-    Ok((flattened_class, compiled_class_hash))
+fn prepare_contract_declaration_params(artifact: &str) -> Result<(FlattenedSierraClass, Felt)> {
+    let class = get_flattened_class(artifact)?;
+    let compiled_class_hash = get_compiled_class_hash(artifact)?;
+    Ok((class, compiled_class_hash))
 }
 
 fn get_flattened_class(artifact: &str) -> Result<FlattenedSierraClass> {
@@ -102,4 +221,22 @@ fn get_compiled_class_hash(artifact: &str) -> Result<Felt> {
     let res = serde_json::to_string(&casm_contract)?;
     let compiled_class: CompiledClass = serde_json::from_str(&res)?;
     Ok(compiled_class.class_hash()?)
+}
+
+// > CONFIG_DIR/$chain_id/config.toml
+fn config_path(id: &str) -> Result<PathBuf> {
+    Ok(config_dir(id)?.join("config").with_extension("toml"))
+}
+
+fn config_dir(id: &str) -> Result<PathBuf> {
+    const KATANA_DIR: &str = "katana";
+
+    let _ = cairo_short_string_to_felt(id).context("Invalid id");
+    let path = dirs::config_local_dir().context("unsupported OS")?.join(KATANA_DIR).join(id);
+
+    if !path.exists() {
+        fs::create_dir_all(&path).expect("failed to create config directory");
+    }
+
+    Ok(path)
 }
