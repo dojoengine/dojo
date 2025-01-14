eaae960488d58ba28e58a8d1d9feb09bf7432852
github-action-benchmark <github@users.noreply.github.com>
2025-01-14 16:26:55 +0000

    add Benchmark (cargo) benchmark result for 1ca1257707a100b75def79d50921fc5302be38f9

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1736870315993,
+  "lastUpdate": 1736872015288,
   "repoUrl": "https://github.com/dojoengine/dojo",
   "entries": {
     "Benchmark": [
@@ -44767,6 +44767,36 @@ window.BENCHMARK_DATA = {
             "name": "Invoke.ERC20.transfer/Blockifier.Cold",
             "value": 8126683,
             "range": "± 68578",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "evergreenkary@gmail.com",
+            "name": "Ammar Arif",
+            "username": "kariy"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1ca1257707a100b75def79d50921fc5302be38f9",
+          "message": "fix(katana): separate deprecated declared class (#2903)\n\nCurrently, we're including both legacy and non-legacy into the same map in the `StateUpdates` struct, while instead the legacy should be put into a different map ie `deprecated_declared_classes`.",
+          "timestamp": "2025-01-15T00:10:08+08:00",
+          "tree_id": "b23d5919402d6a197d1d8d183c0189601ebc170e",
+          "url": "https://github.com/dojoengine/dojo/commit/1ca1257707a100b75def79d50921fc5302be38f9"
+        },
+        "date": 1736872013565,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "build/Sozo.Cold",
+            "value": 10907672423,
+            "range": "± 0",
             "unit": "ns/iter"
           }
         ]
