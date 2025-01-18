1bd8091ac40b7e6e5187da155d46917a6f87ce36
github-action-benchmark <github@users.noreply.github.com>
2025-01-18 04:46:18 +0000

    add Benchmark (cargo) benchmark result for 74ccf1331a329393b6a79dcdb36b72eab11f41be

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1737167682022,
+  "lastUpdate": 1737175578368,
   "repoUrl": "https://github.com/dojoengine/dojo",
   "entries": {
     "Benchmark": [
@@ -46327,6 +46327,36 @@ window.BENCHMARK_DATA = {
             "name": "Invoke.ERC20.transfer/Blockifier.Cold",
             "value": 7724641,
             "range": "± 84562",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "dev@glihm.net",
+            "name": "glihm",
+            "username": "glihm"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "74ccf1331a329393b6a79dcdb36b72eab11f41be",
+          "message": "fix(sozo): return error if inspect doesn't find resource (#2920)",
+          "timestamp": "2025-01-17T22:30:06-06:00",
+          "tree_id": "7c42bb69646a6f58b50e34297358e6a30fb03944",
+          "url": "https://github.com/dojoengine/dojo/commit/74ccf1331a329393b6a79dcdb36b72eab11f41be"
+        },
+        "date": 1737175576344,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "build/Sozo.Cold",
+            "value": 11463041395,
+            "range": "± 0",
             "unit": "ns/iter"
           }
         ]
