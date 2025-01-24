ad1665af836cb31fd04021b42f01c9dd0aa407a1
Ammar Arif <evergreenkary@gmail.com>
2025-01-24 14:10:58 -0500

    chore: rm `saya`

--- crates/saya/provider/src/rpc/state_diff.rs
+++
@@ -1,9 +0,0 @@
-use std::collections::HashSet;
-
-use starknet::core::types::{
-    ContractStorageDiffItem, DeclaredClassItem, DeployedContractItem, FieldElement, NonceUpdate,
-    StateDiff,
-};
-
-// 2 ^ 128
-const CLASS_INFO_FLAG_TRUE: &str = "0x100000000000000000000000000000000";
