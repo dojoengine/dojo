2050c4ee4c0b08ea61a48458e3ee850b8c5c7228
Ammar Arif <evergreenkary@gmail.com>
2025-01-24 16:30:47 -0500

    feat(katana): start block timer only if there txs (#2950)
    Only start the block interval timer when we have executed txs. This means we no longer produce empty blocks after every fixed interval.

--- crates/katana/core/src/service/block_producer.rs
+++ crates/katana/core/src/service/block_producer.rs
@@ -28,6 +28,10 @@ use tracing::{error, info, trace, warn};
 
 use crate::backend::Backend;
 
+#[cfg(test)]
+#[path = "block_producer_tests.rs"]
+mod tests;
+
 pub(crate) const LOG_TARGET: &str = "miner";
 
 #[derive(Debug, thiserror::Error)]
@@ -181,8 +185,12 @@ impl PendingExecutor {
 
 #[allow(missing_debug_implementations)]
 pub struct IntervalBlockProducer<EF: ExecutorFactory> {
-    /// The interval at which new blocks are mined.
-    interval: Option<Interval>,
+    /// How long until the block is closed.
+    ///
+    /// In this mining mode, a new block is only opened upon receiving a new transaction. The block
+    /// is closed after the interval is over. The interval is reset after every block.
+    block_time: Option<u64>,
+
     backend: Arc<Backend<EF>>,
     /// Single active future that mines a new block
     ongoing_mining: Option<BlockProductionFuture>,
@@ -194,23 +202,21 @@ pub struct IntervalBlockProducer<EF: ExecutorFactory> {
     /// Listeners notified when a new executed tx is added.
     tx_execution_listeners: RwLock<Vec<Sender<Vec<TxWithOutcome>>>>,
 
+    // Usage with `validator`
     permit: Arc<Mutex<()>>,
-
     /// validator used in the tx pool
     // the validator needs to always be built against the state of the block producer, so
     // im putting here for now until we find a better way to handle this.
     validator: TxValidator,
+
+    /// The timer should only be `Some` if:
+    /// - `block_time` is `Some`,
+    /// - and, at least one transaction has been executed and thus a new block is opened.
+    timer: Option<Interval>,
 }
 
 impl<EF: ExecutorFactory> IntervalBlockProducer<EF> {
-    pub fn new(backend: Arc<Backend<EF>>, interval: Option<u64>) -> Self {
-        let interval = interval.map(|time| {
-            let duration = Duration::from_millis(time);
-            let mut interval = interval_at(Instant::now() + duration, duration);
-            interval.set_missed_tick_behavior(tokio::time::MissedTickBehavior::Delay);
-            interval
-        });
-
+    pub fn new(backend: Arc<Backend<EF>>, block_time: Option<u64>) -> Self {
         let provider = backend.blockchain.provider();
 
         let latest_num = provider.latest_number().unwrap();
@@ -233,7 +239,8 @@ impl<EF: ExecutorFactory> IntervalBlockProducer<EF> {
             validator,
             permit,
             backend,
-            interval,
+            block_time,
+            timer: None,
             ongoing_mining: None,
             ongoing_execution: None,
             queued: VecDeque::default(),
@@ -384,9 +391,9 @@ impl<EF: ExecutorFactory> Stream for IntervalBlockProducer<EF> {
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
         let pin = self.get_mut();
 
-        if let Some(interval) = &mut pin.interval {
-            // mine block if the interval is over
-            if interval.poll_tick(cx).is_ready() && pin.ongoing_mining.is_none() {
+        if let Some(mut timer) = pin.timer.take() {
+            // Mine block if the interval is over
+            if timer.poll_tick(cx).is_ready() && pin.ongoing_mining.is_none() {
                 pin.ongoing_mining = Some(Box::pin({
                     let executor = pin.executor.clone();
                     let backend = pin.backend.clone();
@@ -394,6 +401,9 @@ impl<EF: ExecutorFactory> Stream for IntervalBlockProducer<EF> {
 
                     pin.blocking_task_spawner.spawn(|| Self::do_mine(permit, executor, backend))
                 }));
+            } else {
+                // Unable to close the block due to ongoing mining.
+                pin.timer = Some(timer);
             }
         }
 
@@ -412,6 +422,16 @@ impl<EF: ExecutorFactory> Stream for IntervalBlockProducer<EF> {
                     .spawn(|| Self::execute_transactions(executor, transactions));
 
                 pin.ongoing_execution = Some(Box::pin(fut));
+
+                if pin.timer.is_none() {
+                    // Start the interval timer if it's not already started
+                    pin.timer = pin.block_time.map(|time| {
+                        let duration = Duration::from_millis(time);
+                        let mut interval = interval_at(Instant::now() + duration, duration);
+                        interval.set_missed_tick_behavior(tokio::time::MissedTickBehavior::Delay);
+                        interval
+                    });
+                }
             }
 
             // poll the ongoing execution if any
