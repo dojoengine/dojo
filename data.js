bd9702cfeeef49d7e92a1d309f510ec292d44344
github-action-benchmark <github@users.noreply.github.com>
2025-01-29 05:01:58 +0000

    add Benchmark (cargo) benchmark result for bc887342e9f64cb342538eecef78732a4730b0d2

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1738116717878,
+  "lastUpdate": 1738126918102,
   "repoUrl": "https://github.com/dojoengine/dojo",
   "entries": {
     "Benchmark": [
@@ -48823,6 +48823,36 @@ window.BENCHMARK_DATA = {
             "name": "Invoke.ERC20.transfer/Blockifier.Cold",
             "value": 7760704,
             "range": "± 238202",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "59736843+Larkooo@users.noreply.github.com",
+            "name": "Larko",
+            "username": "Larkooo"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "bc887342e9f64cb342538eecef78732a4730b0d2",
+          "message": "fix(torii-sqlite): ensure deleting entity works as intended (#2961)\n\n* fix(torii-sqlite): deleting entity\r\n\r\n* fix: delete entity model\r\n\r\n* more strict tests\r\n\r\n* uningnore test\r\n\r\n* check moves and position tables\r\n\r\n* fix\r\n\r\n* better test check",
+          "timestamp": "2025-01-28T22:45:59-06:00",
+          "tree_id": "2847e14a47526402e1aa1e60f11b5980da05846a",
+          "url": "https://github.com/dojoengine/dojo/commit/bc887342e9f64cb342538eecef78732a4730b0d2"
+        },
+        "date": 1738126915961,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "build/Sozo.Cold",
+            "value": 11654619760,
+            "range": "± 0",
             "unit": "ns/iter"
           }
         ]
