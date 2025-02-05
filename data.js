08f07d6b777eb57ba7c5f9305072f11aa5e8a45e
github-action-benchmark <github@users.noreply.github.com>
2025-02-05 17:30:37 +0000

    add Benchmark (cargo) benchmark result for 870087b567d05bcac23b0f34c2d5b26d583fefbd

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1738769758473,
+  "lastUpdate": 1738776637287,
   "repoUrl": "https://github.com/dojoengine/dojo",
   "entries": {
     "Benchmark": [
@@ -50149,6 +50149,36 @@ window.BENCHMARK_DATA = {
             "name": "Invoke.ERC20.transfer/Blockifier.Cold",
             "value": 7845635,
             "range": "± 370302",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "mijailovicmarija@hotmail.com",
+            "name": "Marija Mijailović",
+            "username": "marijamijailovic"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "870087b567d05bcac23b0f34c2d5b26d583fefbd",
+          "message": "feat(sozo-walnut): remove sensitive data from dojo configuration file (#2978)\n\n* Added message to prevent user from exspose their private key during verification\r\n\r\n* Verification propmt message lint fix\r\n\r\n* Remove env section from dojo_*.toml before upload it",
+          "timestamp": "2025-02-05T17:14:30Z",
+          "tree_id": "298999bf5356dbf5df64639553623d6a87b0d7ce",
+          "url": "https://github.com/dojoengine/dojo/commit/870087b567d05bcac23b0f34c2d5b26d583fefbd"
+        },
+        "date": 1738776634882,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "build/Sozo.Cold",
+            "value": 11697556550,
+            "range": "± 0",
             "unit": "ns/iter"
           }
         ]
