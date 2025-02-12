3ccb4239aeaa30d2ec2ff79d175b773532535576
Nasr <larko236@outlook.com>
2025-02-12 22:05:08 +0800

    fix client build

--- crates/torii/client/src/client/mod.rs
+++ crates/torii/client/src/client/mod.rs
@@ -11,7 +11,7 @@ use starknet::providers::jsonrpc::HttpTransport;
 use starknet::providers::JsonRpcClient;
 use tokio::sync::RwLock as AsyncRwLock;
 use torii_grpc::client::{
-    EntityUpdateStreaming, EventUpdateStreaming, IndexerUpdateStreaming, TokenBalanceStreaming,
+    EntityUpdateStreaming, EventUpdateStreaming, IndexerUpdateStreaming, TokenBalanceStreaming, TokenUpdateStreaming,
 };
 use torii_grpc::proto::world::{
     RetrieveControllersResponse, RetrieveEntitiesResponse, RetrieveEventsResponse,
