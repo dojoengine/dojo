d15de99eb3e62b2c299919be3c1def359827df68
Nasr <larko236@outlook.com>
2025-02-12 22:02:31 +0800

    remove constrain

--- crates/torii/migrations/20250211143225_token_id.sql
+++ crates/torii/migrations/20250211143225_token_id.sql
@@ -4,7 +4,3 @@ ADD COLUMN token_id TEXT NULL;
 
 -- Add an index on the token_id column
 CREATE INDEX idx_token_id ON tokens (token_id);
-
--- Add a unique constraint on the contract_address and token_id columns
-ALTER TABLE tokens
-ADD CONSTRAINT unique_contract_address_token_id UNIQUE (contract_address, token_id);
