58816f2151d81e39a4ccc6ba1b528747b6457f1a
Ammar Arif <evergreenkary@gmail.com>
2025-02-15 01:52:34 -0500

    wip

--- crates/katana/rpc/rpc-types/src/class.rs
+++ crates/katana/rpc/rpc-types/src/class.rs
@@ -231,17 +231,12 @@ impl TryFrom<starknet::core::types::ContractClass> for RpcContractClass {
 
 #[cfg(test)]
 mod tests {
-    use jsonrpsee::client_transport::ws::Uri;
-    use katana_cairo::starknet_api::felt;
-    use katana_primitives::block::BlockIdOrTag;
     use katana_primitives::class::{ContractClass, LegacyContractClass, SierraContractClass};
     use starknet::core::types::contract::legacy::LegacyContractClass as StarknetRsLegacyContractClass;
     use starknet::core::types::contract::SierraClass;
-    use starknet::providers::jsonrpc::HttpTransport;
-    use starknet::providers::{JsonRpcClient, Provider, Url};
 
     use super::RpcLegacyContractClass;
-    use crate::class::{RpcContractClass, RpcSierraContractClass};
+    use crate::class::RpcSierraContractClass;
 
     #[test]
     fn rt() {
@@ -340,28 +335,5 @@ mod tests {
         assert_eq!(expected_class.program.reference_manager, class.program.reference_manager);
         // The debug info is stripped when converting to RPC format.
         assert_eq!(serde_json::to_value::<Option<()>>(None).unwrap(), class.program.debug_info);
-    }
-
-    #[tokio::test]
-    async fn get_now() {
-        let class_hash =
-            felt!("0x0699053487675242dc0958e192c17fe4dd57d22238ad78e2e1807fa7919ffde0");
-        let provider = JsonRpcClient::new(HttpTransport::new(
-            Url::parse("https://api.cartridge.gg/x/starknet/mainnet").unwrap(),
-        ));
-
-        let rpc_class: RpcContractClass = provider
-            .get_class(BlockIdOrTag::Number(389009), class_hash)
-            .await
-            .expect("faield to fetch")
-            .try_into()
-            .expect("failed to convert");
-
-        let ContractClass::Legacy(class) = ContractClass::try_from(rpc_class).unwrap() else {
-            panic!("ivnalid class")
-        };
-
-        std::fs::write("path/to/file.json", serde_json::to_string_pretty(&class).unwrap())
-            .expect("failed to write");
     }
 }
