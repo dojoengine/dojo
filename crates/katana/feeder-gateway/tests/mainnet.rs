2748f89c83a9fa6025096ed4cb8f50b4feb1faa7
Ammar Arif <evergreenkary@gmail.com>
2025-02-16 00:58:37 -0500

    update test

--- crates/katana/feeder-gateway/tests/mainnet.rs
+++ crates/katana/feeder-gateway/tests/mainnet.rs
@@ -29,10 +29,19 @@ async fn get_block(
 }
 
 #[rstest]
-#[case::pre_v0_7_0(0, test_data("0.7.0/block/mainnet_genesis.json"))]
-#[case::v0_7_0(2240, test_data("0.7.0/block/mainnet_2240.json"))]
-#[case::v0_8_0(2500, test_data("0.8.0/block/mainnet_2500.json"))]
-#[case::v0_9_0(2800, test_data("0.8.0/block/mainnet_2800.json"))]
+#[case::v0_12_2(350000, test_data("0.12.2/state_update/mainnet_350000.json"))]
+#[tokio::test]
+async fn get_state_update(
+    gateway: SequencerGateway,
+    #[case] block_number: BlockNumber,
+    #[case] expected: StateUpdate,
+) {
+    let id = BlockIdOrTag::Number(block_number);
+    let state_update = gateway.get_state_update(id).await.unwrap();
+    similar_asserts::assert_eq!(state_update, expected);
+}
+
+#[rstest]
 #[case::v0_11_1(65000, test_data("0.11.1/block/mainnet_65000.json"))]
 #[tokio::test]
 async fn get_state_update_with_block(
@@ -42,23 +51,6 @@ async fn get_state_update_with_block(
 ) {
     let id = BlockIdOrTag::Number(block_number);
     let state_update = gateway.get_state_update_with_block(id).await.unwrap();
-    similar_asserts::assert_eq!(state_update, expected);
-}
-
-#[rstest]
-#[case::pre_v0_7_0(0, test_data("0.7.0/block/mainnet_genesis.json"))]
-#[case::v0_7_0(2240, test_data("0.7.0/block/mainnet_2240.json"))]
-#[case::v0_8_0(2500, test_data("0.8.0/block/mainnet_2500.json"))]
-#[case::v0_9_0(2800, test_data("0.8.0/block/mainnet_2800.json"))]
-#[case::v0_11_1(65000, test_data("0.11.1/block/mainnet_65000.json"))]
-#[tokio::test]
-async fn get_state_update(
-    gateway: SequencerGateway,
-    #[case] block_number: BlockNumber,
-    #[case] expected: StateUpdate,
-) {
-    let id = BlockIdOrTag::Number(block_number);
-    let state_update = gateway.get_state_update(id).await.unwrap();
     similar_asserts::assert_eq!(state_update, expected);
 }
 
