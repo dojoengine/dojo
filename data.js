a9f201d2331c7a925cdfa1cf13e0a5207c1773c2
github-action-benchmark <github@users.noreply.github.com>
2025-03-05 19:34:42 +0000

    add Benchmark (cargo) benchmark result for a2797e2996868f89f2c51d288005ee9b6885ac4d

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1741203228678,
+  "lastUpdate": 1741203282367,
   "repoUrl": "https://github.com/dojoengine/dojo",
   "entries": {
     "Benchmark": [
@@ -56575,6 +56575,54 @@ window.BENCHMARK_DATA = {
             "name": "build/Sozo.Cold",
             "value": 47498086130,
             "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "tomar2585@gmail.com",
+            "name": "Ayush Tomar",
+            "username": "ayushtom"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a2797e2996868f89f2c51d288005ee9b6885ac4d",
+          "message": "feat(katana) : add feature flag for controller (#3080)\n\n* feat: add feature flag for controller\n\n* chore: add required comments\n\n* chore: fix ci rust fmt check\n\n* Update crates/katana/explorer/src/lib.rs\n\n* Update crates/katana/explorer/src/lib.rs\n\n* Update crates/katana/explorer/src/lib.rs\n\n* Update crates/katana/explorer/src/lib.rs\n\n* Update crates/katana/explorer/src/lib.rs\n\n* Update crates/katana/explorer/src/lib.rs\n\n* Update crates/katana/explorer/src/lib.rs\n\n* Update crates/katana/explorer/src/lib.rs\n\n* Update crates/katana/explorer/src/lib.rs\n\n* chore: fix fmt check\n\n---------\n\nCo-authored-by: Ammar Arif <evergreenkary@gmail.com>",
+          "timestamp": "2025-03-06T03:16:49+08:00",
+          "tree_id": "83079c2eb7a2c40a32d3700e73fe8348e163b2a6",
+          "url": "https://github.com/dojoengine/dojo/commit/a2797e2996868f89f2c51d288005ee9b6885ac4d"
+        },
+        "date": 1741203278953,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "decompress world contract",
+            "value": 2415953,
+            "range": "± 50177",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Concurrent.Simulate/Blockifier.1",
+            "value": 395035,
+            "range": "± 8779",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Concurrent.Simulate/Blockifier.1000",
+            "value": 2649637607,
+            "range": "± 242212883",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Invoke.ERC20.transfer/Blockifier.Cold",
+            "value": 16921577,
+            "range": "± 201350",
             "unit": "ns/iter"
           }
         ]
