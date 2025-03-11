9a72bfe4ef84b41f79cd9e15666e5472d29d4767
Ammar Arif <evergreenkary@gmail.com>
2025-03-11 14:32:12 -0400

    build(katana): explorer ui build script

---
+++ crates/katana/explorer/build.rs
@@ -0,0 +1,53 @@
+use std::path::Path;
+use std::process::Command;
+
+fn main() {
+    println!("cargo:rerun-if-changed=ui/");
+
+    // Check if we're in a build script
+    if std::env::var("CARGO_MANIFEST_DIR").is_ok() {
+        // $CARGO_MANIFEST_DIR/ui/
+        let ui_dir = Path::new(&std::env::var("CARGO_MANIFEST_DIR").unwrap()).join("ui");
+
+        // Update git submodule
+        println!("UI directory is empty, updating git submodule...");
+        let status = Command::new("git")
+            .arg("submodule")
+            .arg("update")
+            .arg("--init")
+            .arg("--recursive")
+            .status()
+            .expect("Failed to update git submodule");
+
+        if !status.success() {
+            panic!("Failed to update git submodule");
+        }
+
+        // Install dependencies if node_modules doesn't exist
+        // $CARGO_MANIFEST_DIR/ui/node_modules
+        if !Path::new(&ui_dir).join("node_modules").exists() {
+            println!("Installing UI dependencies...");
+            let status = Command::new("bun")
+                .current_dir(&ui_dir)
+                .arg("install")
+                .status()
+                .expect("Failed to install UI dependencies");
+
+            if !status.success() {
+                panic!("Failed to install UI dependencies");
+            }
+        }
+
+        println!("Building UI...");
+        let status = Command::new("bun")
+            .current_dir(&ui_dir)
+            .arg("run")
+            .arg("build")
+            .status()
+            .expect("Failed to build UI");
+
+        if !status.success() {
+            panic!("Failed to build UI");
+        }
+    }
+}
