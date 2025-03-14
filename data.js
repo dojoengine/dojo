a0154e61a4148a669b7f4287b7c7c00e1808aee1
github-action-benchmark <github@users.noreply.github.com>
2025-03-14 16:09:19 +0000

    add Benchmark (cargo) benchmark result for af476bb190b9441d553800f48bb215e335ba3865

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1741968465101,
+  "lastUpdate": 1741968559102,
   "repoUrl": "https://github.com/dojoengine/dojo",
   "entries": {
     "Benchmark": [
@@ -57667,6 +57667,54 @@ window.BENCHMARK_DATA = {
             "name": "build/Sozo.Cold",
             "value": 43696036436,
             "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "dev@glihm.net",
+            "name": "glihm",
+            "username": "glihm"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "af476bb190b9441d553800f48bb215e335ba3865",
+          "message": "feat(katana): add support for sovereign mode for `init` command (#3101)\n\n* feat: add a proposal to support sovereign rollup Katana init\n\n* rename function\n\n* use fixed gas just for semantic\n\n* reject empty string on clap level\n\n* dont allow not specifying any args at all\n\n* fix: remove addresses TODO\n\n---------\n\nCo-authored-by: Ammar Arif <evergreenkary@gmail.com>",
+          "timestamp": "2025-03-14T15:51:12Z",
+          "tree_id": "b21dfd4eed0c15d12e4e264944a93d4c22e52ee7",
+          "url": "https://github.com/dojoengine/dojo/commit/af476bb190b9441d553800f48bb215e335ba3865"
+        },
+        "date": 1741968556012,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "decompress world contract",
+            "value": 2412632,
+            "range": "± 43019",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Concurrent.Simulate/Blockifier.1",
+            "value": 395201,
+            "range": "± 11665",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Concurrent.Simulate/Blockifier.1000",
+            "value": 2725644340,
+            "range": "± 246447860",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Invoke.ERC20.transfer/Blockifier.Cold",
+            "value": 18140020,
+            "range": "± 652264",
             "unit": "ns/iter"
           }
         ]
