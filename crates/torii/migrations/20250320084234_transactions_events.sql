8268f3cc1cf96f10cf84f5f9a8f895f48cb23523
Nasr <larko236@outlook.com>
2025-03-20 17:59:14 +0900

    idx

--- crates/torii/migrations/20250320084234_transactions_events.sql
+++ crates/torii/migrations/20250320084234_transactions_events.sql
@@ -1,5 +1,5 @@
--- Add a foreign key to transactions table from events table
-ALTER TABLE events ADD CONSTRAINT fk_events_transactions FOREIGN KEY (transaction_hash) REFERENCES transactions(id);
+-- Add an index to the events table on transaction_hash
+CREATE INDEX idx_events_transaction_hash ON events (transaction_hash);
 
 -- Remove the id column from the transactions table and set transaction_hash as primary key
 ALTER TABLE transactions DROP COLUMN id;
