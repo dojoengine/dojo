b924b3cd1b15b1ea8b4157b6ae75e09b644e918c
Nasr <larko236@outlook.com>
2025-03-20 18:03:24 +0900

    c

--- crates/torii/migrations/20250320084234_transactions_events.sql
+++
@@ -1,6 +0,0 @@
--- Add an index to the events table on transaction_hash
-CREATE INDEX idx_events_transaction_hash ON events (transaction_hash);
-
--- Remove the id column from the transactions table and set transaction_hash as primary key
-ALTER TABLE transactions DROP COLUMN id;
-ALTER TABLE transactions ADD PRIMARY KEY (transaction_hash);
