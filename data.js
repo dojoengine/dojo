45219611298f0a4dd99e049084d7bf1310c75739
github-action-benchmark <github@users.noreply.github.com>
2025-03-24 18:05:30 +0000

    add Benchmark (cargo) benchmark result for 2eea62453af52f2985a82781104a39a93ee26614

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1742839436403,
+  "lastUpdate": 1742839530522,
   "repoUrl": "https://github.com/dojoengine/dojo",
   "entries": {
     "Benchmark": [
@@ -59431,6 +59431,36 @@ window.BENCHMARK_DATA = {
             "name": "Invoke.ERC20.transfer/Blockifier.Cold",
             "value": 16918071,
             "range": "± 519884",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "59736843+Larkooo@users.noreply.github.com",
+            "name": "Larko",
+            "username": "Larkooo"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2eea62453af52f2985a82781104a39a93ee26614",
+          "message": "opt(torii-sqlite): index on tx timestamp & fix gql (#3126)\n\n* opt(torii-sqlite): index on tx timestamp & fix gql\n\n* handle gql int type fetch",
+          "timestamp": "2025-03-24T11:49:36-06:00",
+          "tree_id": "2728ebfb98ac107220016296274df42190f18af4",
+          "url": "https://github.com/dojoengine/dojo/commit/2eea62453af52f2985a82781104a39a93ee26614"
+        },
+        "date": 1742839527438,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "build/Sozo.Cold",
+            "value": 44346689577,
+            "range": "± 0",
             "unit": "ns/iter"
           }
         ]
