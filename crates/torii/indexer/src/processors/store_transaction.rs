551cfb52deb65fc3c6edc92687a19305757e25a7
Larko <59736843+Larkooo@users.noreply.github.com>
2025-04-01 02:38:03 +0900

    refactor(torii-indexer): index only pending txns with specific contract (#3135)
    * refactor(torii-indexer): index only pending txns with speciifce contracts
    
    * f
    
    * cleanup store transsaction code
    
    * fmt
    
    * fix: fix clippy
    
    ---------
    
    Co-authored-by: glihm <dev@glihm.net>

--- crates/torii/indexer/src/processors/store_transaction.rs
+++ crates/torii/indexer/src/processors/store_transaction.rs
@@ -1,5 +1,6 @@
 use std::collections::HashSet;
 
+use anyhow::Result;
 use async_trait::async_trait;
 use cainome::cairo_serde_derive::CairoSerde;
 use cainome_cairo_serde::CairoSerde;
@@ -43,8 +44,210 @@ pub enum Execute {
     Execute(ExecuteTransaction),
 }
 
+struct TransactionInfo {
+    transaction_hash: Felt,
+    sender_address: Felt,
+    calldata: Vec<Felt>,
+    max_fee: Felt,
+    signature: Vec<Felt>,
+    nonce: Felt,
+    transaction_type: &'static str,
+}
+
 #[derive(Default, Debug)]
 pub struct StoreTransactionProcessor;
+
+impl StoreTransactionProcessor {
+    fn extract_transaction_info(transaction: &Transaction) -> Option<TransactionInfo> {
+        match transaction {
+            Transaction::Invoke(InvokeTransaction::V3(tx)) => Some(TransactionInfo {
+                transaction_hash: tx.transaction_hash,
+                sender_address: tx.sender_address,
+                calldata: tx.calldata.clone(),
+                max_fee: Felt::ZERO,
+                signature: tx.signature.clone(),
+                nonce: tx.nonce,
+                transaction_type: "INVOKE",
+            }),
+            Transaction::Invoke(InvokeTransaction::V1(tx)) => Some(TransactionInfo {
+                transaction_hash: tx.transaction_hash,
+                sender_address: tx.sender_address,
+                calldata: tx.calldata.clone(),
+                max_fee: tx.max_fee,
+                signature: tx.signature.clone(),
+                nonce: tx.nonce,
+                transaction_type: "INVOKE",
+            }),
+            Transaction::L1Handler(tx) => Some(TransactionInfo {
+                transaction_hash: tx.transaction_hash,
+                sender_address: tx.contract_address,
+                calldata: tx.calldata.clone(),
+                max_fee: Felt::ZERO,
+                signature: vec![],
+                nonce: tx.nonce.into(),
+                transaction_type: "L1_HANDLER",
+            }),
+            _ => None,
+        }
+    }
+
+    async fn parse_execute_call<P: Provider + Send + Sync + std::fmt::Debug>(
+        contract_class_cache: &ContractClassCache<P>,
+        call: &ExecuteCall,
+        caller_address: Felt,
+        call_type: CallType,
+    ) -> Result<ParsedCall> {
+        let contract_class = contract_class_cache
+            .get(call.contract_address, BlockId::Tag(BlockTag::Pending))
+            .await?;
+
+        let entrypoint = get_entrypoint_name_from_class(&contract_class, call.selector)
+            .unwrap_or(format!("{:#x}", call.selector));
+
+        Ok(ParsedCall {
+            contract_address: call.contract_address,
+            entrypoint,
+            calldata: call.calldata.clone(),
+            call_type,
+            caller_address,
+        })
+    }
+
+    async fn parse_legacy_execute_call<P: Provider + Send + Sync + std::fmt::Debug>(
+        contract_class_cache: &ContractClassCache<P>,
+        call: &LegacyExecuteCall,
+        full_calldata: &[Felt],
+        caller_address: Felt,
+        call_type: CallType,
+    ) -> Result<ParsedCall> {
+        let contract_class = contract_class_cache
+            .get(call.contract_address, BlockId::Tag(BlockTag::Pending))
+            .await?;
+
+        let entrypoint = get_entrypoint_name_from_class(&contract_class, call.selector)
+            .unwrap_or(format!("{:#x}", call.selector));
+
+        Ok(ParsedCall {
+            contract_address: call.contract_address,
+            entrypoint,
+            calldata: full_calldata[call.data_offset..call.data_offset + call.data_length].to_vec(),
+            call_type,
+            caller_address,
+        })
+    }
+
+    async fn parse_outside_call<P: Provider + Send + Sync + std::fmt::Debug>(
+        contract_class_cache: &ContractClassCache<P>,
+        calldata: &[Felt],
+        base_offset: usize,
+        caller_address: Felt,
+    ) -> Result<ParsedCall> {
+        let to_offset = base_offset;
+        let selector_offset = to_offset + 1;
+        let calldata_offset = selector_offset + 2;
+        let calldata_len: usize = calldata[selector_offset + 1].try_into().unwrap();
+        let contract_address = calldata[to_offset];
+
+        let contract_class =
+            contract_class_cache.get(contract_address, BlockId::Tag(BlockTag::Pending)).await?;
+
+        let entrypoint = get_entrypoint_name_from_class(&contract_class, calldata[selector_offset])
+            .unwrap_or(format!("{:#x}", calldata[selector_offset]));
+
+        Ok(ParsedCall {
+            contract_address,
+            entrypoint,
+            calldata: calldata[calldata_offset..calldata_offset + calldata_len].to_vec(),
+            call_type: CallType::ExecuteFromOutside,
+            caller_address,
+        })
+    }
+
+    async fn process_outside_calls<P: Provider + Send + Sync + std::fmt::Debug>(
+        contract_class_cache: &ContractClassCache<P>,
+        call: &ParsedCall,
+    ) -> Result<Vec<ParsedCall>> {
+        let mut outside_calls = Vec::new();
+
+        match call.entrypoint.as_str() {
+            "execute_from_outside_v3" => {
+                let outside_calls_len: usize = call.calldata[5].try_into().unwrap();
+                for _ in 0..outside_calls_len {
+                    let outside_call = Self::parse_outside_call(
+                        contract_class_cache,
+                        &call.calldata,
+                        6,
+                        call.contract_address,
+                    )
+                    .await?;
+                    outside_calls.push(outside_call);
+                }
+            }
+            "execute_from_outside_v2" => {
+                let outside_calls_len: usize = call.calldata[4].try_into().unwrap();
+                for _ in 0..outside_calls_len {
+                    let outside_call = Self::parse_outside_call(
+                        contract_class_cache,
+                        &call.calldata,
+                        5,
+                        call.contract_address,
+                    )
+                    .await?;
+                    outside_calls.push(outside_call);
+                }
+            }
+            _ => {}
+        }
+
+        Ok(outside_calls)
+    }
+
+    async fn parse_execute<P: Provider + Send + Sync + std::fmt::Debug>(
+        execute: Execute,
+        sender_address: Felt,
+        contract_class_cache: &ContractClassCache<P>,
+    ) -> Result<Vec<ParsedCall>> {
+        let mut calls = Vec::new();
+
+        match execute {
+            Execute::Execute(execute) => {
+                for call in execute.calls {
+                    let parsed_call = Self::parse_execute_call(
+                        contract_class_cache,
+                        &call,
+                        sender_address,
+                        CallType::Execute,
+                    )
+                    .await?;
+                    calls.push(parsed_call);
+                }
+            }
+            Execute::Legacy(execute) => {
+                for call in execute.calls {
+                    let parsed_call = Self::parse_legacy_execute_call(
+                        contract_class_cache,
+                        &call,
+                        &execute.calldata,
+                        sender_address,
+                        CallType::Execute,
+                    )
+                    .await?;
+                    calls.push(parsed_call);
+                }
+            }
+        }
+
+        // Process any outside calls
+        let mut all_calls = calls.clone();
+        for call in calls {
+            let mut outside_calls =
+                Self::process_outside_calls(contract_class_cache, &call).await?;
+            all_calls.append(&mut outside_calls);
+        }
+
+        Ok(all_calls)
+    }
+}
 
 #[async_trait]
 impl<P: Provider + Send + Sync + std::fmt::Debug> TransactionProcessor<P>
@@ -60,172 +263,46 @@ impl<P: Provider + Send + Sync + std::fmt::Debug> TransactionProcessor<P>
         contract_addresses: &HashSet<Felt>,
         transaction: &Transaction,
         contract_class_cache: &ContractClassCache<P>,
-    ) -> Result<(), anyhow::Error> {
-        let transaction_type = match transaction {
-            Transaction::Invoke(_) => "INVOKE",
-            Transaction::L1Handler(_) => "L1_HANDLER",
-            _ => return Ok(()),
+    ) -> Result<()> {
+        let Some(tx_info) = Self::extract_transaction_info(transaction) else {
+            return Ok(());
         };
 
-        let (transaction_hash, sender_address, calldata, max_fee, signature, nonce) =
-            match transaction {
-                Transaction::Invoke(InvokeTransaction::V3(invoke_v3_transaction)) => (
-                    invoke_v3_transaction.transaction_hash,
-                    invoke_v3_transaction.sender_address,
-                    &invoke_v3_transaction.calldata,
-                    Felt::ZERO, // has no max_fee
-                    &invoke_v3_transaction.signature,
-                    invoke_v3_transaction.nonce,
-                ),
-                Transaction::Invoke(InvokeTransaction::V1(invoke_v1_transaction)) => (
-                    invoke_v1_transaction.transaction_hash,
-                    invoke_v1_transaction.sender_address,
-                    &invoke_v1_transaction.calldata,
-                    invoke_v1_transaction.max_fee,
-                    &invoke_v1_transaction.signature,
-                    invoke_v1_transaction.nonce,
-                ),
-                Transaction::L1Handler(l1_handler_transaction) => (
-                    l1_handler_transaction.transaction_hash,
-                    l1_handler_transaction.contract_address,
-                    &l1_handler_transaction.calldata,
-                    Felt::ZERO, // has no max_fee
-                    &vec![],    // has no signature
-                    l1_handler_transaction.nonce.into(),
-                ),
-                _ => return Ok(()),
-            };
+        let calls = if tx_info.transaction_type == "INVOKE" {
+            let execute =
+                if let Ok(execute) = ExecuteTransaction::cairo_deserialize(&tx_info.calldata, 0) {
+                    Some(Execute::Execute(execute))
+                } else if let Ok(execute) =
+                    LegacyExecuteTransaction::cairo_deserialize(&tx_info.calldata, 0)
+                {
+                    Some(Execute::Legacy(execute))
+                } else {
+                    None
+                };
 
-        let mut calls: Vec<ParsedCall> = vec![];
-
-        if transaction_type == "INVOKE" {
-            let execute = if let Ok(execute) = ExecuteTransaction::cairo_deserialize(calldata, 0) {
-                Some(Execute::Execute(execute))
-            } else if let Ok(execute) = LegacyExecuteTransaction::cairo_deserialize(calldata, 0) {
-                Some(Execute::Legacy(execute))
+            if let Some(execute) = execute {
+                Self::parse_execute(execute, tx_info.sender_address, contract_class_cache).await?
             } else {
-                None
-            };
-
-            match execute {
-                Some(Execute::Execute(execute)) => {
-                    for call in execute.calls {
-                        let contract_class = contract_class_cache
-                            .get(call.contract_address, BlockId::Tag(BlockTag::Pending))
-                            .await?;
-                        let entrypoint =
-                            get_entrypoint_name_from_class(&contract_class, call.selector)
-                                .unwrap_or(format!("{:#x}", call.selector));
-
-                        calls.push(ParsedCall {
-                            contract_address: call.contract_address,
-                            entrypoint,
-                            calldata: call.calldata.clone(),
-                            call_type: CallType::Execute,
-                            caller_address: sender_address,
-                        });
-                    }
-                }
-                Some(Execute::Legacy(execute)) => {
-                    for call in execute.calls {
-                        let contract_class = contract_class_cache
-                            .get(call.contract_address, BlockId::Tag(BlockTag::Pending))
-                            .await?;
-                        let entrypoint =
-                            get_entrypoint_name_from_class(&contract_class, call.selector)
-                                .unwrap_or(format!("{:#x}", call.selector));
-
-                        calls.push(ParsedCall {
-                            contract_address: call.contract_address,
-                            entrypoint,
-                            calldata: execute.calldata
-                                [call.data_offset..call.data_offset + call.data_length]
-                                .to_vec(),
-                            call_type: CallType::Execute,
-                            caller_address: sender_address,
-                        });
-                    }
-                }
-                _ => {}
+                vec![]
             }
-
-            for call in calls.clone() {
-                if call.entrypoint == "execute_from_outside_v3" {
-                    let outside_calls_len: usize = call.calldata[5].try_into().unwrap();
-                    for _ in 0..outside_calls_len {
-                        let to_offset = 6;
-                        let selector_offset = to_offset + 1;
-                        let calldata_offset = selector_offset + 2;
-                        let calldata_len: usize =
-                            call.calldata[selector_offset + 1].try_into().unwrap();
-                        let contract_address = call.calldata[to_offset];
-                        let contract_class = contract_class_cache
-                            .get(contract_address, BlockId::Tag(BlockTag::Pending))
-                            .await?;
-                        let entrypoint = get_entrypoint_name_from_class(
-                            &contract_class,
-                            call.calldata[selector_offset],
-                        )
-                        .unwrap_or(format!("{:#x}", call.calldata[selector_offset]));
-
-                        let outside_call = ParsedCall {
-                            contract_address,
-                            entrypoint,
-                            calldata: call.calldata
-                                [calldata_offset..calldata_offset + calldata_len]
-                                .to_vec(),
-                            call_type: CallType::ExecuteFromOutside,
-                            caller_address: call.contract_address,
-                        };
-                        calls.push(outside_call);
-                    }
-                } else if call.entrypoint == "execute_from_outside_v2" {
-                    // the execute_from_outside_v2 nonce is only a felt, thus we have a 4 offset
-                    let outside_calls_len: usize = call.calldata[4].try_into().unwrap();
-                    for _ in 0..outside_calls_len {
-                        let to_offset = 5;
-                        let selector_offset = to_offset + 1;
-                        let calldata_offset = selector_offset + 2;
-                        let calldata_len: usize =
-                            call.calldata[selector_offset + 1].try_into().unwrap();
-                        let contract_address = call.calldata[to_offset];
-                        let contract_class = contract_class_cache
-                            .get(contract_address, BlockId::Tag(BlockTag::Pending))
-                            .await?;
-                        let entrypoint = get_entrypoint_name_from_class(
-                            &contract_class,
-                            call.calldata[selector_offset],
-                        )
-                        .unwrap_or(format!("{:#x}", call.calldata[selector_offset]));
-
-                        let outside_call = ParsedCall {
-                            contract_address,
-                            entrypoint,
-                            calldata: call.calldata
-                                [calldata_offset..calldata_offset + calldata_len]
-                                .to_vec(),
-                            call_type: CallType::ExecuteFromOutside,
-                            caller_address: call.contract_address,
-                        };
-                        calls.push(outside_call);
-                    }
-                }
-            }
-        }
+        } else {
+            vec![]
+        };
 
         db.store_transaction(
-            transaction_hash,
-            sender_address,
-            calldata,
-            max_fee,
-            signature,
-            nonce,
+            tx_info.transaction_hash,
+            tx_info.sender_address,
+            &tx_info.calldata,
+            tx_info.max_fee,
+            &tx_info.signature,
+            tx_info.nonce,
             block_number,
             contract_addresses,
-            transaction_type,
+            tx_info.transaction_type,
             block_timestamp,
             &calls,
         )?;
+
         Ok(())
     }
 }
