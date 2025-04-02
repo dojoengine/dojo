85e3971a8fa543bec50a66c4922c5033eae805fa
Nasr <larko236@outlook.com>
2025-04-02 17:25:59 +0900

    add flag in config to enable checks & move explorer to runner cstegory

--- crates/torii/cli/src/args.rs
+++ crates/torii/cli/src/args.rs
@@ -36,13 +36,12 @@ pub struct ToriiArgs {
     )]
     pub db_dir: Option<PathBuf>,
 
-    /// Open World Explorer on the browser.
-    #[arg(long, help = "Open World Explorer on the browser.")]
-    pub explorer: bool,
-
     /// Configuration file
     #[arg(long, help = "Configuration file to setup Torii.")]
     pub config: Option<PathBuf>,
+
+    #[command(flatten)]
+    pub runner: RunnerOptions,
 
     #[command(flatten)]
     pub indexing: IndexingOptions,
@@ -75,12 +74,12 @@ impl Default for ToriiArgs {
             world_address: None,
             rpc: Url::parse(DEFAULT_RPC_URL).unwrap(),
             db_dir: None,
-            explorer: false,
             config: None,
             indexing: IndexingOptions::default(),
             events: EventsOptions::default(),
             erc: ErcOptions::default(),
             sql: SqlOptions::default(),
+            runner: RunnerOptions::default(),
             #[cfg(feature = "server")]
             metrics: MetricsOptions::default(),
             #[cfg(feature = "server")]
@@ -209,7 +208,8 @@ mod test {
 
         assert_eq!(torii_args.db_dir, None);
 
-        assert!(!torii_args.explorer);
+        assert!(!torii_args.runner.explorer);
+        assert!(!torii_args.runner.check_contracts);
 
         assert_eq!(torii_args.indexing, IndexingOptions::default());
         assert_eq!(torii_args.events, EventsOptions::default());
