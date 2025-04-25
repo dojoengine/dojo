2ca73e9869562f74acf4a221ba941d910d3060ae
remy.baranx@gmail.com <remy.baranx@gmail.com>
2025-04-25 21:44:35 +0100

    fix: inner type extracting for composite types

--- crates/dojo/lang/src/derive_macros/introspect/utils.rs
+++ crates/dojo/lang/src/derive_macros/introspect/utils.rs
@@ -1,7 +1,15 @@
+use regex;
 use std::collections::HashMap;
 
 #[derive(Clone, Default, Debug)]
 pub struct TypeIntrospection(pub usize, pub Vec<usize>);
+
+const TUPLE_PREFIX: &str = "(";
+const TUPLE_SUFFIX: &str = ")";
+const SPAN_PREFIX: &str = "Span<";
+const SPAN_SUFFIX: &str = ">";
+const ARRAY_PREFIX: &str = "Array<";
+const ARRAY_SUFFIX: &str = ">";
 
 // Provides type introspection information for primitive types
 pub fn primitive_type_introspection() -> HashMap<String, TypeIntrospection> {
@@ -32,32 +40,52 @@ pub fn is_byte_array(ty: &str) -> bool {
 }
 
 pub fn is_array(ty: &str) -> bool {
-    ty.starts_with("Array<") || ty.starts_with("Span<")
+    ty.starts_with(ARRAY_PREFIX) || ty.starts_with(SPAN_PREFIX)
 }
 
 pub fn is_tuple(ty: &str) -> bool {
-    ty.starts_with('(')
+    ty.starts_with(TUPLE_PREFIX)
 }
 
 pub fn get_array_item_type(ty: &str) -> String {
-    if ty.starts_with("Array<") {
-        ty.trim().strip_prefix("Array<").unwrap().strip_suffix('>').unwrap().to_string()
+    if ty.starts_with(ARRAY_PREFIX) {
+        extract_composite_inner_type(ty, ARRAY_PREFIX, ARRAY_SUFFIX)
     } else {
-        ty.trim().strip_prefix("Span<").unwrap().strip_suffix('>').unwrap().to_string()
+        extract_composite_inner_type(ty, SPAN_PREFIX, SPAN_SUFFIX)
     }
+}
+
+/// Extracts the inner type of a composite type such as tuple, array or span.
+///
+/// # Arguments
+///   * `ty` - the composite type
+///   * `prefix` - the prefix used to delimit the beginning of the composite type
+///   * `suffix` - the suffix used to delimit the end of the composite type
+///
+/// # Examples
+///    extract_composite_inner_type("Array<(u8, u16)", "Array<", ">") returns "u8, u16"
+pub fn extract_composite_inner_type(ty: &str, prefix: &str, suffix: &str) -> String {
+    // Note: Until at least 2.11, in cairo_lang_* crates, if there is a comment after a struct field type,
+    // without a comma, like `v1: Span<u32> // comment`, the comment is included in the type definition
+    // while reading it from the AST.
+    let re = regex::Regex::new(&format!(
+        "{}\\s*(\\S*.*\\S+)\\s*{}",
+        regex::escape(prefix),
+        regex::escape(suffix)
+    ))
+    .unwrap();
+
+    let caps = re
+        .captures(ty)
+        .expect(&format!("'{ty}' must contain the '{prefix}' prefix and the '{suffix}' suffix."));
+
+    caps[1].to_string().replace(" ", "")
 }
 
 /// split a tuple in array of items (nested tuples are not splitted).
 /// example (u8, (u16, u32), u128) -> ["u8", "(u16, u32)", "u128"]
 pub fn get_tuple_item_types(ty: &str) -> Vec<String> {
-    let tuple_str = ty
-        .trim()
-        .strip_prefix('(')
-        .unwrap()
-        .strip_suffix(')')
-        .unwrap()
-        .to_string()
-        .replace(' ', "");
+    let tuple_str = extract_composite_inner_type(ty, TUPLE_PREFIX, TUPLE_SUFFIX);
     let mut items = vec![];
     let mut current_item = "".to_string();
     let mut level = 0;
@@ -75,10 +103,10 @@ pub fn get_tuple_item_types(ty: &str) -> Vec<String> {
         } else {
             current_item.push(c);
 
-            if c == '(' {
+            if c.to_string() == TUPLE_PREFIX {
                 level += 1;
             }
-            if c == ')' {
+            if c.to_string() == TUPLE_SUFFIX {
                 level -= 1;
             }
         }
@@ -133,4 +161,83 @@ pub fn test_get_tuple_item_types() {
             expected.iter().map(|x| x.to_string()).collect::<Vec<_>>(),
         )
     }
+}
+
+#[test]
+fn test_extract_composite_inner_type_with_tuples() {
+    let test_cases = [
+        ("(u8,)", "u8,"),
+        ("(u8,),", "u8,"),
+        ("(u8, u16)", "u8,u16"),
+        ("(u8, u16,)", "u8,u16,"),
+        ("(u8, u16, (u32,))", "u8,u16,(u32,)"),
+        ("(u8, u16, (u32,),)", "u8,u16,(u32,),"),
+        (
+            "(u8, (Span<u32>, u32, Option<Array<u8>,) u16, (u32,),)",
+            "u8,(Span<u32>,u32,Option<Array<u8>,)u16,(u32,),",
+        ),
+        ("(u8, u32) // comment", "u8,u32"),
+        ("(u8, u32), // comment", "u8,u32"),
+    ];
+
+    for (tuple, expected) in test_cases {
+        let result = extract_composite_inner_type(tuple, TUPLE_PREFIX, TUPLE_SUFFIX);
+        assert!(
+            result == expected,
+            "bad tuple: {} result: {} expected: {}",
+            tuple,
+            result,
+            expected
+        );
+    }
+}
+
+#[test]
+#[should_panic(expected = "'u8, u16' must contain the '(' prefix and the ')' suffix.")]
+fn test_extract_composite_inner_type_with_tuples_bad_ty() {
+    let _ = extract_composite_inner_type("u8, u16", TUPLE_PREFIX, TUPLE_SUFFIX);
+}
+
+#[test]
+fn test_extract_composite_inner_type_with_arrays() {
+    let test_cases = [
+        ("Array<u8>", "u8"),
+        ("Array<(u8, u16)>", "(u8,u16)"),
+        ("Array<Array<(u8, u16)>>", "Array<(u8,u16)>"),
+        ("Array<(u8, u16)> // comment", "(u8,u16)"),
+        ("Array<(u8, u16)>, // comment", "(u8,u16)"),
+    ];
+
+    for (arr, expected) in test_cases {
+        let result = extract_composite_inner_type(arr, ARRAY_PREFIX, ARRAY_SUFFIX);
+        assert!(result == expected, "bad array: {} result: {} expected: {}", arr, result, expected);
+    }
+}
+
+#[test]
+#[should_panic(expected = "'u8, u16' must contain the 'Array<' prefix and the '>' suffix.")]
+fn test_extract_composite_inner_type_with_arrays_bad_ty() {
+    let _ = extract_composite_inner_type("u8, u16", ARRAY_PREFIX, ARRAY_SUFFIX);
+}
+
+#[test]
+fn test_extract_composite_inner_type_with_spans() {
+    let test_cases = [
+        ("Span<u8>", "u8"),
+        ("Span<(u8, u16)>", "(u8,u16)"),
+        ("Span<Array<(u8, u16)>>", "Array<(u8,u16)>"),
+        ("Span<(u8, u16)> // comment", "(u8,u16)"),
+        ("Span<(u8, u16)>, // comment", "(u8,u16)"),
+    ];
+
+    for (sp, expected) in test_cases {
+        let result = extract_composite_inner_type(sp, SPAN_PREFIX, SPAN_SUFFIX);
+        assert!(result == expected, "bad span: {} result: {} expected: {}", sp, result, expected);
+    }
+}
+
+#[test]
+#[should_panic(expected = "'u8, u16' must contain the 'Span<' prefix and the '>' suffix.")]
+fn test_extract_composite_inner_type_with_spans_bad_ty() {
+    let _ = extract_composite_inner_type("u8, u16", SPAN_PREFIX, SPAN_SUFFIX);
 }
