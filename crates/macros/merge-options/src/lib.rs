03aa6c4983714b780bfe2fee23dd4498151f4364
one230six <163239332+one230six@users.noreply.github.com>
2025-06-17 18:21:57 +0100

    chore: add the missing backticks in the comments (#3243)
    Signed-off-by: one230six <723682061@qq.com>

--- crates/macros/merge-options/src/lib.rs
+++ crates/macros/merge-options/src/lib.rs
@@ -74,6 +74,7 @@ use syn::{parse_macro_input, Data, DeriveInput, Fields};
 ///         }
 ///     }
 /// }
+/// ```
 #[proc_macro_derive(MergeOptions, attributes(merge))]
 pub fn derive_merge_options(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
