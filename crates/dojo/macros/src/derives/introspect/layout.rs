370a39d136fa0905e0d2e164632649cd4ac0b688
Remy Baranx <remy.baranx@gmail.com>
2025-06-27 11:45:42 +0200

    fix fmt

--- crates/dojo/macros/src/derives/introspect/layout.rs
+++ crates/dojo/macros/src/derives/introspect/layout.rs
@@ -1,8 +1,8 @@
 use cairo_lang_macro::Diagnostic;
 use cairo_lang_parser::utils::SimpleParserDatabase;
-use cairo_lang_syntax::node::TypedSyntaxNode;
 use cairo_lang_syntax::node::ast::{Expr, TypeClause};
 use cairo_lang_syntax::node::db::SyntaxGroup;
+use cairo_lang_syntax::node::TypedSyntaxNode;
 
 use super::utils::{
     get_array_item_type, get_tuple_item_types, is_array, is_byte_array, is_option, is_tuple,
