0c0f5fdbb94d4688345de54eb0e23914484909eb
Ammar Arif <evergreenkary@gmail.com>
2025-08-14 01:08:58 -0400

    save

--- crates/dojo/macros/src/derives/introspect/ty.rs
+++ crates/dojo/macros/src/derives/introspect/ty.rs
@@ -7,14 +7,17 @@ use super::utils::{get_array_item_type, is_array, is_byte_array};
 pub fn build_ty_from_type_clause(db: &dyn SyntaxGroup, type_clause: &TypeClause) -> String {
     match type_clause.ty(db) {
         Expr::Path(path) => {
+            println!("build_ty_from_type_clause: it's giving path");
             let path_type = path.as_syntax_node().get_text_without_trivia(db);
             build_item_ty_from_type(&path_type)
         }
         Expr::Tuple(expr) => {
+            println!("build_ty_from_type_clause: it's giving tuple");
             let tuple_type = expr.as_syntax_node().get_text_without_trivia(db);
             build_item_ty_from_type(&tuple_type)
         }
         Expr::FixedSizeArray(expr) => {
+            println!("build_ty_from_type_clause: it's giving FixedSizeArray");
             let arr_type = expr.as_syntax_node().get_text_without_trivia(db);
             build_item_ty_from_type(&arr_type)
         }
