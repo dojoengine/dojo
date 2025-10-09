659f39a9cb59269ac0742dd985000cb72c381328
Nasr <larko236@outlook.com>
2025-10-09 12:02:38 -0500

    use cainome to lossy

--- crates/dojo/types/src/schema.rs
+++ crates/dojo/types/src/schema.rs
@@ -6,7 +6,6 @@ use itertools::Itertools;
 use num_traits::ToPrimitive;
 use serde::{Deserialize, Serialize};
 use serde_json::{json, Value as JsonValue};
-use starknet::core::codec::Decode;
 use starknet::core::types::Felt;
 use strum_macros::AsRefStr;
 
@@ -286,14 +285,10 @@ impl Ty {
                 }
             }
             Ty::ByteArray(string) => {
-                let mut iterator = felts.iter();
-                let bytearray = starknet::core::types::ByteArray::decode_iter(&mut iterator)?;
-                let cairo_bytearray = ByteArray::cairo_deserialize(&felts, 0)?;
-                felts.drain(0..ByteArray::cairo_serialized_size(&cairo_bytearray));
+                let bytearray = ByteArray::cairo_deserialize(&felts, 0)?;
+                felts.drain(0..ByteArray::cairo_serialized_size(&bytearray));
 
-                let bytes: Vec<u8> = bytearray.into();
-
-                *string = String::from_utf8_lossy(bytes.as_slice()).to_string();
+                *string = bytearray.to_string_lossy().to_string();
             }
         }
         Ok(())
